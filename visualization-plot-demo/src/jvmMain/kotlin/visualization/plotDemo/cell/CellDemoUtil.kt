e944adf3e897cb9f409e05b09355f21ae553c89b
Igor Alshannikov <Igor.Alshannikov@jetbrains.com>
2019-05-15 20:26:04 -0400

    BarPlotXAxisDemo (in proggress)

--- visualization-plot-demo/src/jvmMain/kotlin/visualization/plotDemo/cell/CellDemoUtil.kt
+++ visualization-plot-demo/src/jvmMain/kotlin/visualization/plotDemo/cell/CellDemoUtil.kt
@@ -1,8 +1,16 @@
 package jetbrains.datalore.visualization.plotDemo.cell
 
+import jetbrains.datalore.base.geometry.DoubleRectangle
+import jetbrains.datalore.base.geometry.DoubleVector
+import jetbrains.datalore.base.observable.event.EventHandler
+import jetbrains.datalore.base.observable.property.PropertyChangeEvent
+import jetbrains.datalore.visualization.base.svg.SvgColors
+import jetbrains.datalore.visualization.base.svg.SvgRectElement
 import jetbrains.datalore.visualization.gogProjectionalDemo.model.cell.BarPlotXAxisDemo
 import jetbrains.datalore.visualization.plotDemo.SwingDemoFrame
 import jetbrains.datalore.visualization.plotDemo.SwingDemoFrame.Companion.createComponent
+import java.awt.Color
+import javax.swing.BorderFactory
 
 object CellDemoUtil {
     @JvmStatic
@@ -16,12 +24,34 @@ object CellDemoUtil {
             val plot = demoModel.createPlot()
             plot.ensureContentBuilt()
 
-            val component = createComponent(plot.svg)
+            val svg = plot.svg
+
+            val plotSizeProperty = demoModel.plotSize
+
+            val viewportRect = SvgRectElement(DoubleRectangle(DoubleVector.ZERO, plotSizeProperty.get()))
+
+            viewportRect.stroke().set(SvgColors.SvgColorKeyword.LIGHT_BLUE)
+            viewportRect.fill().set(SvgColors.SvgColorKeyword.NONE)
+            svg.children().add(viewportRect)
+
+
+            val component = createComponent(svg)
+            component.border = BorderFactory.createLineBorder(Color.BLUE, 1)
             add(component)
 
             SwingDemoFrame.addVSpace(this)
-            val widthControl = WidthControl(demoModel.plotSize.get().x)
+
+            val plotHeight = plotSizeProperty.get().y
+            val widthControl = WidthControl(plotSizeProperty.get().x)
             add(widthControl)
+
+            widthControl.width.addHandler(object : EventHandler<PropertyChangeEvent<out Double>> {
+                override fun onEvent(event: PropertyChangeEvent<out Double>) {
+                    val newWidth = event.newValue!!
+                    val newSize = DoubleVector(newWidth, plotHeight)
+                    plotSizeProperty.set(newSize)
+                }
+            })
         }
     }
 }
