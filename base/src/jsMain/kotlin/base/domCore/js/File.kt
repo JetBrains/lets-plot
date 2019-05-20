ca97e3852e46a8ff6136479825583c3a24e32a6b
Alexander Rigachnyy <arigachnyy@horis.ru>
2019-05-20 14:50:21 +0300

    use typealiases for File and FileList

--- base/src/jsMain/kotlin/base/domCore/js/File.kt
+++ base/src/jsMain/kotlin/base/domCore/js/File.kt
@@ -1,15 +1,3 @@
 package jetbrains.datalore.base.domCore.js
 
-class File(private val file: org.w3c.files.File) {
-    val name: String
-        get() = file.name
-
-    val size: Int
-        get() = file.size
-
-    companion object {
-        fun create(file: org.w3c.files.File?): File? {
-            return if (file != null) File(file) else null
-        }
-    }
-}
+typealias File = org.w3c.files.File
--- base/src/jsMain/kotlin/base/domCore/js/FileList.kt
+++ base/src/jsMain/kotlin/base/domCore/js/FileList.kt
@@ -1,10 +1,3 @@
 package jetbrains.datalore.base.domCore.js
 
-class FileList(private val fileList: org.w3c.files.FileList) {
-    val length: Int
-        get() = fileList.length
-
-    fun item(index: Int): File? {
-        return File.create(fileList.item(index))
-    }
-}
+typealias FileList = org.w3c.files.FileList
