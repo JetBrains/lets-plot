b058d26937d61b4bef9bbc0808e2b94a84c21996
Igor Alshannikov <Igor.Alshannikov@jetbrains.com>
2019-06-20 16:45:10 -0400

    Improve demos

--- visualization-base-svg-mapper-demo/src/jvmJfxMain/kotlin/visualization/svgMapperDemo/SvgElementsDemoSceneMapper.kt
+++ visualization-base-svg-mapper-demo/src/jvmJfxMain/kotlin/visualization/svgMapperDemo/SvgElementsDemoSceneMapper.kt
@@ -3,21 +3,13 @@ package jetbrains.datalore.visualization.svgMapperDemo
 import jetbrains.datalore.base.geometry.DoubleVector
 import jetbrains.datalore.visualization.base.swing.SceneMapperDemoFrame
 import jetbrains.datalore.visualization.svgMapperDemo.model.DemoModel
-import javax.swing.SwingUtilities
 
-class SvgElementsDemoSceneMapper {
-    companion object {
-        @JvmStatic
-        fun main(args: Array<String>) {
-            SwingUtilities.invokeLater { show() }
-        }
-
-        private fun show() {
-            val svgRoots = listOf(DemoModel.createModel())
-            SceneMapperDemoFrame.showSvg(svgRoots,
-                    listOf("/svgMapperDemo.css"),
-                    DoubleVector(500.0, 300.0),
-                    "Svg Elements (JFX SVG mapper)")
-        }
-    }
+fun main() {
+    val svgRoots = listOf(DemoModel.createModel())
+    SceneMapperDemoFrame.showSvg(
+        svgRoots,
+        listOf("/svgMapperDemo.css"),
+        DoubleVector(500.0, 300.0),
+        "Svg Elements (JFX SVG mapper)"
+    )
 }
