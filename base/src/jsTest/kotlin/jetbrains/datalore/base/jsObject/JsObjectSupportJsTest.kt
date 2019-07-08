286e93e23b95c43c0def6a0ea20209ddef4dc337
Igor Alshannikov <Igor.Alshannikov@jetbrains.com>
2019-07-08 11:13:14 -0400

    add JsObjectSupportTest (JS) and trying to run it (mocha, karma , yarn)

---
+++ base/src/jsTest/kotlin/jetbrains/datalore/base/jsObject/JsObjectSupportJsTest.kt
@@ -0,0 +1,38 @@
+package jetbrains.datalore.base.jsObject
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class JsObjectSupportJsTest {
+
+    @Test
+    fun runTests() {
+        for ((index, datum) in testData.withIndex()) {
+            @Suppress("UNCHECKED_CAST")
+            oneTest(datum[0], datum[1] as Map<String, *>)
+        }
+
+    }
+
+    private fun oneTest(input: dynamic, expected: Map<String, *>) {
+        val actual = dynamicObjectToMap(input)
+        assertEquals(expected, actual)
+    }
+
+    companion object {
+        @Suppress("UnsafeCastFromDynamic")
+        val testData: List<Array<Any>> = listOf(
+            arrayOf(
+                js(
+                    """
+                {
+                    key:"val"
+                }"""
+                ),
+                mapOf<String, Any?>(
+                    "key" to "val1"
+                )
+            )
+        )
+    }
+}
