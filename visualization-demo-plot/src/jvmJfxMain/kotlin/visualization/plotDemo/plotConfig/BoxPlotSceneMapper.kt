ddd3e3312a10f70d91fd0eaa92cacf992ea33a52
Igor Alshannikov <Igor.Alshannikov@jetbrains.com>
2019-07-19 16:10:28 -0400

    JFX mapping: add plot config demos

---
+++ visualization-demo-plot/src/jvmJfxMain/kotlin/visualization/plotDemo/plotConfig/BoxPlotSceneMapper.kt
@@ -0,0 +1,21 @@
+package jetbrains.datalore.visualization.plotDemo.plotConfig
+
+import jetbrains.datalore.visualization.demoUtils.jfx.SceneMapperDemoFactory
+import jetbrains.datalore.visualization.plot.builder.presentation.Style.JFX_PLOT_STYLESHEET
+import jetbrains.datalore.visualization.plotDemo.model.plotConfig.BoxPlot
+
+object BoxPlotSceneMapper {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        with(BoxPlot()) {
+            @Suppress("UNCHECKED_CAST")
+            val plotSpecList = plotSpecList() as List<MutableMap<String, Any>>
+            PlotConfigDemoUtil.show(
+                "Box plot",
+                plotSpecList,
+                SceneMapperDemoFactory(JFX_PLOT_STYLESHEET),
+                demoComponentSize
+            )
+        }
+    }
+}
---
+++ visualization-demo-plot/src/jvmJfxMain/kotlin/visualization/plotDemo/plotConfig/HistogramSceneMapper.kt
@@ -0,0 +1,21 @@
+package jetbrains.datalore.visualization.plotDemo.plotConfig
+
+import jetbrains.datalore.visualization.demoUtils.jfx.SceneMapperDemoFactory
+import jetbrains.datalore.visualization.plot.builder.presentation.Style.JFX_PLOT_STYLESHEET
+import jetbrains.datalore.visualization.plotDemo.model.plotConfig.Histogram
+
+object HistogramSceneMapper {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        with(Histogram()) {
+            @Suppress("UNCHECKED_CAST")
+            val plotSpecList = plotSpecList() as List<MutableMap<String, Any>>
+            PlotConfigDemoUtil.show(
+                "Histogram",
+                plotSpecList,
+                SceneMapperDemoFactory(JFX_PLOT_STYLESHEET),
+                demoComponentSize
+            )
+        }
+    }
+}
