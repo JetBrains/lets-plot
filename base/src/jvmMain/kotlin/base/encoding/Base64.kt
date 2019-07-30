3aef26188fee950de3eb81095b6c4acbe99720fd
IKupriyanov-HORIS <ikupriyanov@horis.ru>
2019-07-30 20:36:56 +0300

    Common Base64 decoder

--- base/src/jvmMain/kotlin/base/encoding/Base64.kt
+++ base/src/jvmMain/kotlin/base/encoding/Base64.kt
@@ -11,27 +11,51 @@ actual object Base64 {
     }
 
     actual fun decode(s: String): ByteArray {
-        val delta = if (s.endsWith("==")) 2 else if (s.endsWith("=")) 1 else 0
-        val buffer = ByteArray(s.length * 3 / 4 - delta)
-        val mask = 0xFF
-        var index = 0
-        var i = 0
-        while (i < s.length) {
-            val c0 = toInt[s[i].toInt()]
-            val c1 = toInt[s[i + 1].toInt()]
-            buffer[index++] = (c0 shl 2 or (c1 shr 4) and mask).toByte()
-            if (index >= buffer.size) {
-                return buffer
+
+        class ByteParser(val output: MutableList<Byte>) {
+            var c0: Int? = null
+            var c1: Int? = null
+            var c2: Int? = null
+            var c3: Int? = null
+            val mask = 0xFF
+
+            fun read(v: Int) {
+                if (c0 == null) {
+                    c0 = toInt[v]
+                    return
+                }
+
+                if (c1 == null) {
+                    c1 = toInt[v]
+                    output.add((c0!! shl 2 or (c1!! shr 4) and mask).toByte())
+                    return
+                }
+
+                if (c2 == null) {
+                    c2 = toInt[v]
+                    output.add((c1!! shl 4 or (c2!! shr 2) and mask).toByte())
+                    return
+                }
+
+                if (c3 == null) {
+                    c3 = toInt[v]
+                    output.add((c2!! shl 6 or c3!! and mask).toByte())
+
+                    c0 = null
+                    c1 = null
+                    c2 = null
+                    c3 = null
+                }
             }
-            val c2 = toInt[s[i + 2].toInt()]
-            buffer[index++] = (c1 shl 4 or (c2 shr 2) and mask).toByte()
-            if (index >= buffer.size) {
-                return buffer
-            }
-            val c3 = toInt[s[i + 3].toInt()]
-            buffer[index++] = (c2 shl 6 or c3 and mask).toByte()
-            i += 4
         }
-        return buffer
+
+
+        val res = ArrayList<Byte>()
+        val byteParser = ByteParser(res)
+        s.asSequence()
+            .filterNot { ALPHABET.contains(it) }
+            .forEach { byteParser.read(it.toInt()) }
+
+        return ByteArray(res.size, res::get)
     }
 }
