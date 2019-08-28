2889f06cb327ef22fdfaa3b535d16e9cf0c2f7d7
IKupriyanov-HORIS <ikupriyanov@horis.ru>
2019-08-28 20:51:02 +0300

    HorizontalTooltipExpander refactoring

--- visualization-plot-builder/src/commonMain/kotlin/jetbrains/datalore/visualization/plot/builder/tooltip/layout/HorizontalTooltipExpander.kt
+++ visualization-plot-builder/src/commonMain/kotlin/jetbrains/datalore/visualization/plot/builder/tooltip/layout/HorizontalTooltipExpander.kt
@@ -8,76 +8,65 @@ import jetbrains.datalore.visualization.plot.builder.presentation.Defaults.Commo
 import jetbrains.datalore.visualization.plot.builder.tooltip.layout.LayoutManager.Companion.moveIntoLimit
 import jetbrains.datalore.visualization.plot.builder.tooltip.layout.LayoutManager.PositionedTooltip
 
-internal typealias TtGroup  = HorizontalTooltipExpander.Group<PositionedTooltip>
-
 internal class HorizontalTooltipExpander(private val mySpace: DoubleRange) {
-    internal class Group<ItemT: PositionedTooltip> internal constructor(
-        item: ItemT,
-        private var range: DoubleRange,
-        private val space: DoubleRange
-    ) {
-        private val items = ArrayList<Pair<ItemT, Double>>()
-
-        init {
-            items.add(item, range.length())
-        }
-
-        fun overlaps(other: Group<ItemT>) = range.extend(MARGIN_BETWEEN_TOOLTIPS).overlaps(other.range)
-
-        internal fun positions(): List<Pair<PositionedTooltip, DoubleRange>> {
-            var y = range.start()
-            return items.map { Pair(it.tooltip, rangeWithLength(y, it.length)).apply { y += it.length + MARGIN_BETWEEN_TOOLTIPS } }
-        }
-
-        fun join(other: Group<ItemT>): Group<ItemT> {
-            items.addAll(other.items)
-            val unitedLength = items.map { it.length + MARGIN_BETWEEN_TOOLTIPS }.sum() - MARGIN_BETWEEN_TOOLTIPS
-            val unitedMiddle = (this.range.middle() + other.range.middle()) / 2
-            val unitedRange = withStartAndLength(unitedMiddle - unitedLength / 2, unitedLength)
-            range = moveIntoLimit(unitedRange, space)
-            return this
-        }
-    }
-
 
     fun fixOverlapping(tooltips: List<PositionedTooltip>): List<PositionedTooltip> {
         return tooltips
-            .sortedWith(compareBy( { it.stemCoord.y}, { it.tooltipCoord.y } ) )
-            .fold(ArrayList<TtGroup>(), ::spaceOutTooltip)
-            .flatMap { it.positions() }
-            .map { (tt, position) -> tt.moveTo(DoubleVector(tt.left, position.start())) }
+            .sortedWith(compareBy({ it.stemCoord.y }, { it.tooltipCoord.y }))
+            .fold(ArrayList<Group>(), ::spaceOutTooltip)
+            .flatMap {
+                var y = it.range.start()
+                it.tooltips.map { tooltip ->
+                    tooltip.moveTo(DoubleVector(tooltip.left, y))
+                        .also { y += tooltip.height + MARGIN_BETWEEN_TOOLTIPS }
+                }
+            }
     }
 
-    private fun spaceOutTooltip(groups: ArrayList<TtGroup>, tt: PositionedTooltip): ArrayList<TtGroup> {
-        groups.add(TtGroup(tt, withStartAndLength(tt.tooltipCoord.y, tt.height), mySpace))
+    private fun spaceOutTooltip(groups: ArrayList<Group>, tt: PositionedTooltip): ArrayList<Group> {
+        groups.add(Group(tt, mySpace))
 
         // space out one by one overlapped group
         // 50 overlapped groups maximum in case of very limited space, when we have no room for all groups
-        for (i in 1..50) {
-            val overlapping = findOverlappedGroups(groups)
-            if (overlapping == null) {
-                break;
-            }
-
-            val upperIndex = groups.indexOf(overlapping.first)
-            groups[upperIndex] = overlapping.first.join(overlapping.second)
-            groups.remove(overlapping.second)
+        repeat(50) {
+            groups
+                .windowed(2) // [1, 2, 3, 4] -> [1, 2], [2, 3], [3, 4]
+                .firstOrNull { (g1, g2) -> g1.overlaps(g2) }
+                ?.let { (g1, g2) ->
+                    val index = groups.indexOf(g1)
+                    groups.set(index, Group(g1.tooltips + g2.tooltips, mySpace))
+                    groups.remove(g2)
+                }
+                ?: return groups // stop the loop if no overlappings
         }
         return groups
     }
 
-    private fun findOverlappedGroups(groups: List<TtGroup>): Pair<TtGroup, TtGroup>? {
-        return groups
-            .windowed(2)
-            .filter { (upper, lower) -> upper.overlaps(lower) }
-            .map { (upper, lower) -> Pair(upper, lower) }
-            .firstOrNull()
+    internal class Group internal constructor(
+        internal val tooltips: List<PositionedTooltip>,
+        private val space: DoubleRange
+    ) {
+        internal val range: DoubleRange
+
+        constructor(tooltip: PositionedTooltip, space: DoubleRange)
+                : this(listOf(tooltip), space) {}
+
+        init {
+            val length = tooltips.map { it.length + MARGIN_BETWEEN_TOOLTIPS }.sum() - MARGIN_BETWEEN_TOOLTIPS
+            val start = when(tooltips.size) {
+                0 -> 0.0
+                1 -> tooltips[0].top
+                else -> tooltips.sumByDouble { it.middle } / tooltips.size  - length / 2
+            }
+
+            range = rangeWithLength(start, length).run { moveIntoLimit(this, space) }
+        }
+
+        fun overlaps(other: Group) = range.extend(MARGIN_BETWEEN_TOOLTIPS).overlaps(other.range)
     }
 }
 
 private fun DoubleRange.extend(delta: Double) = withStartAndEnd(start() - delta, end() + delta)
-private fun DoubleRange.middle() = start() + length() / 2
-private fun <A, B> MutableList<Pair<A, B>>.add(a: A, b: B) = add(Pair(a, b))
-private val Pair<PositionedTooltip, Double>.length get() = second
-private val Pair<PositionedTooltip, Double>.tooltip get() = first
 private fun rangeWithLength(start: Double, length: Double) = withStartAndLength(start, length)
+private val PositionedTooltip.length get() = height
+private val PositionedTooltip.middle get() = bottom - height / 2
