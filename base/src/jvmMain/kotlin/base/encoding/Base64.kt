b3670d72135da2eeeedb9f4cc61b638df530b6f4
IKupriyanov-HORIS <ikupriyanov@horis.ru>
2019-08-30 13:22:32 +0300

    Commented using of base64.MimeDecoder over base64.Decoder

--- base/src/jvmMain/kotlin/base/encoding/Base64.kt
+++ base/src/jvmMain/kotlin/base/encoding/Base64.kt
@@ -1,5 +1,8 @@
 package jetbrains.datalore.base.encoding
 
 actual object Base64 {
-    actual fun decode(s: String) = java.util.Base64.getMimeDecoder().decode(s)
+    actual fun decode(s: String): ByteArray {
+        // Decoder throws exception on '\n' in src, use MimeDecoder
+        return java.util.Base64.getMimeDecoder().decode(s)
+    }
 }
