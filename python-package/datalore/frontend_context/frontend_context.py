c25492047f7b2781ea08f6ecaabd435d9a9c5a65
Igor Alshannikov <Igor.Alshannikov@jetbrains.com>
2019-09-26 18:58:53 -0400

    add type_util to convert input numpy/pandas objects to python built-in types

--- python-package/datalore/frontend_context/frontend_context.py
+++ python-package/datalore/frontend_context/frontend_context.py
@@ -1,5 +1,8 @@
 from typing import Dict
 
+from ..plot.core import PlotSpec
+from ..type_utils.type_utils import is_dict_or_dataframe, standardize_dict
+
 
 class FrontendContext:
     def configure(self):
@@ -12,8 +15,21 @@ class FrontendContext:
 _default_frontend_contexts: Dict[str, FrontendContext] = {}
 
 
+def standardize_plot_spec(plot_spec: Dict) -> Dict:
+    """
+    :param plot_spec: dict or pandas.DataFrame
+    """
+    if not is_dict_or_dataframe(plot_spec):
+        raise ValueError("dict or pandas.Dataframe expected but was {}".format(type(plot_spec)))
+
+    return standardize_dict(plot_spec)
+
+
 def as_html(plot_spec: Dict) -> str:
-    # ToDo: convert all types (pandas, numpy) to built-in python types
+    """
+    :param plot_spec: dict or pandas.DataFrame
+    """
+    plot_spec = standardize_plot_spec(plot_spec)
 
     if 'html' not in _default_frontend_contexts:
         connected = False
@@ -25,7 +41,7 @@ def as_html(plot_spec: Dict) -> str:
     return _default_frontend_contexts['html'].as_str(plot_spec)
 
 
-def display_plot(plot_spec):
+def display_plot(plot_spec: PlotSpec):
     """
     Draw plot or `bunch` of plots in the current frontend context
     :param plot_spec: PlotSpec or GGBunch object
@@ -39,7 +55,9 @@ def display_plot(plot_spec):
 
     try:
         from IPython.display import display_html
-        display_html(as_html(plot_spec.as_dict()), raw=True)
+
+        plot_spec_dict = standardize_plot_spec(plot_spec.as_dict())
+        display_html(as_html(plot_spec_dict), raw=True)
         return
     except ImportError:
         pass
