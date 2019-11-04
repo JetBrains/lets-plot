127e7adb0be079e053d84dd8a301fafe5423a409
Igor Alshannikov <Igor.Alshannikov@jetbrains.com>
2019-11-04 18:22:56 -0500

    Hide geom_livemap, geom_image and image matrix.

--- python-package/datalore_plot/plot/__init__.py
+++ python-package/datalore_plot/plot/__init__.py
@@ -25,10 +25,10 @@ __all__ = (coord.__all__ +
            facet.__all__ +
            geom.__all__ +
            geom_extras.__all__ +
-           geom_image_.__all__ +
-           geom_livemap_.__all__ +
+           # geom_image_.__all__ +
+           # geom_livemap_.__all__ +
            guide.__all__ +
-           image_matrix.__all__ +
+           # image_matrix.__all__ +
            label.__all__ +
            plot.__all__ +
            pos.__all__ +
