ba97d0d620e5cbd6becf81c2de6c08969aee5413
Valentin Dovidaytis <vdovidaytis@horis.ru>
2019-11-06 16:08:28 +0300

    Fixed Python versions. Removed unused dependency. Fixed variables.

--- tools/build_manylinux_wheel.sh
+++ tools/build_manylinux_wheel.sh
@@ -8,26 +8,24 @@ set -e -x
 
 working_dir="/tmp/python-package/"
 dist_dir="dist/"
-python_bin_version="cp3[5-7]*"
+python_bin_version="cp3[7-8]*"
 
-# Install a system package required by our library
-yum install -y atlas-devel
 
 cd $working_dir
 
 # Compile wheels
-for PYBIN in /opt/python/${python_bin_version}/bin; do
-    "${PYBIN}/pip" wheel $working_dir -w $dist_dir
+for pybin in /opt/python/${python_bin_version}/bin; do
+    "${pybin}/pip" wheel ${working_dir} -w ${dist_dir}
 done
 
 # Bundle external shared libraries into the wheels
 for whl in ${dist_dir}/*.whl; do
-    auditwheel repair "$whl" --plat $PLAT -w $dist_dir
+    auditwheel repair "$whl" --plat ${PLAT} -w ${dist_dir}
 done
 
 # Remove source wheels with common platform tag 
 shopt -s extglob
-rm $dist_dir*-linux_*
+rm ${dist_dir}*-linux_*
 
 # Change folder ownership to user (uid: 1000)
-chown -R 1000:1000 $working_dir$dist_dir
+chown -R 1000:1000 ${working_dir}${dist_dir}
