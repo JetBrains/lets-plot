688794a11eb07ea9dddae4897a3b5afa2e11ec44
Ivan Kupriyanov <ikupriyanov@horis.ru>
2019-11-19 16:24:24 +0300

    scale_x_datetime demo

---
+++ plot-demo/src/commonMain/kotlin/jetbrains/datalore/plotDemo/model/scale/DateTimeScaleX.kt
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2019. JetBrains s.r.o.
+ * Use of this source code is governed by the MIT license that can be found in the LICENSE file.
+ */
+
+package jetbrains.datalore.plotDemo.model.scale
+
+import jetbrains.datalore.base.datetime.Date
+import jetbrains.datalore.base.datetime.DateTime
+import jetbrains.datalore.base.datetime.Month
+import jetbrains.datalore.base.datetime.tz.TimeZone.Companion.UTC
+import jetbrains.datalore.plot.parsePlotSpec
+import jetbrains.datalore.plotDemo.model.PlotConfigDemoBase
+import kotlin.random.Random
+
+open class DateTimeScaleX : PlotConfigDemoBase() {
+    fun plotSpecList(): List<Map<String, Any>> {
+        return listOf(
+            days()
+        )
+    }
+
+    companion object {
+        private const val second = 1000.0
+        private const val minute = 60.0 * second
+        private const val hour = 60.0 * minute
+        private const val day = 24.0 * hour
+        private val instant = UTC.toInstant(DateTime(Date(1, Month.FEBRUARY, 2003)))
+
+        fun days(): Map<String, Any> {
+            val n = 30
+
+            val rnd = Random(0)
+            val time = (0..n).map { instant.timeSinceEpoch + it * day }.joinToString()
+            val values = (0..n).map { rnd.nextDouble(0.0, 20.0) }.joinToString()
+            val data = "   {" +
+                    "      'time': [$time]," +
+                    "      'values': [$values]" +
+                    "   }"
+            val spec = "{" +
+                    "   'kind': 'plot'," +
+                    "   'data': " + data +
+                    "           ," +
+                    "   'scales': [" +
+                    "               {" +
+                    "                 'name': 'Time(day)'," +
+                    "                 'aesthetic': 'x'," +
+                    "                 'datetime': true" +
+                    "               }" +
+                    "             ]," +
+                    "   'mapping': {" +
+                    "             'x': 'time'," +
+                    "             'y': 'values'" +
+                    "           }," +
+                    "   'layers': [" +
+                    "               {" +
+                    "                 'geom': 'line'" +
+                    "               }" +
+                    "           ]" +
+                    "}"
+
+            return parsePlotSpec(spec)
+        }
+    }
+}
