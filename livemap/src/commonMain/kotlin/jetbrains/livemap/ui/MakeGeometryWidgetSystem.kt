fdac9078527e448770229e310e86710df811a819
Ivan Seleznev <iseleznev@horis.ru>
2019-11-21 16:03:25 +0300

    Fixed MakeGeometryWidgetSystem.

--- livemap/src/commonMain/kotlin/jetbrains/livemap/ui/MakeGeometryWidgetSystem.kt
+++ livemap/src/commonMain/kotlin/jetbrains/livemap/ui/MakeGeometryWidgetSystem.kt
@@ -14,9 +14,12 @@ import jetbrains.livemap.api.geometry
 import jetbrains.livemap.camera.Viewport
 import jetbrains.livemap.core.animation.Animations.AnimationBuilder
 import jetbrains.livemap.core.ecs.EcsComponentManager
+import jetbrains.livemap.core.ecs.EcsEntity
+import jetbrains.livemap.core.input.InputMouseEvent
 import jetbrains.livemap.core.input.MouseInputComponent
 import jetbrains.livemap.entities.Entities.MapEntityFactory
 import jetbrains.livemap.entities.rendering.LayerEntitiesComponent
+import jetbrains.livemap.projections.LonLatPoint
 import jetbrains.livemap.projections.MapProjection
 import jetbrains.livemap.projections.toClientPoint
 
@@ -25,41 +28,62 @@ class MakeGeometryWidgetSystem(
     private val myMapProjection: MapProjection,
     private val myViewport: Viewport
 ) : LiveMapSystem(myComponentManager) {
+
     override fun updateImpl(context: LiveMapContext, dt: Double) {
-        myComponentManager.tryGetSingletonEntity(WIDGET_COMPONENTS)?.let { widgetLayer ->
-            val input = widgetLayer.get<MouseInputComponent>()
-            input.click?.let {
-                if (!it.isStopped) {
-                    val lonlat = it.location
+
+        getWidgetLayer()?.let { widgetLayer ->
+
+            widgetLayer.click()?.let { event ->
+                if (!event.isStopped) {
+                    event.location
                         .toClientPoint()
                         .run(myViewport::getMapCoord)
                         .run(myMapProjection::invert)
-                    val points = widgetLayer.get<MakeGeometryWidgetComponent>().points
-                    val factory = MapEntityFactory(widgetLayer)
-
-                    PointBuilder(factory, myMapProjection)
-                        .apply {
-                            point = lonlat
-                            strokeColor = parseHex("#cc7a00")
-                            shape = 20
+                        .let { point ->
+                            createVisualEntities(point, widgetLayer)
+                            widgetLayer.add(point)
                         }
-                        .build(pointScaling = false, animationBuilder = AnimationBuilder(500.0))
-
-                    if (points.count() > 0) {
-                        PathBuilder(factory, myMapProjection)
-                            .apply {
-                                geometry(listOf(points.last(), lonlat), false)
-                                strokeColor = parseHex("#cc7a00")
-                                strokeWidth = 1.5
-                            }
-                            .build()
-                    }
-
-                    points.add(lonlat)
                 }
             }
         }
     }
+
+    private fun createVisualEntities(lonlat: LonLatPoint, widgetLayer: EcsEntity) {
+        val factory = MapEntityFactory(widgetLayer)
+
+        PointBuilder(factory, myMapProjection)
+            .apply {
+                point = lonlat
+                strokeColor = DARK_ORANGE
+                shape = 20
+            }
+            .build(pointScaling = false, animationBuilder = AnimationBuilder(500.0))
+
+        if (widgetLayer.count() > 0) {
+            PathBuilder(factory, myMapProjection)
+                .apply {
+                    geometry(listOf(widgetLayer.last(), lonlat), false)
+                    strokeColor = DARK_ORANGE
+                    strokeWidth = 1.5
+                }
+                .build()
+        }
+    }
+
+    private fun getWidgetLayer(): EcsEntity? =
+        myComponentManager.tryGetSingletonEntity(WIDGET_COMPONENTS)
+
+    private fun EcsEntity.click(): InputMouseEvent? =
+        get<MouseInputComponent>().click
+
+    private fun EcsEntity.count(): Int =
+        get<MakeGeometryWidgetComponent>().points.count()
+
+    private fun EcsEntity.last(): LonLatPoint =
+        get<MakeGeometryWidgetComponent>().points.last()
+
+    private fun EcsEntity.add(point: LonLatPoint) =
+        get<MakeGeometryWidgetComponent>().points.add(point)
 
     companion object {
         val WIDGET_COMPONENTS = listOf(
@@ -67,6 +91,8 @@ class MakeGeometryWidgetSystem(
             MouseInputComponent::class,
             MakeGeometryWidgetComponent::class
         )
+
+        val DARK_ORANGE = parseHex("#cc7a00")
     }
 }
 
