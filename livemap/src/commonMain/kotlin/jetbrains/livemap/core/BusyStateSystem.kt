68b49153f8d40bccadfd2edb5fe823be96840f97
Ivan Kupriyanov <ikupriyanov@horis.ru>
2019-12-30 18:26:57 +0300

    Add graphical marker

--- livemap/src/commonMain/kotlin/jetbrains/livemap/core/BusyStateSystem.kt
+++ livemap/src/commonMain/kotlin/jetbrains/livemap/core/BusyStateSystem.kt
@@ -12,7 +12,9 @@ import jetbrains.livemap.core.BusyStateSystem.EntitesState.NOT_BUSY
 import jetbrains.livemap.core.BusyStateSystem.MarkerState.NOT_SHOWING
 import jetbrains.livemap.core.BusyStateSystem.MarkerState.SHOWING
 import jetbrains.livemap.core.ecs.*
-import jetbrains.livemap.core.rendering.primitives.Text
+import jetbrains.livemap.core.rendering.primitives.Circle
+import jetbrains.livemap.core.rendering.primitives.Frame
+import jetbrains.livemap.core.rendering.primitives.RenderBox
 import jetbrains.livemap.ui.UiService
 
 /**
@@ -32,6 +34,27 @@ class BusyStateSystem(
     componentManager: EcsComponentManager,
     private val uiService: UiService
 ) : AbstractSystem<EcsContext>(componentManager) {
+    private lateinit var spinnerGraphics: RenderBox
+    private var spinnerEntity: EcsEntity? = null
+
+    override fun initImpl(context: EcsContext) {
+        val pos = DoubleVector(18.0, 245.0)
+        val size = 16.0
+        val dim = DoubleVector(size, size)
+
+
+        spinnerGraphics =
+            Frame(pos, listOf(
+                Circle().apply {
+                    origin = DoubleVector.ZERO
+                    dimension = dim
+                    fillColor = Color.TRANSPARENT
+                    strokeColor = Color.BLACK
+                    strokeWidth = 2.0
+                }
+            ))
+    }
+
     override fun updateImpl(context: EcsContext, dt: Double) {
         val entitiesState = BUSY.takeIf { componentManager.count<BusyStateComponent>() > 0 } ?: NOT_BUSY
         val markerState = SHOWING.takeIf { componentManager.count<BusyMarkerComponent>() > 0 } ?: NOT_SHOWING
@@ -40,19 +63,10 @@ class BusyStateSystem(
             Pair(BUSY, SHOWING) -> {}
             Pair(NOT_BUSY, NOT_SHOWING) -> {}
             Pair(NOT_BUSY, SHOWING) -> {
-                println("Stop to be busy")
-                getSingletonEntity<BusyMarkerComponent>().remove()
+                spinnerEntity!!.remove()
             }
             Pair(BUSY, NOT_SHOWING) -> {
-                println("Start to be busy")
-                uiService.addRenderable(
-                    Text().apply {
-                        fontHeight = 15.0
-                        origin = DoubleVector(50.0, 250.0)
-                        text = listOf("Busy")
-                        color = Color.BLACK
-                    }
-                , "ui_busy_marker").addComponents { +BusyMarkerComponent() }
+                spinnerEntity = uiService.addRenderable(spinnerGraphics, "ui_busy_marker").addComponents { +BusyMarkerComponent() }
             }
         }
     }
