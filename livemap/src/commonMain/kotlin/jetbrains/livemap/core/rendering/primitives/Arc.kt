9138b001741a1737c5785b0023b324280c867905
Ivan Seleznev <iseleznev@horis.ru>
2020-02-14 18:07:26 +0300

    New busy spinner.

--- livemap/src/commonMain/kotlin/jetbrains/livemap/core/rendering/primitives/Arc.kt
+++ livemap/src/commonMain/kotlin/jetbrains/livemap/core/rendering/primitives/Arc.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019. JetBrains s.r.o.
+ * Copyright (c) 2020. JetBrains s.r.o.
  * Use of this source code is governed by the MIT license that can be found in the LICENSE file.
  */
 
@@ -10,18 +10,18 @@ import jetbrains.datalore.base.values.Color
 import jetbrains.datalore.vis.canvas.Context2d
 import kotlin.math.PI
 
-class Circle : RenderBox {
+class Arc : RenderBox {
     override var origin: DoubleVector = DoubleVector.ZERO
-    set(value) {
-        field = value
-        update()
-    }
+        set(value) {
+            field = value
+            update()
+        }
 
     override var dimension: DoubleVector = DoubleVector.ZERO
-    set(value) {
-        field = value
-        update()
-    }
+        set(value) {
+            field = value
+            update()
+        }
 
     private var center: DoubleVector = DoubleVector.ZERO
 
@@ -31,15 +31,13 @@ class Circle : RenderBox {
 
     var strokeColor: Color? = null
     var strokeWidth: Double? = null
-
-    var fillColor: Color? = null
+    var angle = PI / 2
+    var startAngle = 0.0
 
 
     override fun render(ctx: Context2d) {
         ctx.beginPath()
-        ctx.arc(center.x, center.y, dimension.x / 2.0, 0.0, 2 * PI)
-        fillColor?.let { ctx.setFillStyle(it.toCssColor()) }
-        ctx.fill()
+        ctx.arc(center.x, center.y, dimension.x / 2.0, startAngle, startAngle + angle)
 
         strokeWidth?.let { ctx.setLineWidth(it) }
         strokeColor?.let { ctx.setStrokeStyle(it.toCssColor()) }
