20fe8f7369d37bb1b484ff6434b1aa0c32ba33c5
Ivan Seleznev <iseleznev@horis.ru>
2020-02-21 17:59:15 +0300

    New design of busy spinner.

--- livemap/src/commonMain/kotlin/jetbrains/livemap/core/BusyStateSystem.kt
+++ livemap/src/commonMain/kotlin/jetbrains/livemap/core/BusyStateSystem.kt
@@ -7,15 +7,17 @@ package jetbrains.livemap.core
 
 import jetbrains.datalore.base.geometry.DoubleVector
 import jetbrains.datalore.base.values.Color
-import jetbrains.livemap.core.BusyStateSystem.EntitesState.BUSY
-import jetbrains.livemap.core.BusyStateSystem.EntitesState.NOT_BUSY
+import jetbrains.livemap.core.BusyStateSystem.EntitiesState.BUSY
+import jetbrains.livemap.core.BusyStateSystem.EntitiesState.NOT_BUSY
 import jetbrains.livemap.core.BusyStateSystem.MarkerState.NOT_SHOWING
 import jetbrains.livemap.core.BusyStateSystem.MarkerState.SHOWING
 import jetbrains.livemap.core.ecs.*
+import jetbrains.livemap.core.rendering.primitives.Arc
 import jetbrains.livemap.core.rendering.primitives.Circle
 import jetbrains.livemap.core.rendering.primitives.Frame
 import jetbrains.livemap.core.rendering.primitives.RenderBox
 import jetbrains.livemap.ui.UiService
+import kotlin.math.PI
 
 /**
  * Entities with this component causes the map to show a spinner.
@@ -36,10 +38,12 @@ class BusyStateSystem(
 ) : AbstractSystem<EcsContext>(componentManager) {
     private lateinit var spinnerGraphics: RenderBox
     private var spinnerEntity: EcsEntity? = null
+    private var myStartAngle = 0.0
+    private var mySpinnerArc = Arc()
 
     override fun initImpl(context: EcsContext) {
-        val pos = DoubleVector(18.0, 245.0)
-        val size = 16.0
+        val pos = DoubleVector(14.0, 169.0)
+        val size = 26.0
         val dim = DoubleVector(size, size)
 
 
@@ -48,9 +52,25 @@ class BusyStateSystem(
                 Circle().apply {
                     origin = DoubleVector.ZERO
                     dimension = dim
+                    fillColor = Color.WHITE
+                    strokeColor = Color.LIGHT_GRAY
+                    strokeWidth = 1.0
+                },
+
+                Circle().apply {
+                    origin = DoubleVector(4.0, 4.0)
+                    dimension = DoubleVector(18.0, 18.0)
                     fillColor = Color.TRANSPARENT
-                    strokeColor = Color.BLACK
+                    strokeColor = Color.LIGHT_GRAY
                     strokeWidth = 2.0
+                },
+
+                mySpinnerArc.apply {
+                    origin = DoubleVector(4.0, 4.0)
+                    dimension = DoubleVector(18.0, 18.0)
+                    strokeColor = Color.parseHex("#70a7e3")
+                    strokeWidth = 2.0
+                    angle = PI / 4
                 }
             ))
     }
@@ -58,20 +78,23 @@ class BusyStateSystem(
     override fun updateImpl(context: EcsContext, dt: Double) {
         val entitiesState = BUSY.takeIf { componentManager.count<BusyStateComponent>() > 0 } ?: NOT_BUSY
         val markerState = SHOWING.takeIf { componentManager.count<BusyMarkerComponent>() > 0 } ?: NOT_SHOWING
+        myStartAngle += (2 * PI * dt) / 1000
 
         when (Pair(entitiesState, markerState)) {
-            Pair(BUSY, SHOWING) -> {}
+            Pair(BUSY, SHOWING) -> { mySpinnerArc.apply { startAngle = myStartAngle } }
             Pair(NOT_BUSY, NOT_SHOWING) -> {}
             Pair(NOT_BUSY, SHOWING) -> {
                 spinnerEntity!!.remove()
             }
             Pair(BUSY, NOT_SHOWING) -> {
-                spinnerEntity = uiService.addRenderable(spinnerGraphics, "ui_busy_marker").addComponents { +BusyMarkerComponent() }
+                spinnerEntity = uiService
+                    .addRenderable(spinnerGraphics, "ui_busy_marker")
+                    .add(BusyMarkerComponent())
             }
         }
     }
 
-    private enum class EntitesState {
+    private enum class EntitiesState {
         BUSY,
         NOT_BUSY
     }
