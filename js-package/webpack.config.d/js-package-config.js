c228b95ff9d8b11b977ee9b3d595f391e8a69fa8
Ivan Ryabov <iryabov@horis.ru>
2020-04-21 15:44:58 +0300

    Fix js build

--- js-package/webpack.config.d/js-package-config.js
+++ js-package/webpack.config.d/js-package-config.js
@@ -3,7 +3,10 @@ config.output.library = "LetsPlot";
 config.output.libraryTarget = "window";
 config.output.globalObject = "window";
 
+config.node = config.node || {};
+config.node.Buffer = false;
+
 const webpack = require('webpack');
 
 config.plugins = config.plugins || [];
-config.plugins.push(new webpack.IgnorePlugin(/^(ws|text-encoding|abort-controller)$/));
+config.plugins.push(new webpack.IgnorePlugin(/^(ws|text-encoding|abort-controller|buffer|node-fetch)$/));
