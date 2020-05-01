57f1dbedcfee23eda5d2f173a3856a7bba04ddd2
Ivan Kupriyanov <ikupriyanov@horis.ru>
2020-05-02 01:36:37 +0300

    Test for https://github.com/JetBrains/lets-plot/issues/124

---
+++ plot-builder-portable/src/jvmTest/kotlin/plot/builder/scale/ScaleProviderHelperTest.kt
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2020. JetBrains s.r.o.
+ * Use of this source code is governed by the MIT license that can be found in the LICENSE file.
+ */
+
+package jetbrains.datalore.plot.builder.scale
+
+import jetbrains.datalore.plot.base.Aes
+import jetbrains.datalore.plot.base.DataFrame
+import org.junit.Ignore
+import kotlin.test.Test
+
+class ScaleProviderHelperTest {
+
+    @Ignore("Temp to not break a build")
+    @Test()
+    fun nullValuesInDataFrame() {
+        val region = DataFrame.Variable("region")
+        val df = DataFrame.Builder()
+            .put(region, listOf("Europe", "Asia", null, "Australia"))
+            .build()
+
+        ScaleProviderHelper.createDefault(Aes.MAP_ID).createScale(df, region)
+    }
+}
