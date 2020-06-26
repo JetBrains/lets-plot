71222b81fc9401f18d1002677060851e74a75a49
Ivan Kupriyanov <ikupriyanov@horis.ru>
2020-06-27 00:10:22 +0300

    Cleanup GeomInteraction

--- plot-config-portable/src/commonMain/kotlin/jetbrains/datalore/plot/config/PlotConfigClientSideUtil.kt
+++ plot-config-portable/src/commonMain/kotlin/jetbrains/datalore/plot/config/PlotConfigClientSideUtil.kt
@@ -10,19 +10,12 @@ import jetbrains.datalore.plot.base.Aes
 import jetbrains.datalore.plot.base.DataFrame
 import jetbrains.datalore.plot.base.GeomKind
 import jetbrains.datalore.plot.base.data.DataFrameUtil.variables
-import jetbrains.datalore.plot.base.interact.GeomTargetLocator.LookupStrategy
-import jetbrains.datalore.plot.base.interact.ValueSource
 import jetbrains.datalore.plot.builder.GeomLayer
 import jetbrains.datalore.plot.builder.assemble.GeomLayerBuilder
 import jetbrains.datalore.plot.builder.assemble.GuideOptions
 import jetbrains.datalore.plot.builder.assemble.PlotAssembler
 import jetbrains.datalore.plot.builder.assemble.TypedScaleProviderMap
-import jetbrains.datalore.plot.builder.interact.GeomInteractionBuilder
-import jetbrains.datalore.plot.builder.interact.GeomInteractionBuilder.Companion.AREA_GEOM
-import jetbrains.datalore.plot.builder.interact.GeomInteractionBuilder.Companion.NON_AREA_GEOM
-import jetbrains.datalore.plot.builder.theme.Theme
-import jetbrains.datalore.plot.builder.tooltip.CompositeValue
-import jetbrains.datalore.plot.builder.tooltip.TooltipLineSpecification
+import jetbrains.datalore.plot.builder.interact.GeomInteraction
 
 object PlotConfigClientSideUtil {
     internal fun createGuideOptionsMap(scaleConfigs: List<ScaleConfig<*>>): Map<Aes<*>, GuideOptions> {
@@ -68,15 +61,16 @@ object PlotConfigClientSideUtil {
             val tileDataByLayer = layersDataByTile.next()
 
             val isMultilayer = tileDataByLayer.size > 1
+            val isLiveMap = cfg.layerConfigs.any { it.geomProto.geomKind == GeomKind.LIVE_MAP }
 
             for (layerIndex in tileDataByLayer.indices) {
                 checkState(layerBuilders.size >= layerIndex)
 
                 if (layerBuilders.size == layerIndex) {
                     val layerConfig = cfg.layerConfigs[layerIndex]
-                    val layerBuilder = createLayerBuilder(layerConfig, scaleProvidersMap)
-                    configGeomTargets(layerBuilder, layerConfig, isMultilayer, cfg.theme)
-                    layerBuilders.add(layerBuilder)
+                    val geomInteraction = GeomInteractionUtil.configGeomTargets(layerConfig, isMultilayer, isLiveMap, cfg.theme)
+
+                    layerBuilders.add(createLayerBuilder(layerConfig, scaleProvidersMap, geomInteraction))
                 }
 
                 val layerTileData = tileDataByLayer[layerIndex]
@@ -89,30 +83,10 @@ object PlotConfigClientSideUtil {
         return layersByTile
     }
 
-    private fun configGeomTargets(
-        layerBuilder: GeomLayerBuilder,
-        layerConfig: LayerConfig,
-        multilayer: Boolean,
-        theme: Theme
-    ) {
-        val axisWithoutTooltip = ArrayList<Aes<*>>()
-        if (!theme.axisX().showTooltip()) axisWithoutTooltip.add(Aes.X)
-        if (!theme.axisY().showTooltip()) axisWithoutTooltip.add(Aes.Y)
-
-        val geomInteraction = createGeomInteractionBuilder(
-            layerConfig,
-            axisWithoutTooltip,
-            multilayer
-        ).build()
-
-        layerBuilder
-            .locatorLookupSpec(geomInteraction.createLookupSpec())
-            .contextualMappingProvider(geomInteraction)
-     }
-
     private fun createLayerBuilder(
         layerConfig: LayerConfig,
-        scaleProvidersMap: TypedScaleProviderMap
+        scaleProvidersMap: TypedScaleProviderMap,
+        geomInteraction: GeomInteraction
     ): GeomLayerBuilder {
         val geomProvider = (layerConfig.geomProto as GeomProtoClientSide).geomProvider(layerConfig)
 
@@ -156,196 +130,11 @@ object PlotConfigClientSideUtil {
         }
 
         layerBuilder.disableLegend(layerConfig.isLegendDisabled)
+
+        layerBuilder
+            .locatorLookupSpec(geomInteraction.createLookupSpec())
+            .contextualMappingProvider(geomInteraction)
+
         return layerBuilder
-    }
-
-    internal fun createGeomInteractionBuilder(
-        renders: List<Aes<*>>,
-        geomKind: GeomKind,
-        statKind: StatKind,
-        multilayer: Boolean
-    ): GeomInteractionBuilder {
-
-        val builder = initGeomInteractionBuilder(renders, geomKind, statKind)
-
-        if (multilayer) {
-            // Only these kinds of geoms should be switched to NEAREST XY strategy on a multilayer plot.
-            // Rect, histogram and other column alike geoms should not switch searching strategy, otherwise
-            // tooltips behaviour becomes unexpected(histogram shows tooltip when cursor is close enough,
-            // but not above a column).
-            if (listOf(GeomKind.LINE, GeomKind.DENSITY, GeomKind.AREA, GeomKind.FREQPOLY).contains(geomKind)) {
-                builder.multilayerLookupStrategy()
-            } else if (statKind === StatKind.SMOOTH) {
-                when (geomKind) {
-                    GeomKind.POINT, GeomKind.CONTOUR -> builder.multilayerLookupStrategy()
-
-                    else -> {
-                    }
-                }
-            }
-        }
-
-        return builder
-    }
-
-    internal fun createGeomInteractionBuilder(
-        layerConfig: LayerConfig,
-        axisWithoutTooltip: List<Aes<*>>,
-        multilayer: Boolean
-    ): GeomInteractionBuilder {
-
-        val builder = createGeomInteractionBuilder(
-            layerConfig.geomProto.renders(),
-            layerConfig.geomProto.geomKind,
-            layerConfig.statKind,
-            multilayer
-        )
-
-        // Set aes lists
-        val hiddenAesList = createHiddenAesList(layerConfig.geomProto.geomKind) + axisWithoutTooltip
-        val axisAes = createAxisAesList(builder, layerConfig.geomProto.geomKind) - hiddenAesList
-        val aesList = createTooltipAesList(layerConfig, builder.getAxisFromFunctionKind) - hiddenAesList
-
-        builder.axisAes(axisAes)
-            .tooltipAes(aesList)
-            .tooltipValueSources(createTooltipValueSourceList(layerConfig.tooltips))
-
-        return builder
-    }
-
-    private fun createHiddenAesList(geomKind: GeomKind): List<Aes<*>> {
-        return when (geomKind) {
-            GeomKind.BOX_PLOT -> listOf(Aes.Y)
-            GeomKind.RECT -> listOf(Aes.XMIN, Aes.YMIN, Aes.XMAX, Aes.YMAX)
-            else -> emptyList()
-        }
-    }
-
-    private fun createAxisAesListByGeomKind(geomKind: GeomKind): List<Aes<*>> {
-        return if (geomKind === GeomKind.SMOOTH)
-            listOf(Aes.X)
-        else
-            emptyList()
-    }
-
-    private fun createAxisAesList(geomBuilder: GeomInteractionBuilder, geomKind: GeomKind): List<Aes<*>> {
-        return when {
-            !geomBuilder.isAxisTooltipEnabled -> emptyList()
-            else -> {
-                val axisAesFromConfig = createAxisAesListByGeomKind(geomKind)
-                if (axisAesFromConfig.isNotEmpty())
-                    axisAesFromConfig
-                else
-                    geomBuilder.getAxisFromFunctionKind
-            }
-        }
-    }
-
-    private fun createTooltipAesList(
-        layerConfig: LayerConfig,
-        axisAes: List<Aes<*>>
-    ): List<Aes<*>> {
-
-        fun isVariableContinuous(aes: Aes<*>): Boolean {
-            val scale = layerConfig.getScaleForAes(aes)
-            return scale?.isContinuous ?: false
-        }
-
-        // remove axis mapping: if aes and axis are bound to the same data
-        val aesListForTooltip = ArrayList(layerConfig.geomProto.renders() - axisAes)
-        for (aes in axisAes) {
-            if (isVariableContinuous(aes)) {
-                val axisVariable = layerConfig.getVariableForAes(aes)
-                aesListForTooltip.removeAll { layerConfig.getVariableForAes(it) == axisVariable }
-            }
-        }
-
-        // remove auto generated mappings
-        val autoGenerated = listOf<String>()
-        aesListForTooltip.removeAll { layerConfig.getScaleForAes(it)?.name in autoGenerated }
-
-        return aesListForTooltip
-    }
-
-    private fun createTooltipValueSourceList(tooltipLineSpecifications: List<TooltipLineSpecification>?): List<ValueSource>? {
-        if (tooltipLineSpecifications == null) {
-            return null
-        }
-
-        val tooltipValueSourceList = mutableListOf<ValueSource>()
-        tooltipLineSpecifications.forEach { tooltipLineSpecification ->
-            if (tooltipLineSpecification.data.size == 1) {
-                tooltipValueSourceList.add(tooltipLineSpecification.data.single())
-            } else {
-                tooltipValueSourceList.add(
-                    CompositeValue(
-                        tooltipLineSpecification.data,
-                        tooltipLineSpecification.label,
-                        tooltipLineSpecification.format
-                    )
-                )
-            }
-        }
-        return tooltipValueSourceList
-    }
-
-    private fun initGeomInteractionBuilder(
-        renders: List<Aes<*>>,
-        geomKind: GeomKind,
-        statKind: StatKind
-    ): GeomInteractionBuilder {
-        val builder = GeomInteractionBuilder(renders)
-        if (statKind === StatKind.SMOOTH) {
-            when (geomKind) {
-                GeomKind.POINT,
-                GeomKind.CONTOUR -> return builder.univariateFunction(LookupStrategy.NEAREST)
-                else -> {
-                }
-            }
-        }
-
-        when (geomKind) {
-            GeomKind.DENSITY,
-            GeomKind.FREQPOLY,
-            GeomKind.HISTOGRAM,
-            GeomKind.LINE,
-            GeomKind.AREA,
-            GeomKind.BAR,
-            GeomKind.ERROR_BAR,
-            GeomKind.CROSS_BAR,
-            GeomKind.POINT_RANGE,
-            GeomKind.LINE_RANGE -> return builder.univariateFunction(LookupStrategy.HOVER)
-            GeomKind.BOX_PLOT -> return builder.univariateFunction(LookupStrategy.HOVER)
-            GeomKind.V_LINE -> return builder.univariateFunction(LookupStrategy.HOVER)
-            GeomKind.SMOOTH,
-            GeomKind.POINT,
-            GeomKind.CONTOUR,
-            GeomKind.RIBBON,
-            GeomKind.DENSITY2D -> {
-                return builder.bivariateFunction(NON_AREA_GEOM)
-            }
-            GeomKind.PATH -> {
-                when (statKind) {
-                    StatKind.CONTOUR, StatKind.CONTOURF, StatKind.DENSITY2D -> return builder.bivariateFunction(
-                        NON_AREA_GEOM
-                    )
-                    else -> {
-                    }
-                }
-                return builder.bivariateFunction(AREA_GEOM)
-            }
-            GeomKind.H_LINE,
-            GeomKind.TILE,
-            GeomKind.DENSITY2DF,
-            GeomKind.CONTOURF,
-            GeomKind.POLYGON,
-            GeomKind.BIN_2D,
-            GeomKind.MAP -> return builder.bivariateFunction(AREA_GEOM)
-            GeomKind.RECT -> return builder.bivariateFunction(AREA_GEOM)
-
-            GeomKind.LIVE_MAP -> return builder.bivariateFunction(NON_AREA_GEOM)
-
-            else -> return builder.none()
-        }
     }
 }
