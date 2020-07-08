50b97213744bce1287c4a84a6928a08184d62908
Ivan Kupriyanov <ikupriyanov@horis.ru>
2020-07-08 16:38:15 +0300

    Fix JFX initialization in livemap demo

--- livemap-demo/src/jvmJfxPlotMain/kotlin/jetbrains/livemap/demo/LiveMapJfx.kt
+++ livemap-demo/src/jvmJfxPlotMain/kotlin/jetbrains/livemap/demo/LiveMapJfx.kt
@@ -5,13 +5,25 @@
 
 package jetbrains.livemap.demo
 
+import javafx.embed.swing.JFXPanel
 import jetbrains.datalore.plot.builder.presentation.Style
 import jetbrains.datalore.vis.demoUtils.SceneMapperDemoFactory
 import jetbrains.livemap.plotDemo.LiveMap
+import java.util.concurrent.CountDownLatch
+import javax.swing.SwingUtilities
+
 
 object LiveMapJfx {
     @JvmStatic
     fun main(args: Array<String>) {
+
+        val latch = CountDownLatch(1)
+        SwingUtilities.invokeLater {
+            JFXPanel() // initializes JavaFX environment
+            latch.countDown()
+        }
+        latch.await()
+
         with(LiveMap()) {
             @Suppress("UNCHECKED_CAST")
             val plotSpecList = plotSpecList() as List<MutableMap<String, Any>>
