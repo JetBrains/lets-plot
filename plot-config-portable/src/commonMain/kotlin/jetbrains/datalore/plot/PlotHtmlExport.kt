60b6c917a654c0a82545ef88afe3b785439182a4
Igor Alshannikov <Igor.Alshannikov@jetbrains.com>
2020-08-03 13:23:19 -0400

    PlotHtmlExport: remove unnecessary overload.

--- plot-config-portable/src/commonMain/kotlin/jetbrains/datalore/plot/PlotHtmlExport.kt
+++ plot-config-portable/src/commonMain/kotlin/jetbrains/datalore/plot/PlotHtmlExport.kt
@@ -14,20 +14,6 @@ object PlotHtmlExport {
      * @param plotSpec Raw specification of a plot or GGBunch.
      * @param version Version of Lets-plot JS library.
      * @param iFrame Whether to wrap HTML in IFrame
-     */
-    @Suppress("MemberVisibilityCanBePrivate")
-    fun buildHtmlFromRawSpecs(
-        plotSpec: MutableMap<String, Any>,
-        version: String = "latest",
-        iFrame: Boolean = false
-    ): String {
-        return buildHtmlFromRawSpecs(plotSpec, version, iFrame, null)
-    }
-
-    /**
-     * @param plotSpec Raw specification of a plot or GGBunch.
-     * @param version Version of Lets-plot JS library.
-     * @param iFrame Whether to wrap HTML in IFrame
      * @param plotSize Desired plot size. Has no effect on GGBunch.
      */
     @Suppress("MemberVisibilityCanBePrivate")
@@ -35,7 +21,7 @@ object PlotHtmlExport {
         plotSpec: MutableMap<String, Any>,
         version: String = "latest",
         iFrame: Boolean = false,
-        plotSize: DoubleVector?
+        plotSize: DoubleVector? = null
     ): String {
 
         val configureHtml = PlotHtmlHelper.getStaticConfigureHtml(PlotHtmlHelper.scriptUrl(version))
