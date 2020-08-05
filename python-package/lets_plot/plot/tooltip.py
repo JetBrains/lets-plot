7982043596a7627218d7c3591e0eaa6ebe05c756
Olga Larionova <olarionova@horis.ru>
2020-08-05 19:23:50 +0300

    Add 'format' and 'line' to tooltip.py. Update tooltip_config.ipynb.

--- python-package/lets_plot/plot/tooltip.py
+++ python-package/lets_plot/plot/tooltip.py
@@ -3,6 +3,7 @@
 #  Use of this source code is governed by the MIT license that can be found in the LICENSE file.
 
 from .core import FeatureSpec
+from typing import List, Dict
 
 #
 # Tooltips
@@ -32,7 +33,11 @@ def tooltip_line(value=None, label=None, format=None):
     return dict(value=value, label=label, format=format)
 
 
-def layer_tooltips(lines=None):
+# def layer_tooltips(lines=None):
+#    return FeatureSpec('tooltips', name=None, lines=lines)
+
+
+class layer_tooltips(FeatureSpec):
     """
     Define tooltips.
 
@@ -43,8 +48,12 @@ def layer_tooltips(lines=None):
         using the function 'tooltip_line':
             tooltip_line(value = <variable_name>, label = <label_text>, format = <format>)
 
-             - lines = None - default tooltips
-             - lines = [] - no tooltips
+        lines = None - default tooltips
+        lines = [] - no tooltips
+
+    TODO: other parameters:
+        - tooltip_formats/tooltip_lines
+        - format/line function
 
     Returns
     -------
@@ -52,12 +61,44 @@ def layer_tooltips(lines=None):
 
     Examples
     ---------
+    Variant 1
+
     >>> import pandas as pd
     >>> from lets_plot import *
     >>> mpg_url = 'https://vincentarelbundock.github.io/Rdatasets/csv/ggplot2/mpg.csv'
     >>> mpg = pd.read_csv(mpg_url)
-    >>> p = ggplot(mpg, aes(x='displ', y='hwy')) + geom_point(aes(color='cty', shape='drv'),
-    ... tooltips=layer_tooltips(lines=[tooltip_line(value='color', label='city miles per gallon:')]))
-    """
+    >>> p = ggplot(mpg, aes(x='displ', y='hwy')) \
+    >>>   + geom_point(aes(color='cty', shape='drv'), \
+    >>>                tooltips=layer_tooltips(lines=[tooltip_line('$color', label = '', format='{.1f} (miles per gallon)')]))
 
-    return FeatureSpec('tooltips', name=None, lines=lines)
+    Variant 2
+    ---------
+    >>> p = ggplot(mpg, aes(x='displ', y='hwy')) \
+    >>>   + geom_point(aes(color='cty', shape='drv'), \
+    >>>                tooltip_formats={'$color': '.1f'},\
+    >>>                tooltip_lines=['$color (miles per gallon)'])
+
+    Variant 3
+    ---------
+    >>> p = ggplot(mpg, aes(x='displ', y='hwy')) \
+    >>>   + geom_point(aes(color='cty', shape='drv'), \
+    >>>                tooltips=layer_tooltips().format({'$color':'.1f'})
+    >>>                                         .line('$color (miles per gallon)'))
+   """
+
+    def __init__(self, lines=None):
+        self.tooltip_formats: Dict = {}
+        self.tooltip_lines: List = []
+        super().__init__('tooltips',
+                         name=None,
+                         lines=lines,
+                         tooltip_lines=self.tooltip_lines,
+                         tooltip_formats=self.tooltip_formats)
+
+    def format(self, format):
+        self.tooltip_formats.update(format)
+        return self
+
+    def line(self, value):
+        self.tooltip_lines.append(value)
+        return self
