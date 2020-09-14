9fcd054b4e955fd5792ca5b30a15c6f0d32c8444
Ilya Krukov <ikrukov@horis.ru>
2020-09-14 12:54:05 +0300

    TextGeom - approximate maxTextWidth

--- plot-base-portable/src/commonMain/kotlin/jetbrains/datalore/plot/base/geom/TextGeom.kt
+++ plot-base-portable/src/commonMain/kotlin/jetbrains/datalore/plot/base/geom/TextGeom.kt
@@ -8,24 +8,29 @@ package jetbrains.datalore.plot.base.geom
 import jetbrains.datalore.base.gcommon.base.Strings
 import jetbrains.datalore.base.numberFormat.NumberFormat
 import jetbrains.datalore.plot.base.*
+import jetbrains.datalore.plot.base.aes.AesScaling
 import jetbrains.datalore.plot.base.geom.util.GeomHelper
 import jetbrains.datalore.plot.base.render.LegendKeyElementFactory
 import jetbrains.datalore.plot.base.render.SvgRoot
 import jetbrains.datalore.plot.base.render.svg.TextLabel
 import jetbrains.datalore.plot.common.data.SeriesUtil
-import jetbrains.datalore.vis.svg.SvgNodeContainer
-import jetbrains.datalore.vis.svg.SvgSvgElement
 
 class TextGeom : GeomBase() {
     var formatter: NumberFormat? = null
     var naValue = DEF_NA_VALUE
     var sizeUnit: String? = null
-    var sizeUnitScale: Double? = null
+    private var sizeUnitScale: Double? = null
 
     override val legendKeyElementFactory: LegendKeyElementFactory
         get() = TextLegendKeyElementFactory()
 
-    override fun buildIntern(root: SvgRoot, aesthetics: Aesthetics, pos: PositionAdjustment, coord: CoordinateSystem, ctx: GeomContext) {
+    override fun buildIntern(
+        root: SvgRoot,
+        aesthetics: Aesthetics,
+        pos: PositionAdjustment,
+        coord: CoordinateSystem,
+        ctx: GeomContext
+    ) {
         val helper = GeomHelper(pos, coord, ctx)
         for (p in aesthetics.dataPoints()) {
             val x = p.x()
@@ -34,7 +39,7 @@ class TextGeom : GeomBase() {
             if (SeriesUtil.allFinite(x, y) && !Strings.isNullOrEmpty(text)) {
                 val label = TextLabel(text)
                 val scale = getScale(ctx, p)
-                GeomHelper.decorate(label, p, scale )
+                GeomHelper.decorate(label, p, scale)
 
                 val loc = helper.toClient(x, y, p)
                 label.moveTo(loc)
@@ -43,20 +48,23 @@ class TextGeom : GeomBase() {
         }
     }
 
-    private fun getScale(ctx: GeomContext, p: DataPointAesthetics) : Double {
+    private fun approximateMaxTextWidth(p: DataPointAesthetics): Double {
+        val testString = toString(TEST_VAL)
+        val fontSize = AesScaling.textSize(p)
+
+        return testString.length * fontSize * TEXT_WIDTH_NORM
+    }
+
+    private fun getScale(ctx: GeomContext, p: DataPointAesthetics): Double {
         sizeUnitScale?.let { return sizeUnitScale!! }
         sizeUnitScale = 1.0
 
         sizeUnit?.let {
             val aes = GeomHelper.getSizeUnitAes(sizeUnit!!)
-            val testString = toString(testValue)
-            val label = TextLabel(testString)
-            GeomHelper.decorate(label, p, 1.0 )
             val unitRes = ctx.getUnitResolution(aes)
-//            val textWidth = label.computedTextLength
-            val textWidth = 50.0
+            val maxTextWidth = approximateMaxTextWidth(p)
 
-            sizeUnitScale = unitRes / textWidth
+            sizeUnitScale = unitRes / maxTextWidth
         }
 
         return sizeUnitScale!!
@@ -75,9 +83,10 @@ class TextGeom : GeomBase() {
     }
 
     companion object {
-        val DEF_NA_VALUE = "n/a"
-        val HANDLES_GROUPS = false
-        const val testValue = -9.99999
+        const val DEF_NA_VALUE = "n/a"
+        const val HANDLES_GROUPS = false
+        const val TEST_VAL = -9.99999
+        const val TEXT_WIDTH_NORM = 50.0
     }
 }
 
