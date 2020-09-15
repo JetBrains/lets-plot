04c65728f6535ce44474a66e67c02baf7ea4c5c1
Ilya Krukov <ikrukov@horis.ru>
2020-09-15 09:59:33 +0300

    corr_matrix builder

--- python-package/lets_plot/plot/_corr_matrix.py
+++ python-package/lets_plot/plot/_corr_matrix.py
@@ -4,92 +4,91 @@
 
 """Correlation matrix implementation module"""
 
-from . import ggplot
-from .geom import geom_point, geom_text
-from .scale import scale_y_discrete_reversed, scale_color_gradient2, scale_size
+from .core import PlotSpec
+from .geom import geom_point, geom_text, geom_tile
+from .scale import scale_y_discrete_reversed, scale_color_gradient2
 from .scale_identity import scale_size_identity
 from .coord import coord_fixed
 from .theme_ import theme, element_blank
 from .tooltip import layer_tooltips
 
-__all__ = ['corr_matrix_point', 'corr_matrix_text', 'corr_matrix']
+__all__ = ['corr_matrix']
 
 
-def common_corr_spec(data):
-    res = ggplot(data=data)
-    res += theme(axis_title=element_blank(), legend_title=element_blank())
-    res += coord_fixed()
-    res += scale_y_discrete_reversed()
-    res += scale_size_identity(name="")
-    res += scale_color_gradient2(name='Correlation',
-                                 low='blue', mid='light_gray', high='red',
-                                 limits=[-1.0, 1.0])
-
-    return res
+def to_type(tp):
+    return tp if tp else 'full'
 
 
-def corr_tooltips_spec(format):
-    return layer_tooltips().format({'$color': format}).line('Corr|$color')
+def to_format(fmt):
+    return fmt if fmt else '.2f'
 
 
-def corr_matrix_point(data, show_legend=None, type=None, fill_diagonal=None, format=None, **other):
-    type = type if type else 'full'
-    format = format if format else '.2f'
-
-    res = common_corr_spec(data)
-    res += geom_point(stat='corr', show_legend=show_legend, size_unit='x',
-                      tooltips=corr_tooltips_spec(format),
-                      type=type, fill_diagonal=fill_diagonal, **other)
-    res += scale_size_identity()
-
-    return res
+def to_method(method):
+    return method if method else 'pearson'
 
 
-def corr_matrix_text(data, show_legend=None, type=None, fill_diagonal=None, format=None, **other):
-    type = type if type else 'full'
-    format = format if format else '.2f'
+def add_common_params(plot):
+    plot += theme(axis_title=element_blank(), legend_title=element_blank())
+    plot += coord_fixed()
+    plot += scale_y_discrete_reversed()
+    plot += scale_size_identity(name="")
+    plot += scale_color_gradient2(name='Correlation',
+                                  low='blue', mid='light_gray', high='red',
+                                  limits=[-1.0, 1.0])
 
-    res = common_corr_spec(data)
-    res += geom_text(stat='corr', show_legend=show_legend,  size_unit='x', label_format=format,
-                     tooltips=corr_tooltips_spec(format),
-                     type=type, fill_diagonal=fill_diagonal, **other)
-
-    return res
-
-
-def corr_matrix(data, show_legend=None,
-                draw=None, type=None, fill_diagonal=None,
-                format=None, text_size=None, **other):
-    """
-    Returns correlation matrix plot.
-
-    data : dictionary or pandas DataFrame, required.
+    return plot
 
 
-    """
-    # draw - "points", "text", "mixed"
-    draw = draw if draw else "points"
-    type = type if type else 'full'
-    format = format if format else '.2f'
+class corr_matrix():
+    def __init__(self, data, show_legend=None, format=None, method=None):
+        self.data = data
+        self.show_legend = show_legend
+        self.format = to_format(format)
+        self.method = to_method(method)
+        self.layers = []
 
-    res = common_corr_spec(data)
+    def get_format(self, format):
+        return format if format else self.format
 
-    if draw == "points":
-        res += geom_point(stat='corr', show_legend=show_legend, size_unit='x',
-                          tooltips=corr_tooltips_spec(format),
-                          type=type, fill_diagonal=fill_diagonal, **other)
-    elif draw == "text":
-        res += geom_text(stat='corr', show_legend=show_legend, label_format=format,
-                         tooltips=corr_tooltips_spec(format),
-                         type=type, fill_diagonal=fill_diagonal, size=text_size, **other)
-    elif draw == "mixed":
-        res += geom_point(stat='corr', show_legend=show_legend,  size_unit='x',
-                          tooltips=corr_tooltips_spec(format),
-                          type="upper", fill_diagonal=True, **other)
+    def tooltip_spec(self, format):
+        return layer_tooltips().format({'$color': self.get_format(format)}).line('Corr|$color')
 
-        res += geom_text(stat='corr', show_legend=show_legend,  size_unit='x',
-                         type="lower", fill_diagonal=False,
-                         label_format=format, size=text_size, **other)
+    def points(self, type=None, fill_diagonal=None, tooltip_format=None):
+        points = geom_point(stat='corr', show_legend=self.show_legend, size_unit='x',
+                            tooltips=self.tooltip_spec(tooltip_format),
+                            type=to_type(type), fill_diagonal=fill_diagonal,
+                            method=self.method)
 
+        self.layers.append(points)
 
-    return res
+        return self
+
+    def text(self, type=None, fill_diagonal=None, text_format=None, text_size=None):
+
+        if text_size:
+            kwargs = {'size': text_size }
+        else:
+            kwargs = {'size_unit': 'x'}
+
+        text = geom_text(stat='corr', show_legend=self.show_legend,
+                         label_format=self.get_format(text_format),
+                         type=to_type(type), fill_diagonal=fill_diagonal,
+                         method=self.method, **kwargs )
+
+        self.layers.append(text)
+
+        return self
+
+    def tiles(self, type=None, fill_diagonal=None, tooltip_format=None):
+        tiles = geom_tile(stat='corr', show_legend=self.show_legend, size_unit='x',
+                          tooltips=self.tooltip_spec(tooltip_format),
+                          type=to_type(type), fill_diagonal=fill_diagonal,
+                          method=self.method)
+
+        self.layers.append(tiles)
+
+        return self
+
+    def build(self):
+        plot = PlotSpec(self.data, mapping=None, scales=[], layers=self.layers)
+        return add_common_params(plot)
