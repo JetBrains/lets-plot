051a8a4544ce5f105a4bb7f43e06bd409fe6663a
Olga Larionova <olarionova@horis.ru>
2020-12-01 12:36:56 +0300

    Fix error massage.

--- plot-builder-portable/src/commonMain/kotlin/jetbrains/datalore/plot/builder/tooltip/MappingValue.kt
+++ plot-builder-portable/src/commonMain/kotlin/jetbrains/datalore/plot/builder/tooltip/MappingValue.kt
@@ -28,7 +28,7 @@ class MappingValue(
     }
 
     override fun setDataContext(dataContext: DataContext) {
-        require(!::myDataAccess.isInitialized) { "Data context can be set once" }
+        require(!::myDataAccess.isInitialized) { "Data context can be set only once" }
         myDataAccess = dataContext.mappedDataAccess
 
         require(myDataAccess.isMapped(aes)) { "$aes have to be mapped" }
