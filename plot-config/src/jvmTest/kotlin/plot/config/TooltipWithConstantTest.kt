a96a5d831e96ba05f5b7a37fa8456ccdf669ce87
Olga Larionova <olarionova@horis.ru>
2020-12-03 15:04:20 +0300

    Add tests for tooltips with constant value.

---
+++ plot-config/src/jvmTest/kotlin/plot/config/TooltipWithConstantTest.kt
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2020. JetBrains s.r.o.
+ * Use of this source code is governed by the MIT license that can be found in the LICENSE file.
+ */
+
+package jetbrains.datalore.plot.config
+
+import jetbrains.datalore.plot.builder.GeomLayer
+import jetbrains.datalore.plot.builder.interact.TooltipSpec
+import jetbrains.datalore.plot.builder.interact.TooltipSpec.Line
+import jetbrains.datalore.plot.parsePlotSpec
+import jetbrains.datalore.plot.server.config.ServerSideTestUtil
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class TooltipWithConstantTest {
+
+    @Test
+    fun `tooltip with value from aes mapping`() {
+        val layerSpec = """
+            "geom": "vline",
+            "mapping": {
+                "xintercept": [ 0.25 ]
+            }
+        """
+        val layer = buildGeomLayer(layerSpec)
+        assertEquals(
+            expected = listOf("xintercept: 0.25"),
+            actual = getTooltipLines(layer)
+        )
+
+    }
+
+    @Test
+    fun `tooltip with constant value`() {
+        val layerSpec = """
+            "geom": "vline",
+            "xintercept": 0.25
+        """
+        val layer = buildGeomLayer(layerSpec)
+
+        assertEquals(
+            expected = listOf("0.25"),
+            actual = getTooltipLines(layer)
+        )
+    }
+
+    @Test
+    fun `should use value from constant`() {
+        val layerSpec = """
+            "geom": "vline",
+            "mapping": {
+                "xintercept": [ 0.10 ]
+            },
+            "xintercept": 0.25
+        """
+        val layer = buildGeomLayer(layerSpec)
+
+        assertEquals(
+            expected = listOf("0.25"),
+            actual = getTooltipLines(layer)
+        )
+    }
+
+    private fun buildGeomLayer(layerSpec: String): GeomLayer {
+        val spec = """{
+            "data": { "x": [0.0, 0.5] },
+            "mapping": { "x": "x" },
+            "kind": "plot",
+            "layers": [  { $layerSpec } ]
+        }"""
+        val transformed = ServerSideTestUtil.serverTransformWithoutEncoding(parsePlotSpec(spec))
+        return PlotConfigClientSideUtil.createPlotAssembler(transformed).layersByTile.single().single()
+    }
+
+    private fun getTooltipLines(geomLayer: GeomLayer): List<String> {
+        return geomLayer.contextualMapping.getDataPoints(index = 0)
+            .map { dataPoint -> Line.withLabelAndValue(dataPoint.label, dataPoint.value) }
+            .map(TooltipSpec.Line::toString)
+    }
+}
