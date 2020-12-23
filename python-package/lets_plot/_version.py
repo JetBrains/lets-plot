3164d0248261565fd55055fb3c526f22aaf8528d
Valentin Dobidaytis <vdovidaytis@horis.ru>
2020-12-23 23:01:55 +0300

    Updated version v1.5.7.dev1

--- python-package/lets_plot/_version.py
+++ python-package/lets_plot/_version.py
@@ -3,4 +3,4 @@
 # Use of this source code is governed by the MIT license that can be found in the LICENSE file.
 #
 # see: https://www.python.org/dev/peps/pep-0440/#developmental-releases
-__version__ = '1.5.6'
+__version__ = '1.5.7.dev1'
