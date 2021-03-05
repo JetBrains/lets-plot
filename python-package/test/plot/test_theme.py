9fa737c718fc4bff8b67c0fab2e8cb94576d7e2d
Olga Larionova <olarionova@horis.ru>
2021-03-05 14:48:46 +0300

    Improve test.

--- python-package/test/plot/test_theme.py
+++ python-package/test/plot/test_theme.py
@@ -10,23 +10,31 @@ def test_theme_merge_opts():
     geom = _geom('geom')
     theme_opts1 = theme(axis_line='blank', axis_title='blank')
     theme_opts2 = theme(axis_text='blank', axis_tooltip='blank')
+
+    spec = gg.ggplot() + theme_opts1 + geom + theme_opts2
     expected_theme = {
         'axis_title': 'blank',
         'axis_text': 'blank',
         'axis_line': 'blank',
         'axis_tooltip': 'blank'
     }
-
-    assert (gg.ggplot() + theme_opts1 + geom + theme_opts2).as_dict()['theme'] == expected_theme
+    assert_theme(spec, expected_theme)
 
 
 def test_theme_override_opts():
     geom = _geom('geom')
     theme_opts = theme(axis_tooltip='blank', legend_position='none')
     overridden_opts = theme(legend_position='bottom')
-    expected_theme = {
-        'legend_position': 'bottom',
-        'axis_tooltip': 'blank'
-    }
 
-    assert (gg.ggplot() + theme_opts + geom + overridden_opts).as_dict()['theme'] == expected_theme
+    spec = gg.ggplot() + theme_opts + geom + overridden_opts
+    assert_theme(spec, {'axis_tooltip': 'blank', 'legend_position': 'bottom'})
+
+
+def test_theme_geom_in_opts():
+    geom = _geom('geom')
+    spec = gg.ggplot() + geom + theme(legend_position=geom)
+    assert_theme(spec, {'legend_position': geom})
+
+
+def assert_theme(spec, expected):
+    assert spec.props()['theme'] == expected
