28f0f8dc7c0617a671a497d55167e4bbbb518a20
ASmirnov-HORIS <asmirnov@horis.ru>
2021-04-01 11:18:22 +0300

    Update docstrings for tooltips.

--- python-package/lets_plot/plot/tooltip.py
+++ python-package/lets_plot/plot/tooltip.py
@@ -16,88 +16,64 @@ class layer_tooltips(FeatureSpec):
     """
     Configure tooltips.
 
-    Parameters
-    ----------
-    format() defines the format for displaying the value:
-            .format(field = 'density', format = '.1f')
-            .format(field = '^color', format = 'value is {.1f}')
-        This format will be applied to the mapped value in the default tooltip or to the corresponding value
-        specified in the 'line' template.
-        The field name starts with a '^' prefix for aesthetics, variable names are specified without prefix.
-        It's possible to set the format for all positional aesthetics:
-            field = "^X" - for all positional x;
-            field = "^Y" - for all positional y.
-        The format contains a number format ('1.f') or a string template ('{.1f}').
-        The numeric format for non-numeric value will be ignored.
-        If you need to include a brace character in the literal text, it can be escaped by doubling: {{ and }}, e.g.,
-            .format('^color', '{{ {.1f} }}') -> "{ 17.0 }"
-            .format('model', '{} {{text}}') -> "mustang {text}"
-        The string template in format will allow to change lines for the default tooltip without 'line' specifying.
-        Also the template will change the line for outliers.
-        Aes and var formats are not interchangeable, i.e. var format will not be applied to aes, mapped to this variable.
-
-    line() - line to show in the tooltip.
-        Adds a line template to the tooltip with a label.
-        Variables and aesthetics can be accessed via a special syntax:
-            - ^color for aes
-            - @year for variable
-            - @{number of cylinders} for variable with spaces in the name
-            - @{square m^2} for variable with spaces and '^' symbol in the name
-            - @nameWith^ for the variable with '^' symbol in its name
-        A '^' symbol can be escaped with a backslash, a brace character in the literal text - by doubling:
-            .line('text') -> "text"
-            .line('{{text}}') -> "{text}"
-            .line('@model') -> "mustang"
-            .line('{{@model}}') -> "{mustang}"
-        The specified 'line' for outlier will move it to the general multi-line tooltip.
-        The default tooltip has a label before the value, usually containing the name of the mapped variable.
-        It has it's own behaviour, like blank label for axis aesthetics.
-        This default label can be set in template using a pair of symbols '@|'.
-        The label can be overridden by specifying a string value before '|' symbol.
-        Within the tooltip line the label is left-aligned, the formed by template string is right-aligned.
-        If a label is not specified, the string will be centered in the tooltip. For example:
-            - line('^color'): no label, value is centered;
-            - line('|^color'): label is empty, value is right-aligned;
-            - line('@|^color'): default label is used, value is right-aligned;
-            - line('my label|^color'): label is specified, value is right-aligned.
-
-    Set tooltips = "none" to hide tooltips from this layer.
-
-    anchor() - the corner of the plot to move the general tooltip:
-        ['top_left' | 'top_center' | 'top_right' |
-                      'middle_left' | 'middle_center' | 'middle_right' |
-                      'bottom_left' | 'bottom_center' | 'bottom_right']
-
-    min_width() - minimum width of the general tooltip.
-    color() - the color for the general tooltip.
-
-    Returns
-    -------
-        layer tooltips specification
+    Note
+    ----
+    Set tooltips='none' to hide tooltips from this layer.
 
     Note
     ----
     For more info see the `formatting reference <https://jetbrains.github.io/lets-plot-docs/pages/formats.html>`_.
 
     Examples
-    ---------
-    >>> import pandas as pd
-    >>> from lets_plot import *
-    >>> LetsPlot.setup_html()
-    >>> mpg_url = 'https://vincentarelbundock.github.io/Rdatasets/csv/ggplot2/mpg.csv'
-    >>> mpg = pd.read_csv(mpg_url)
-    >>> ggplot(mpg, aes(x='displ', y='hwy')) +
-    ... geom_point(aes(color='cty', shape='drv'),
-    ...                tooltips=layer_tooltips()
-    ...                         .format('hwy', '.1f')              # set the format for the variable value
-    ...                         .line('@manufacturer @model')      # "    ford mustang    "
-    ...                         .line('cty/hwy|^color/@hwy')       # "cty/hwy    17.0/26.0"
-    ...                         .line('@|@class')                  # "class     subcompact"
-    ...                         .line('|@year')                    # "                2008"
-    ...                         .line('--[mpg dataset] --'))       # " --[mpg dataset] -- "
+    --------
+    .. jupyter-execute::
+        :linenos:
+        :emphasize-lines: 15
+
+        import numpy as np
+        from lets_plot import *
+        LetsPlot.setup_html()
+        n = 100
+        np.random.seed(42)
+        data = {
+            'id': np.arange(n),
+            'x': np.random.normal(size=n),
+            'y': np.random.normal(size=n),
+            'c': np.random.choice(['a', 'b'], size=n),
+            'w': np.random.randint(1, 11, size=n)
+        }
+        ggplot(data, aes('x', 'y')) + \\
+            geom_point(aes(color='c', size='w'), \\
+                       tooltips=layer_tooltips().line('@c "@id"')
+                                                .line('---')
+                                                .format('@y', '.2f')
+                                                .line('(x, y)|(^x, @y)')
+                                                .line('@|@w')) + \\
+            scale_size(range=[2, 4])
+
+    |
+
+    .. jupyter-execute::
+        :linenos:
+        :emphasize-lines: 12
+
+        import numpy as np
+        from lets_plot import *
+        LetsPlot.setup_html()
+        n = 100
+        np.random.seed(42)
+        data = {
+            'x': np.random.normal(size=n),
+            'y': np.random.normal(size=n),
+            'c': np.random.randint(10, size=n)
+        }
+        ggplot(data, aes('x', 'y')) + \\
+            geom_point(aes(color='c'), tooltips='none')
+
     """
 
     def __init__(self):
+        """Initialize self."""
         self._tooltip_formats: List = []
         self._tooltip_lines: List = None
         self._tooltip_anchor = None
@@ -106,6 +82,29 @@ class layer_tooltips(FeatureSpec):
         super().__init__('tooltips', name=None)
 
     def as_dict(self):
+        """
+        Returns the dictionary of all properties of the object.
+
+        Returns
+        -------
+        dict
+            Dictionary of properties.
+
+        Examples
+        --------
+        .. jupyter-execute::
+            :linenos:
+            :emphasize-lines: 7
+
+            from lets_plot import *
+            LetsPlot.setup_html()
+            layer_tooltips().format('@x', '.2f')\\
+                            .line('@x @y')\\
+                            .line('^fill')\\
+                            .color('black')\\
+                            .as_dict()
+
+        """
         d = super().as_dict()
         d['tooltip_formats'] = self._tooltip_formats
         d['tooltip_lines'] = self._tooltip_lines
@@ -115,23 +114,295 @@ class layer_tooltips(FeatureSpec):
         return d
 
     def format(self, field=None, format=None):
+        """
+        Defines the format for displaying the value.
+        This format will be applied to the mapped value in the default tooltip
+        or to the corresponding value specified in the 'line' template.
+
+        Parameters
+        ----------
+        field : str
+            Name of an aesthetic or variable that would be formatted.
+            The field name starts with a '^' prefix for aesthetics,
+            variable names are specified without prefix.
+        format : str
+            Formatting specification. The format contains a number format ('1.f')
+            or a string template ('{.1f}').
+            The numeric format for non-numeric value will be ignored.
+            If you need to include a brace character in the literal text,
+            it can be escaped by doubling: `{{` and `}}`.
+
+        Returns
+        -------
+        `layer_tooltips`
+            Layer tooltips specification.
+
+        Note
+        ----
+        It's possible to set the format for all positional aesthetics:
+
+        - field='^X' - for all positional x,
+        - field='^Y' - for all positional y.
+
+        Note
+        ----
+        The string template in format will allow to change lines
+        for the default tooltip without `line` specifying.
+        Also the template will change the line for outliers.
+        Aes and var formats are not interchangeable, i.e. var format
+        will not be applied to aes, mapped to this variable.
+
+        Examples
+        --------
+        .. jupyter-execute::
+            :linenos:
+            :emphasize-lines: 15-16, 18, 20, 22
+
+            import numpy as np
+            from lets_plot import *
+            LetsPlot.setup_html()
+            n = 100
+            np.random.seed(42)
+            data = {
+                'a': np.random.normal(size=n),
+                'b': np.random.normal(size=n),
+                'c': np.random.choice(['X', 'Y'], size=n),
+                'd': np.random.uniform(size=n),
+                'e': np.random.randint(100, size=n)
+            }
+            ggplot(data, aes('a', 'b')) + \\
+                geom_point(aes(shape='c', size='e', color='d'), show_legend=False, \\
+                           tooltips=layer_tooltips().format(field='a', format='.1f')\\
+                                                    .format('^y', '.1f')\\
+                                                    .line('(@a, ^y)')\\
+                                                    .format('c', '{{{}}}')\\
+                                                    .line('@|@c')\\
+                                                    .format('^color', '≈ {.2f}')\\
+                                                    .line('@|^color')\\
+                                                    .format('e', '{}%')\\
+                                                    .line('e|@e')) + \\
+                scale_size(range=[2, 4])
+
+        |
+
+        .. jupyter-execute::
+            :linenos:
+            :emphasize-lines: 11-13
+
+            import numpy as np
+            from lets_plot import *
+            LetsPlot.setup_html()
+            n = 50
+            np.random.seed(42)
+            data = {
+                'v': np.random.normal(size=n),
+                'c': np.random.choice(['a', 'b', 'c'], size=n),
+            }
+            ggplot(data, aes('c', 'v')) + \\
+                geom_boxplot(tooltips=layer_tooltips().format('^Y', '.4f')\\
+                                                      .format('^ymin', 'min y: {.2f}')\\
+                                                      .format('^ymax', 'max y: {.2f}'))
+
+        """
         self._tooltip_formats.append({"field": field, "format": format})
         return self
 
     def line(self, value):
+        """
+        Line to show in the tooltip.
+        Adds a line template to the tooltip with a label.
+
+        Parameters
+        ----------
+        value : str
+            Enriched string which becomes one line of the tooltip.
+
+        Returns
+        -------
+        `layer_tooltips`
+            Layer tooltips specification.
+
+        Note
+        ----
+        Variables and aesthetics can be accessed via a special syntax:
+
+        - ^color for aes,
+        - @x for variable,
+        - @{x + 1} for variable with spaces in the name,
+        - @{x^2 + 1} for variable with spaces and '^' symbol in the name,
+        - @x^2 for the variable with '^' symbol in its name.
+
+        A '^' symbol can be escaped with a backslash, a brace character
+        in the literal text - by doubling:
+
+        - 'x\^2' -> "x^2"
+        - '{{x}}' -> "{x}"
+
+        The specified 'line' for outlier will move it to the general multi-line tooltip.
+        The default tooltip has a label before the value,
+        usually containing the name of the mapped variable.
+        It has it's own behaviour, like blank label for axis aesthetics.
+        This default label can be set in template using a pair of symbols '@|'.
+        The label can be overridden by specifying a string value before '|' symbol.
+        Within the tooltip line the label is left-aligned,
+        the formed by template string is right-aligned.
+        If a label is not specified, the string will be centered in the tooltip.
+
+        Examples
+        --------
+        .. jupyter-execute::
+            :linenos:
+            :emphasize-lines: 12, 14
+
+            import numpy as np
+            from lets_plot import *
+            LetsPlot.setup_html()
+            n = 100
+            np.random.seed(42)
+            x = np.linspace(-3, 3, n)
+            y = 9 - x ** 2 + np.random.normal(scale=.3, size=n)
+            data = {'x': x, '9 - x^2': y}
+            ggplot(data) + \\
+                geom_point(aes('x', '9 - x^2'), \\
+                           tooltips=layer_tooltips().format('x', '.3f')\\
+                                                    .line('x = @x')\\
+                                                    .format('9 - x^2', '.3f')\\
+                                                    .line('9 - x\^2 = @{9 - x^2}'))
+
+        |
+
+        .. jupyter-execute::
+            :linenos:
+            :emphasize-lines: 17-20
+
+            import numpy as np
+            from lets_plot import *
+            LetsPlot.setup_html()
+            n = 100
+            np.random.seed(42)
+            data = {
+                'x': np.random.normal(size=n),
+                'y': np.random.normal(size=n),
+                'c': np.random.choice(['X', 'Y'], size=n),
+                'p': np.random.uniform(size=n),
+                'w': np.random.randint(100, size=n)
+            }
+            ggplot(data, aes('x', 'y')) + \\
+                geom_point(aes(shape='c', size='w', color='p'), show_legend=False, \\
+                           tooltips=layer_tooltips().format('x', '.2f')\\
+                                                    .format('y', '.2f')\\
+                                                    .line('(^x, ^y)')\\
+                                                    .line('|^shape')\\
+                                                    .line('@|^color')\\
+                                                    .line('w|^size')) + \\
+                scale_size(range=[2, 4])
+
+        """
         if self._tooltip_lines is None:
             self._tooltip_lines = []
         self._tooltip_lines.append(value)
         return self
 
     def anchor(self, value):
+        """
+        The corner of the plot to move the general tooltip.
+
+        Parameters
+        ----------
+        value : {'top_left', 'top_center', 'top_right', 'middle_left', 'middle_center', 'middle_right', 'bottom_left', 'bottom_center', 'bottom_right'}
+            Type of the tooltip anchoring.
+
+        Returns
+        -------
+        `layer_tooltips`
+            Layer tooltips specification.
+
+        Examples
+        --------
+        .. jupyter-execute::
+            :linenos:
+            :emphasize-lines: 10
+
+            import numpy as np
+            from lets_plot import *
+            LetsPlot.setup_html()
+            n = 100
+            np.random.seed(42)
+            x = np.random.normal(size=n)
+            y = np.random.normal(size=n)
+            ggplot({'x': x, 'y': y}, aes('x', 'y')) + \\
+                geom_point(tooltips=layer_tooltips().line('(^x, ^y)')\\
+                                                    .anchor('top_center'))
+
+        """
         self._tooltip_anchor = value
         return self
 
     def min_width(self, value):
+        """
+        Minimum width of the general tooltip.
+
+        Parameters
+        ----------
+        value : int
+            Minimum width value.
+
+        Returns
+        -------
+        `layer_tooltips`
+            Layer tooltips specification.
+
+        Examples
+        --------
+        .. jupyter-execute::
+            :linenos:
+            :emphasize-lines: 10
+
+            import numpy as np
+            from lets_plot import *
+            LetsPlot.setup_html()
+            n = 100
+            np.random.seed(42)
+            x = np.random.normal(size=n)
+            y = np.random.normal(size=n)
+            ggplot({'x': x, 'y': y}, aes('x', 'y')) + \\
+                geom_point(tooltips=layer_tooltips().line('(^x, ^y)')\\
+                                                    .min_width(200))
+
+        """
         self._tooltip_min_width = value
         return self
 
     def color(self, value):
+        """
+        The color for the general tooltip.
+
+        Parameters
+        ----------
+        value : str
+            Color of the tooltip.
+
+        Returns
+        -------
+        `layer_tooltips`
+            Layer tooltips specification.
+
+        Examples
+        --------
+        .. jupyter-execute::
+            :linenos:
+            :emphasize-lines: 10
+
+            import numpy as np
+            from lets_plot import *
+            LetsPlot.setup_html()
+            n = 100
+            np.random.seed(42)
+            x = np.random.normal(size=n)
+            y = np.random.normal(size=n)
+            ggplot({'x': x, 'y': y}, aes('x', 'y')) + \\
+                geom_point(tooltips=layer_tooltips().line('(^x, ^y)')\\
+                                                    .color('magenta'))
+        """
         self._tooltip_color = value
         return self
