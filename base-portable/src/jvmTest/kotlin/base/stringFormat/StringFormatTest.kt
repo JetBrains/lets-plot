bd1d0b87b9850d33d801132c29248d970edea56a
Olga Larionova <olarionova@horis.ru>
2021-06-23 13:36:42 +0300

    Add support of DateTime format to StringFormat. Add tests.

--- base-portable/src/jvmTest/kotlin/base/stringFormat/StringFormatTest.kt
+++ base-portable/src/jvmTest/kotlin/base/stringFormat/StringFormatTest.kt
@@ -5,8 +5,12 @@
 
 package jetbrains.datalore.base.stringFormat
 
-import jetbrains.datalore.base.stringFormat.StringFormat.FormatType.NUMBER_FORMAT
-import jetbrains.datalore.base.stringFormat.StringFormat.FormatType.STRING_FORMAT
+import jetbrains.datalore.base.datetime.Date
+import jetbrains.datalore.base.datetime.DateTime
+import jetbrains.datalore.base.datetime.Month
+import jetbrains.datalore.base.datetime.Time
+import jetbrains.datalore.base.datetime.tz.TimeZone
+import jetbrains.datalore.base.stringFormat.StringFormat.FormatType.*
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
@@ -20,6 +24,8 @@ class StringFormatTest {
         assertEquals(1, StringFormat.create("{.1f} test").argsNumber)
         assertEquals(2, StringFormat.create("{.1f} {}").argsNumber)
         assertEquals(3, StringFormat.create("{.1f} {.2f} {.3f}").argsNumber)
+        assertEquals(1, StringFormat.create("%d.%m.%y %H:%M", DATETIME_FORMAT).argsNumber)
+        assertEquals(2,StringFormat.create("at {%H:%M} on {%A}", STRING_FORMAT).argsNumber)
     }
 
     @Test
@@ -93,7 +99,10 @@ class StringFormatTest {
         val exception = assertFailsWith(IllegalStateException::class) {
             StringFormat.create(formatPattern).format(valuesToFormat)
         }
-        assertEquals("Can't format values [1, 2] with pattern \"{.1f} x {.2f} x {.3f}\"). Wrong number of arguments: expected 3 instead of 2", exception.message)
+        assertEquals(
+            "Can't format values [1, 2] with pattern '{.1f} x {.2f} x {.3f}'). Wrong number of arguments: expected 3 instead of 2",
+            exception.message
+        )
     }
 
     @Test
@@ -119,10 +128,86 @@ class StringFormatTest {
     @Test
     fun `try to format static text as number format`() {
         val exception = assertFailsWith(IllegalStateException::class) {
-            StringFormat.create("text", type = NUMBER_FORMAT).format(emptyList())
+            StringFormat.create("pattern", type = NUMBER_FORMAT).format("text")
         }
         assertEquals(
-            "Wrong number pattern: text",
+            "Wrong number pattern: pattern",
+            exception.message
+        )
+    }
+
+    private val dateTimeToFormat = TimeZone.UTC.toInstant(
+        DateTime(Date(6, Month.AUGUST, 2019), Time(4, 46, 35))
+    ).timeSinceEpoch
+
+    @Test
+    fun `DateTime format`() {
+        assertEquals("August", StringFormat.create("%B").format(dateTimeToFormat))
+        assertEquals("Tuesday", StringFormat.create("%A").format(dateTimeToFormat))
+        assertEquals("2019", StringFormat.create("%Y").format(dateTimeToFormat))
+        assertEquals("06.08.19", StringFormat.create("%d.%m.%y").format(dateTimeToFormat))
+        assertEquals("06.08.19 04:46", StringFormat.create("%d.%m.%y %H:%M").format(dateTimeToFormat))
+    }
+
+    @Test
+    fun `string pattern with Number and DateTime`() {
+        val formatPattern = "{d}nd day of {%B}"
+        val valuesToFormat = listOf(2, dateTimeToFormat)
+        val formattedString = StringFormat.create(formatPattern, STRING_FORMAT).format(valuesToFormat)
+        assertEquals("2nd day of August", formattedString)
+    }
+
+    @Test
+    fun `use DateTime format in the string pattern`() {
+        assertEquals(
+            "at 04:46 on Tuesday",
+            StringFormat.create(
+                pattern = "at {%H:%M} on {%A}",
+                type = STRING_FORMAT
+            ).format(listOf(dateTimeToFormat, dateTimeToFormat))
+        )
+    }
+    @Test
+    fun `DateTime format can be used to form the string without braces in its pattern`() {
+        assertEquals(
+            expected = "at 04:46 on Tuesday",
+            StringFormat.create(
+                pattern = "at %H:%M on %A",
+                type = DATETIME_FORMAT
+            ).format(dateTimeToFormat)
+        )
+    }
+
+    @Test
+    fun `Number pattern as DateTime format will return string with pattern`() {
+        assertEquals(
+            expected = ".1f",
+            StringFormat.create(".1f", type = DATETIME_FORMAT).format(dateTimeToFormat)
+        )
+    }
+
+    @Test
+    fun `try to format static text as DateTime format`() {
+        val exception = assertFailsWith(IllegalStateException::class) {
+            StringFormat.create("%d.%m.%y").format("01.01.2000")
+        }
+        assertEquals(
+            "Value '01.01.2000' to be formatted as DateTime expected to be a Number, but was String",
+            exception.message
+        )
+    }
+
+    @Test
+    fun `try to use undefined pattern inside string pattern`() {
+        val formatPattern = "{.1f} x {PP}"
+        val valuesToFormat = listOf(1, 2)
+
+        val exception = assertFailsWith(IllegalStateException::class) {
+            StringFormat.create(formatPattern).format(valuesToFormat)
+        }
+
+        assertEquals(
+            "Can't detect type of pattern 'PP' used in string pattern '{.1f} x {PP}'",
             exception.message
         )
     }
