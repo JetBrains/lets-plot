f76768bfebf6eef9321495f32afd691aa047497e
Olga Larionova <olarionova@horis.ru>
2021-07-23 17:08:52 +0300

    Fix after merge.

--- plot-config-portable/src/commonMain/kotlin/jetbrains/datalore/plot/config/LayerConfig.kt
+++ plot-config-portable/src/commonMain/kotlin/jetbrains/datalore/plot/config/LayerConfig.kt
@@ -16,6 +16,7 @@ import jetbrains.datalore.plot.builder.VarBinding
 import jetbrains.datalore.plot.builder.assemble.PosProvider
 import jetbrains.datalore.plot.builder.data.OrderOptionUtil.OrderOption
 import jetbrains.datalore.plot.builder.data.OrderOptionUtil.OrderOption.Companion.mergeWith
+import jetbrains.datalore.plot.builder.data.OrderOptionUtil.createOrderSpec
 import jetbrains.datalore.plot.builder.sampling.Sampling
 import jetbrains.datalore.plot.builder.tooltip.TooltipSpecification
 import jetbrains.datalore.plot.config.ConfigUtil.createAesMapping
