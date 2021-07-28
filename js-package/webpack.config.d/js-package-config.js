fa4745c4899232c89e176048bef8b2e89d8ec130
Valentin Dobidaytis <vdovidaytis@horis.ru>
2021-07-28 19:31:41 +0300

    Fixed build issues related to Webpack version update.

--- js-package/webpack.config.d/js-package-config.js
+++ js-package/webpack.config.d/js-package-config.js
@@ -3,10 +3,7 @@ config.output.library = "LetsPlot";
 config.output.libraryTarget = "window";
 config.output.globalObject = "window";
 
-config.node = config.node || {};
-config.node.Buffer = false;
-
 const webpack = require('webpack');
 
 config.plugins = config.plugins || [];
-config.plugins.push(new webpack.IgnorePlugin(/^(ws|text-encoding|abort-controller|buffer|node-fetch)$/));
+config.plugins.push(new webpack.IgnorePlugin({ resourceRegExp: /^(ws|text-encoding|abort-controller|buffer|node-fetch|crypto)$/ }));
