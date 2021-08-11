ff5238563168ae02dc58ed9f685b7e14e07591a1
Igor Alshannikov <Igor.Alshannikov@jetbrains.com>
2021-08-11 14:24:57 -0400

    Move computation of axis breaks AxisUtil and clean-up ScaleUtil.

--- plot-builder-portable/src/commonMain/kotlin/jetbrains/datalore/plot/builder/AxisUtil.kt
+++ plot-builder-portable/src/commonMain/kotlin/jetbrains/datalore/plot/builder/AxisUtil.kt
@@ -5,6 +5,7 @@
 
 package jetbrains.datalore.plot.builder
 
+import jetbrains.datalore.base.geometry.DoubleVector
 import jetbrains.datalore.plot.base.CoordinateSystem
 import jetbrains.datalore.plot.base.Scale
 import jetbrains.datalore.plot.base.scale.ScaleUtil
@@ -36,8 +37,49 @@ object AxisUtil {
         axis.tickMarkWidth.set(theme.tickMarkWidth())
     }
 
-    fun setBreaks(axis: AxisComponent, scale: Scale<Double>, coord: CoordinateSystem, isHorizontal: Boolean) {
-        axis.breaks.set(ScaleUtil.axisBreaks(scale, coord, isHorizontal))
-        axis.labels.set(ScaleUtil.labels(scale))
+    fun setBreaks(axis: AxisComponent, scale: Scale<Double>, coord: CoordinateSystem, horizontal: Boolean) {
+        val scaleBreaks = scale.getScaleBreaks()
+        val mappedBreaks = toAxisCoord(scaleBreaks.transformedValues, scale, coord, horizontal)
+
+//        axis.breaks.set(ScaleUtil.axisBreaks(scale, coord, horizontal))
+        axis.breaks.set(mappedBreaks)
+        axis.labels.set(scaleBreaks.labels)
+    }
+
+    private fun toAxisCoord(
+        transformedBreaks: List<Double>,
+        scale: Scale<Double>,
+        coord: CoordinateSystem,
+        horizontal: Boolean
+    ): List<Double> {
+        val breaksMapped = ScaleUtil.map(transformedBreaks, scale).map {
+            // Don't expect NULLs.
+            it as Double
+        }
+        val axisBreaks = ArrayList<Double>()
+        for (br in breaksMapped) {
+            val mappedBrPoint = when (horizontal) {
+                true -> DoubleVector(br, 0.0)
+                false -> DoubleVector(0.0, br)
+            }
+
+            val axisBrPoint = coord.toClient(mappedBrPoint)
+            val axisBr = if (horizontal)
+                axisBrPoint.x
+            else
+                axisBrPoint.y
+
+            axisBreaks.add(axisBr)
+            if (!axisBr.isFinite()) {
+                throw IllegalStateException(
+                    "Illegal axis '" + scale.name + "' break position " + axisBr +
+                            " at index " + (axisBreaks.size - 1) +
+                            "\nsource breaks    : " + scale.breaks +
+                            "\ntranslated breaks: " + breaksMapped +
+                            "\naxis breaks      : " + axisBreaks
+                )
+            }
+        }
+        return axisBreaks
     }
 }
