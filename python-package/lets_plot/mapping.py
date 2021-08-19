e9e0fb9770e33bb36a46f5660ba95e6c7d412163
Artem Smirnov <asmirnov@horis.ru>
2021-08-19 22:08:06 +0300

    Update some docstrings (#422)
    * Update docstring for geom_point() function.
    
    * Update examples in docstrings for geom.py.
    
    * Docstring: Remove word 'optional' from parameters description.
    
    * Docstring: Update data, map and map_join parameters description.
    
    * Docstring: Add linenos parameter to the example for point geometry.
    
    * Updated core.py docstrings.
    
    * Update examples in docstring core.py
    
    * Minor fixes.
    
    * Update coord.py docstring
    
    * Minor fixes
    
    * Update examples in coord.py docstrings, update geom_extras.py docstrings
    
    * Minor fixes
    
    * Add extra imports from docstrings for geom.py.
    
    * Minor fixes
    
    * Fixed example for geom_extras
    
    * List of values in geom_extras.py
    
    * Docstring: add returns type for coordinates.
    
    * Update docstring in plot.py
    
    * Docstrings: replace >>> to :linenos: in geom.py.
    
    * Tiny fix in docstring for geom_step().
    
    * Update examples in plot.py, update docstring in label.py.
    
    * Update docstrings for the image_matrix() function.
    
    * Update docstring for correlation plot.
    
    * Update docstring for ggsave() function.
    
    * Tiny fix in the ggsave() docstring.
    
    * Add examples with correlation plot to docstrings of geom_point(), geom_raster(), geom_tile() and geom_text().
    
    * Update docstring for the geocode() function.
    
    * Minor fixes.
    
    * Indents fixed.
    
    * Docstrings of plot.py and label.py: fix types, whitespaces, emphasize-lines and remove extra Notes.
    
    * Tiny fixes in docstrings.
    
    * Update docstrings in geo_data.
    
    * Add docstrings for NamesGeocoder.
    
    * Update docstrings for geo data functions.
    
    * Update docstrings for geom.py: add computed variables.
    
    * Add link to documentation page for formatting in tooltips and geom_text docstrings.
    
    * Fix lists in docstrings notes (according to Numpydoc and rst rules).
    
    * Tiny fixes in geoms docstrings.
    
    * Update links in docstrings.
    
    * Add docstrings in sampling.py.
    
    * Add more docstrings, minor fixes.
    
    * Documentation for sampling: fix returns and update a few examples.
    
    * Update facets documentation.
    
    * Add docstrings to the functions lims(), xlim(), ylim().
    
    * Add docstring to the function element_blank().
    
    * Remove extra images.
    
    * Add 'Tooltip Customization' into the TOC of README_PYTHON.
    
    * Add interactive map in PyCharm animation to docs.
    
    * Remove extra files for the tooltips documentation.
    
    * Fix an example in the geom_boxplot() docstring.
    
    * Fix docstrings in the mapping.py.
    
    * Update tiles from Stamen to NASA in the example for maptiles_zxy().
    
    * Fix example for the maptiles_solid() function.
    
    Co-authored-by: EArkhipova-HORIS <horis.earkhipova@gmail.com>

--- python-package/lets_plot/mapping.py
+++ python-package/lets_plot/mapping.py
@@ -17,37 +17,94 @@ class MappingMeta:
 
 def as_discrete(variable, label=None, order_by=None, order=None):
     """
-    Marks a numeric variable as categorical.
+    The function is used to annotate a numeric data series as categorical data with the possibility of its ordering for the purposes of given visualization.
 
     Parameters
     ----------
-    variable : string
-        The name of the variable
-
-    label : string
-        The name of the scale - used as the axis label or the legend title
-
-    order_by : string
+    variable : str
+        The name of the variable.
+    label : str
+        The name of the scale - used as the axis label or the legend title.
+    order_by : str
         The variable name to order by.
     order : int
-        The ordering direction. 1 - ascending, -1 - descending.
+        The ordering direction. 1 for ascending, -1 for descending.
 
     Returns
     -------
-        variable meta information
+    `MappingMeta` or list
+        Variable meta information.
 
-    Notes
-    -----
+    Note
+    ----
     The plot will use a discrete scale for the aesthetic mapping.
-    It is similar to the factor() function from R but works differently - there is no data transformation.
+    It is similar to the `factor()` function from R but works differently - there is no data transformation.
+
+    To enable ordering mode, at least one ordering parameter (`order_by` or `order`) should be specified.
+    By the default, it will use descending direction and ordering by eigenvalues.
+    You cannot specify different order settings for the same variable.
+    But if these settings don't contradict each other, they will be combined.
+
     Examples
-    ---------
-    >>> df = {
-    >>>       'x': [0, 5, 10, 15],
-    >>>       'y': [0, 5, 10, 15],
-    >>>       'a': [1, 2, 3, 2]
-    >>> }
-    >>> ggplot(df, aes(x='x', y='y')) + geom_point(aes(color=pm.as_discrete('a')), size=9)
+    --------
+    .. jupyter-execute::
+        :linenos:
+        :emphasize-lines: 13
+
+        import numpy as np
+        from lets_plot import *
+        from lets_plot.mapping import as_discrete
+        LetsPlot.setup_html()
+        n = 100
+        np.random.seed(42)
+        data = {
+            'x': np.random.normal(size=n),
+            'y': np.random.normal(size=n),
+            'c': np.random.randint(5, size=n),
+        }
+        ggplot(data, aes('x', 'y')) + \\
+            geom_point(aes(color=as_discrete('c')))
+
+    |
+
+    .. jupyter-execute::
+        :linenos:
+        :emphasize-lines: 12
+
+        import numpy as np
+        from lets_plot import *
+        from lets_plot.mapping import as_discrete
+        LetsPlot.setup_html()
+        n = 100
+        np.random.seed(42)
+        data = {
+            'x': np.random.uniform(size=100),
+            'c': np.random.choice(list('abcde'), size=100),
+        }
+        ggplot(data) + \\
+            geom_boxplot(aes(as_discrete('c', label='class', order=1), 'x'))
+
+    |
+
+    .. jupyter-execute::
+        :linenos:
+        :emphasize-lines: 13-14
+
+        import numpy as np
+        from lets_plot import *
+        from lets_plot.mapping import as_discrete
+        LetsPlot.setup_html()
+        n = 100
+        np.random.seed(42)
+        data = {
+            'x': np.random.normal(size=n),
+            'c': np.random.choice(list('abcde'), size=n),
+            'i': np.random.randint(3, size=n),
+        }
+        ggplot(data) + \\
+            geom_bar(aes(as_discrete('c', order=1, order_by='..count..'), 'x', \\
+                         fill=as_discrete('i', order=1, order_by='..count..')))
+
     """
     if isinstance(variable, str):
         label = variable if label is None else label
