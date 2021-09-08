b6144f3e8489983521b9116f74e6fdf3af1833e5
Ilya Krukov <ikrukov@horis.ru>
2021-09-08 14:13:19 +0300

    Remove DataFrameEncoding

--- plot-config/src/commonMain/kotlin/jetbrains/datalore/plot/server/config/PlotConfigClientSideJvmJs.kt
+++ plot-config/src/commonMain/kotlin/jetbrains/datalore/plot/server/config/PlotConfigClientSideJvmJs.kt
@@ -5,20 +5,13 @@
 
 package jetbrains.datalore.plot.server.config
 
-import jetbrains.datalore.plot.config.PlotConfig
 import jetbrains.datalore.plot.config.PlotConfigClientSide
-import jetbrains.datalore.plot.config.transform.encode.DataSpecEncodeTransforms
 
 /**
  * DataSpecEncodeTransforms are only implemented for JS/JVM
  */
 object PlotConfigClientSideJvmJs {
     fun processTransform(plotSpec: MutableMap<String, Any>): MutableMap<String, Any> {
-        val isGGBunch = PlotConfig.isGGBunchSpec(plotSpec)
-        @Suppress("NAME_SHADOWING")
-        var plotSpec = DataSpecEncodeTransforms.clientSideDecode(isGGBunch).apply(plotSpec)
-
-        @Suppress("NAME_SHADOWING")
         return PlotConfigClientSide.processTransform(plotSpec)
     }
 }
