dfc43107d4eeadd002fd07346e1abd9e9482691d
Valentin Dobidaytis <vdovidaytis@horis.ru>
2021-09-22 14:19:06 +0300

    Refactoring.

--- gis/src/jsMain/kotlin/jetbrains/gis/tileprotocol/socket/TileWebSocketBuilder.kt
+++ gis/src/jsMain/kotlin/jetbrains/gis/tileprotocol/socket/TileWebSocketBuilder.kt
@@ -8,13 +8,11 @@ package jetbrains.gis.tileprotocol.socket
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.js.Js
 import io.ktor.client.features.websocket.WebSockets
-import kotlinx.coroutines.ObsoleteCoroutinesApi
 
 actual class TileWebSocketBuilder
 actual constructor(
     private val myUrl: String
 ) : SocketBuilder {
-    @ObsoleteCoroutinesApi
     override fun build(handler: SocketHandler): Socket {
         val client = HttpClient(Js) {
             install(WebSockets)
--- gis/src/jvmMain/kotlin/jetbrains/gis/tileprotocol/socket/TileWebSocketBuilder.kt
+++ gis/src/jvmMain/kotlin/jetbrains/gis/tileprotocol/socket/TileWebSocketBuilder.kt
@@ -8,12 +8,10 @@ package jetbrains.gis.tileprotocol.socket
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.features.websocket.WebSockets
-import kotlinx.coroutines.ObsoleteCoroutinesApi
 
 actual class TileWebSocketBuilder actual constructor(
     private val myUrl: String
 ) : SocketBuilder {
-    @ObsoleteCoroutinesApi
     override fun build(handler: SocketHandler): Socket {
         val client = HttpClient(CIO) {
             install(WebSockets)
