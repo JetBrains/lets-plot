4a6470f32631a9e0b1163a7886e29bfd18adf7cb
Igor Alshannikov <Igor.Alshannikov@jetbrains.com>
2021-10-05 14:09:12 -0400

    Update theme2_.py

--- python-package/lets_plot/plot/theme2_.py
+++ python-package/lets_plot/plot/theme2_.py
@@ -4,19 +4,9 @@
 #
 from .core import FeatureSpec
 
-__all__ = ['theme', 'element_blank']
+# __all__ = ['theme', 'element_blank']
 
 _VAL_ELEMENT_BLANK = "blank"
-
-
-class theme_lp_light(ThemeSpec):
-    def __init__(self):
-        super().__init__('theme', name='lp_light')
-
-
-class theme_grey(ThemeSpec):
-    def __init__(self):
-        super().__init__('theme', name='grey')
 
 
 class ThemeSpec(FeatureSpec):
@@ -24,56 +14,70 @@ class ThemeSpec(FeatureSpec):
     ToDo
     """
 
-    def __init__(self, name):
-        super().__init__('theme', name=name)
+    def __init__(self, name, **kwargs):
+        super().__init__('theme', name, **kwargs)
 
-    def _with_option(self, name: str, value) -> ThemeSpec:
+    def _with_option(self, name: str, value):
         copy = ThemeSpec(self.props().get('name'))
         copy.props().update(self.props())
         copy.props()[name] = value
         return copy
 
-    def _to_axis_option(self, base_name: str, axis: str):
-        if axis == 'both':
-            return base_name
-        elif axis in ['x', 'y']:
-            return base_name + "_" + axis
-        else:
-            raise ValueError("Expected 'axis' value: 'x', 'y' or 'both'")
-
-    def axis_blank(self, axis: str = 'both') -> ThemeSpec:
+    def axis_blank(self, axis: str = 'both'):
         return (self.axis_title_blank(axis)
+                .axis_line_blank(axis)
                 .axis_text_blank(axis)
                 .axis_ticks_blank(axis)
-                .axis_tooltip_blank(axis)
                 .axis_tooltip_blank(axis))
 
-    def axis_title_blank(self, axis: str = 'both') -> ThemeSpec:
+    def axis_title_blank(self, axis: str = 'both'):
         return self._with_option(_to_axis_option('axis_title', axis), _VAL_ELEMENT_BLANK)
 
-    def axis_text_blank(self, axis: str = 'both') -> ThemeSpec:
-        return self._with_option(_to_axis_option('axis_text', axis), _VALELEMENT_BLANK)
+    def axis_text_blank(self, axis: str = 'both'):
+        return self._with_option(_to_axis_option('axis_text', axis), _VAL_ELEMENT_BLANK)
 
-    def axis_ticks_blank(self, axis: str = 'both') -> ThemeSpec:
-        return self._with_option(_to_axis_option('axis_ticks', axis), _VALELEMENT_BLANK)
+    def axis_ticks_blank(self, axis: str = 'both'):
+        return self._with_option(_to_axis_option('axis_ticks', axis), _VAL_ELEMENT_BLANK)
 
-    def axis_line_blank(self, axis: str = 'both') -> ThemeSpec:
-        return self._with_option(_to_axis_option('axis_line', axis), _VALELEMENT_BLANK)
+    def axis_line_blank(self, axis: str = 'both'):
+        return self._with_option(_to_axis_option('axis_line', axis), _VAL_ELEMENT_BLANK)
 
-    def axis_tooltip_blank(self, axis: str = 'both') -> ThemeSpec:
-        return self._with_option(_to_axis_option('axis_tooltip', axis), _VALELEMENT_BLANK)
+    def axis_tooltip_blank(self, axis: str = 'both'):
+        return self._with_option(_to_axis_option('axis_tooltip', axis), _VAL_ELEMENT_BLANK)
 
-    def panel_background(self, color: str) -> ThemeSpec:
-        return self._with_option('panel_background', color)
+    def panel_blank(self):
+        return self._with_option('panel_rect', _VAL_ELEMENT_BLANK)
 
-    def panel_border_blank(self) -> ThemeSpec:
-        return self._with_option('panel_border', _VALELEMENT_BLANK)
+    # def panel_grid_blank(self, axis: str = 'both'):
+    #     return self._with_option(_to_axis_option('panel_grid', axis), _VAL_ELEMENT_BLANK)
+    #
+    # def panel_grid_major_blank(self, axis: str = 'both'):
+    #     return self._with_option(_to_axis_option('panel_grid_major', axis), _VAL_ELEMENT_BLANK)
+    #
+    # def panel_grid_major_blank(self, axis: str = 'both'):
+    #     return self._with_option(_to_axis_option('panel_grid_major', axis), _VAL_ELEMENT_BLANK)
 
-    def panel_grid_blank(self, axis: str = 'both') -> ThemeSpec:
-        return self._with_option(_to_axis_option('panel_grid', axis), _VALELEMENT_BLANK)
 
-    def panel_grid_major_blank(self, axis: str = 'both') -> ThemeSpec:
-        return self._with_option(_to_axis_option('panel_grid_major', axis), _VALELEMENT_BLANK)
+def _to_axis_option(base_name: str, axis: str):
+    if axis == 'both':
+        return base_name
+    elif axis in ['x', 'y']:
+        return base_name + "_" + axis
+    else:
+        raise ValueError("Expected 'axis' value: 'x', 'y' or 'both'")
 
-    def panel_grid_major_blank(self, axis: str = 'both') -> ThemeSpec:
-        return self._with_option(_to_axis_option('panel_grid_major', axis), _VALELEMENT_BLANK)
+
+class theme2(ThemeSpec):
+    # Replacment for old `theme()` function
+    def __init__(self, **kwargs):
+        super().__init__(None, **kwargs)
+
+
+class theme_lp_light(ThemeSpec):
+    def __init__(self, **kwargs):
+        super().__init__(name='lp_light', **kwargs)
+
+
+class theme_grey(ThemeSpec):
+    def __init__(self, **kwargs):
+        super().__init__(name='grey', **kwargs)
