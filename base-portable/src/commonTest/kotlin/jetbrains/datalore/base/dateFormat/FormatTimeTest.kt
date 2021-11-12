872bf401466bd5ba466ff21de596bb3bdebedf15
Olga Larionova <olarionova@horis.ru>
2021-11-12 13:13:27 +0300

    Fix formatting of 12-hour periods.

--- base-portable/src/commonTest/kotlin/jetbrains/datalore/base/dateFormat/FormatTimeTest.kt
+++ base-portable/src/commonTest/kotlin/jetbrains/datalore/base/dateFormat/FormatTimeTest.kt
@@ -17,4 +17,26 @@ class FormatTimeTest {
         val f = DateTimeFormat("%Y-%m-%dT%H:%M:%S")
         assertEquals("--T04:46:35", f.apply(time))
     }
+
+    @Test
+    fun time_12HourPeriods() {
+        // Format                  24-hour   12-hour
+        // Midnight (start of day)  00:00   12:00 am
+        // Noon                     12:00   12:00 pm
+        // Midnight (end of day)    24:00   12:00 am
+
+        val dayStart = Time(0, 0)
+        val noon = Time(12, 0)
+        val dayEnd = Time(24, 0)
+
+        val f12 = DateTimeFormat("%I:%M %P")
+        assertEquals("12:00 am", f12.apply(dayStart))
+        assertEquals("12:00 pm", f12.apply(noon))
+        assertEquals("12:00 am", f12.apply(dayEnd))
+
+        val f24 = DateTimeFormat("%H:%M")
+        assertEquals("00:00", f24.apply(dayStart))
+        assertEquals("12:00", f24.apply(noon))
+        assertEquals("24:00", f24.apply(dayEnd))
+    }
 }
