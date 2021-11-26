131dec4b8d02ad5f6f2eb9375479120d68778f12
Ivan Kupriyanov <ikupriyanov@horis.ru>
2021-11-26 21:41:22 +0300

    Cleanup GeoData demo.

--- plot-demo-common/src/commonMain/kotlin/jetbrains/datalore/plotDemo/model/plotConfig/GeoData.kt
+++ plot-demo-common/src/commonMain/kotlin/jetbrains/datalore/plotDemo/model/plotConfig/GeoData.kt
@@ -15,10 +15,7 @@ class GeoData {
             mapGeoDataFrame_NoMapJoin_MixedShapes("polygon"),
             mapGeoDataFrame_NoMapJoin_MixedShapes("point"),
             mapGeoDataFrame_NoMapJoin_MixedShapes("path"),
-            mapGeoDataFrame_Empty(),
-            mapGeoDict_MapJoin(),
-            dataGeoDataFrame_Empty(),
-            dataGeoDataFrame_NoMapJoin_GeomText()
+            dataGeoDataFrame_NoMapJoin_GeomText(),
         )
     }
 
@@ -30,37 +27,6 @@ class GeoData {
         private const val multipolygon =
             """{\"type\": \"MultiPolygon\", \"coordinates\": [[[[11.0, 12.0], [13.0, 14.0], [15.0, 13.0], [11.0, 12.0]]]]}"""
 
-        private fun mapGeoDict_MapJoin(): MutableMap<String, Any> {
-            val spec = """
-{
-  "ggtitle": {"text": "mapJoinDict"},
-  "ggsize": {
-    "width": 500,
-    "height": 300
-  },
-  "kind": "plot",
-  "layers": [
-    {
-      "geom": "polygon",
-      "data": {
-        "Country": ["UK", "Germany", "France"],
-        "Population": [66650000.0, 83020000.0, 66990000.0]
-      },
-      "mapping": {"fill": "Population"},
-      "map": {
-        "lon": [-2.598046, -2.37832, -2.46621, -3.169335, -1.938867, -0.576562, -0.31289, 0.873632, 0.082617, -2.598046, 7.685156, 9.926367, 13.661718, 14.101171, 11.464453, 12.870703, 8.564062, 8.651953, 6.80625, 6.938085, 7.685156, -2.246484, 2.367773, 7.245703, 5.268164, 6.586523, 2.895117, 2.279882, -0.532617, -0.356835, -2.246484],
-        "lat": [ 51.030349, 51.797754, 53.94575, 54.561879, 55.193929, 53.816229, 52.924809, 52.525588, 51.113188, 51.030349, 53.294124, 54.049078, 53.60816, 51.305902, 50.221916, 48.679365, 48.007575, 49.485266, 50.024691, 51.552493, 53.294124, 48.095702, 50.586036, 48.795295, 46.365136, 44.169607, 43.663114, 43.088157, 43.631315, 46.51655, 48.095702],
-        "country": [ "UK", "UK", "UK", "UK", "UK", "UK", "UK", "UK", "UK", "UK", "Germany", "Germany", "Germany", "Germany", "Germany", "Germany", "Germany", "Germany", "Germany", "Germany", "Germany", "France", "France", "France", "France", "France", "France", "France", "France", "France", "France"]
-      },
-      "map_join": ["Country", "country"],
-      "map_data_meta": {"geodict": {}},
-      "alpha": 0.3
-    }
-  ]
-}                
-            """.trimIndent()
-            return parsePlotSpec(spec)
-        }
 
         private fun mapGeoDataFrame_MapJoin(): MutableMap<String, Any> {
             val spec = """
@@ -72,7 +38,7 @@ class GeoData {
             |        "data": {"labels": ["A", "B"], "values": [12, 3]}, 
             |        "mapping": {"color": "values"}, 
             |        "map_data_meta": {"geodataframe": {"geometry": "coord"}}, 
-            |        "map_join": ["labels", "map_names"], 
+            |        "map_join": [["labels"], ["map_names"]], 
             |        "map": {
             |            "map_names": ["A", "B"], 
             |            "coord": ["$pointA", "$pointB"]
@@ -81,44 +47,6 @@ class GeoData {
             |}            
         """.trimMargin()
 
-            return parsePlotSpec(spec)
-        }
-
-        fun dataGeoDataFrame_Empty(): MutableMap<String, Any> {
-            val spec = """
-                |{
-                |   "ggtitle": {"text": "emptyDataGdf"},
-                |    "kind": "plot", 
-                |    "layers": [{
-                |        "geom": "polygon", 
-                |        "mapping": {"label": "map_names"}, 
-                |        "data_meta": {"geodataframe": {"geometry": "coord"}}, 
-                |        "data": {
-                |            "map_names": ["A", "B"], 
-                |            "coord": ["$pointA", "$pointB"]
-                |        } 
-                |    }]
-                |}    
-                """.trimMargin()
-            return parsePlotSpec(spec)
-        }
-
-        fun mapGeoDataFrame_Empty(): MutableMap<String, Any> {
-            val spec = """
-                |{
-                |   "ggtitle": {"text": "mapGeoDataFrame_Empty"},
-                |    "kind": "plot", 
-                |    "layers": [{
-                |        "geom": "polygon", 
-                |        "mapping": {"label": "map_names"}, 
-                |        "map_data_meta": {"geodataframe": {"geometry": "coord"}}, 
-                |        "map": {
-                |            "map_names": ["A", "B"], 
-                |            "coord": ["$pointA", "$pointB"]
-                |        } 
-                |    }]
-                |}    
-                """.trimMargin()
             return parsePlotSpec(spec)
         }
 
