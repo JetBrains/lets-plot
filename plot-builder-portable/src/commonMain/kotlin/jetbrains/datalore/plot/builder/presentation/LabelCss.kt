d036b00340398b65ad439367f8dba21626cad2a4
Igor Alshannikov <Igor.Alshannikov@jetbrains.com>
2021-11-29 20:35:44 -0500

    Remove 'boldness' from legend title.

--- plot-builder-portable/src/commonMain/kotlin/jetbrains/datalore/plot/builder/presentation/LabelCss.kt
+++ plot-builder-portable/src/commonMain/kotlin/jetbrains/datalore/plot/builder/presentation/LabelCss.kt
@@ -9,15 +9,17 @@ object LabelCss {
     operator fun get(labelSpec: LabelSpec, selector: String): String {
         val css = StringBuilder()
         css
-                .append(selector).append(" {")
-                .append(if (labelSpec.isMonospaced)
+            .append(selector).append(" {")
+            .append(
+                if (labelSpec.isMonospaced)
                     "\n  font-family: " + Defaults.FONT_FAMILY_MONOSPACED + ";"
                 else
-                    "\n")
-                .append("\n  font-size: ").append(labelSpec.fontSize).append("px;")
-                .append(if (labelSpec.isBold) "\n  font-weight: bold;" else "")
-                //      .append("\n  fill: red;")
-                .append("\n}\n")
+                    "\n"
+            )
+            .append("\n  font-size: ").append(labelSpec.fontSize).append("px;")
+            .append(if (labelSpec.isBold) "\n  font-weight: bold;" else "")
+            //      .append("\n  fill: red;")
+            .append("\n}\n")
         return css.toString()
     }
 }
