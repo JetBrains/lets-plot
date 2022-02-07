3111644878e4b8b78de85eb97b9285b3ca172acd
Olga Larionova <olarionova@horis.ru>
2022-02-07 15:40:55 +0300

    New experiments to estimate the text width. Improve demo.

--- plot-demo/src/commonMain/kotlin/jetbrains/datalore/plotDemo/model/component/PlotSpecLabelSizesDemo.kt
+++ plot-demo/src/commonMain/kotlin/jetbrains/datalore/plotDemo/model/component/PlotSpecLabelSizesDemo.kt
@@ -11,10 +11,7 @@ import jetbrains.datalore.base.values.Color
 import jetbrains.datalore.plot.base.render.svg.GroupComponent
 import jetbrains.datalore.plot.base.render.svg.Text
 import jetbrains.datalore.plot.base.render.svg.TextLabel
-import jetbrains.datalore.plot.builder.defaultTheme.DefaultTheme
-import jetbrains.datalore.plot.builder.layout.PlotLabelSpecFactory
 import jetbrains.datalore.plot.builder.presentation.PlotLabelSpec
-import jetbrains.datalore.plot.builder.presentation.Style
 import jetbrains.datalore.plotDemo.model.SimpleDemoBase
 import jetbrains.datalore.vis.svg.SvgRectElement
 import jetbrains.datalore.vis.svg.SvgUtils
@@ -22,79 +19,100 @@ import jetbrains.datalore.vis.svg.SvgUtils
 class PlotSpecLabelSizesDemo : SimpleDemoBase(DEMO_BOX_SIZE) {
 
     // todo experimental rule for text width calculating
-    enum class LetterCategory(val value: Double) {
-        NARROW(0.3),
-        NORMAL(0.67),
-        WIDE(0.8),
-        EXTRA_WIDE(0.98);
+    enum class CharCategory(val value: Double) {
+        EXTRA_NARROW(0.5),
+        NARROW(0.6),
+        NORMAL(1.0),
+        WIDE(1.3),
+        EXTRA_WIDE(1.5);
 
         companion object {
-            private fun getLetterCategory(letter: Char): LetterCategory {
-                return when (letter) {
+            private fun getCharCategory(ch: Char): CharCategory {
+                return when (ch) {
                     'i', 'j', 'l',
-                    '!', '.', ',', '\'',
-                    'I' -> NARROW
-                    'm', 'M', 'W' -> EXTRA_WIDE
-                    'w', in 'A'..'Z', in '0'..'9' -> WIDE
+                    '.', ',', '\'',
+                    'I' -> EXTRA_NARROW
+                    'f', 't', '(', ')', '[', ']', '{', '}', '!' -> NARROW
+                    'm', 'M', 'W', '%' -> EXTRA_WIDE
+                    'w', in 'A'..'Z', '+', '&' -> WIDE
                     else -> NORMAL
                 }
             }
 
-            fun getLetterRatio(letter: Char): Double {
-                return getLetterCategory(letter).value
-            }
+            fun getCharRatio(ch: Char) = getCharCategory(ch).value
         }
     }
 
-    private fun PlotLabelSpec.width(text: String): Double {
-        return text.map(LetterCategory.Companion::getLetterRatio).sum() * fontSize
-    }
+    // todo later should be moved to LabelMetrics.kt
+    fun PlotLabelSpec.width(text: String): Double {
+        ////
+        val FONT_WEIGHT_BOLD_TO_NORMAL_WIDTH_RATIO = 1.075
+        val FONT_WEIGHT_TO_NORMAL_WIDTH_RATIO = 0.63
+        val FONT_SIZE_TO_GLYPH_WIDTH_RATIO_MONOSPACED = 0.6
+        ////
 
-    ////////
+        val ratioFunc: (Char) -> Double = when {
+            isMonospaced -> {
+                { FONT_SIZE_TO_GLYPH_WIDTH_RATIO_MONOSPACED }
+            }
+            else -> CharCategory::getCharRatio
+        }
+        val width = text.map(ratioFunc).sum() * fontSize * FONT_WEIGHT_TO_NORMAL_WIDTH_RATIO
+        return if (isBold) {
+            width * FONT_WEIGHT_BOLD_TO_NORMAL_WIDTH_RATIO
+        } else {
+            width
+        }
+    }
 
     fun createModels(): List<GroupComponent> {
-        fun titles(charRange: CharRange): List<String> =
-            charRange.map { letter -> List(15) { letter }.joinToString("") }
-
-        val theme = DefaultTheme.minimal2()
-
-        return listOf(
-            Style.PLOT_TITLE to PlotLabelSpecFactory.plotTitle(theme.plot()),
-            "${Style.AXIS_TITLE}-x" to PlotLabelSpecFactory.axisTitle(theme.horizontalAxis(false))
-        ).flatMap {
-            listOf(
-                createModel(it, titles('A'..'Z')),
-                createModel(it, titles('a'..'z')),
-                createModel(it, titles('!'..'9'))
-            )
-        }
+        return createModelForLabelSpec(PlotLabelSpec.PLOT_TITLE) +
+                createModelForLabelSpec(PlotLabelSpec.PLOT_SUBTITLE) +
+                createModelForLabelSpec(PlotLabelSpec.AXIS_TITLE) +
+                createModelForLabelSpec(PlotLabelSpec.AXIS_TICK) +
+                createModelForLabelSpec(PlotLabelSpec.AXIS_TICK_SMALL) +
+                createModelForLabelSpec(PlotLabelSpec.LEGEND_TITLE) +
+                createModelForLabelSpec(PlotLabelSpec.LEGEND_ITEM)
     }
 
-    private fun createModel(plotLabel: Pair<String, PlotLabelSpec>, titles: List<String>): GroupComponent {
+    private fun createModelForLabelSpec(labelSpec: PlotLabelSpec): List<GroupComponent> {
+        fun titles(charRange: CharRange): List<String> =
+            charRange.map { ch -> List(15) { ch }.joinToString("") }
+
+        return listOf(
+            createModel(labelSpec, titles('A'..'Z')),
+            createModel(labelSpec, titles('a'..'z')),
+            createModel(labelSpec, titles('!'..'9')),
+        )
+    }
+
+    private fun createModel(plotLabelSpec: PlotLabelSpec, titles: List<String>): GroupComponent {
         val groupComponent = GroupComponent()
 
         val x = 120.0
         var y = 20.0
 
-        val nameSpecElement = TextLabel(plotLabel.first).rootGroup
+        val nameSpecElement = TextLabel(labelSpecToString(plotLabelSpec)).rootGroup
         SvgUtils.transformTranslate(nameSpecElement, 10.0, y)
         groupComponent.add(nameSpecElement)
 
+        y += plotLabelSpec.height() * 2
+
         titles
-            .forEach { title ->
-                val spec = LabelSpec(title, plotLabel.second)
-                val textLabel = createTextLabel(spec, plotLabel.first)
+            .map { title -> LabelSpec(title, plotLabelSpec) }
+            .forEach { spec ->
+                val textLabel = createTextLabel(spec)
 
                 val element = textLabel.rootGroup
                 SvgUtils.transformTranslate(element, x, y)
                 groupComponent.add(element)
 
-                val titleSizeNew = titleDimensions(spec, useLetterTypes = true)
+                val titleSizeNew = titleDimensions(spec, useNewWidthFunction = true)
                 val rectNew = DoubleRectangle(x, y - titleSizeNew.y / 2, titleSizeNew.x, titleSizeNew.y)
-                val titleSizeOld = titleDimensions(spec, useLetterTypes = false)
+                val titleSizeOld = titleDimensions(spec, useNewWidthFunction = false)
                 val rectOld = DoubleRectangle(x, y - titleSizeOld.y / 2, titleSizeOld.x, titleSizeOld.y)
 
-                groupComponent.add(svgRect(rectOld, Color.DARK_GREEN, strokeWidth = 2.0))
+                groupComponent.add(svgRect(rectOld, Color.LIGHT_BLUE, strokeWidth = 2.0))
                 groupComponent.add(svgRect(rectNew, Color.MAGENTA, strokeWidth = 1.5))
 
                 y += titleSizeOld.y + 10.0
@@ -110,11 +128,11 @@ class PlotSpecLabelSizesDemo : SimpleDemoBase(DEMO_BOX_SIZE) {
         return rect
     }
 
-    private fun titleDimensions(spec: LabelSpec, useLetterTypes: Boolean): DoubleVector {
+    private fun titleDimensions(spec: LabelSpec, useNewWidthFunction: Boolean): DoubleVector {
         if (spec.text.isEmpty()) {
             return DoubleVector.ZERO
         }
-        val width = if (useLetterTypes) {
+        val width = if (useNewWidthFunction) {
             spec.plotLabelSpec.width(spec.text)
         } else {
             spec.plotLabelSpec.width(spec.text.length)
@@ -130,13 +148,23 @@ class PlotSpecLabelSizesDemo : SimpleDemoBase(DEMO_BOX_SIZE) {
             val plotLabelSpec: PlotLabelSpec
         )
 
-        private fun createTextLabel(spec: LabelSpec, className: String): TextLabel {
+        private fun createTextLabel(spec: LabelSpec): TextLabel {
             val label = TextLabel(spec.text)
-            label.addClassName(className)
+
+            label.setFontSize(spec.plotLabelSpec.fontSize)
+            if (spec.plotLabelSpec.isBold) {
+                label.setFontWeight("bold")
+            }
             label.textColor().set(Color.DARK_BLUE)
+
             label.setHorizontalAnchor(Text.HorizontalAnchor.LEFT)
             label.setVerticalAnchor(Text.VerticalAnchor.CENTER)
+
             return label
+        }
+
+        private fun labelSpecToString(plotLabelSpec: PlotLabelSpec): String {
+            return plotLabelSpec.run { "$name: fontSize=$fontSize; bold=$isBold; monospaced=$isMonospaced" }
         }
     }
 }
