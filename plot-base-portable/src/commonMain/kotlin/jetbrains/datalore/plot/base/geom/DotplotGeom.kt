eb16d68bae4f6658090169680f078ffb117f99b6
ASmirnov-HORIS <asmirnov@horis.ru>
2022-02-10 18:07:07 +0300

    Update hints for dotplot.

--- plot-base-portable/src/commonMain/kotlin/jetbrains/datalore/plot/base/geom/DotplotGeom.kt
+++ plot-base-portable/src/commonMain/kotlin/jetbrains/datalore/plot/base/geom/DotplotGeom.kt
@@ -5,11 +5,15 @@
 
 package jetbrains.datalore.plot.base.geom
 
+import jetbrains.datalore.base.geometry.DoubleRectangle
 import jetbrains.datalore.base.geometry.DoubleVector
 import jetbrains.datalore.plot.base.*
 import jetbrains.datalore.plot.base.geom.util.GeomHelper
 import jetbrains.datalore.plot.base.geom.util.GeomUtil
+import jetbrains.datalore.plot.base.geom.util.HintColorUtil
 import jetbrains.datalore.plot.base.geom.util.LinesHelper
+import jetbrains.datalore.plot.base.interact.GeomTargetCollector
+import jetbrains.datalore.plot.base.interact.TipLayoutHint
 import jetbrains.datalore.plot.base.render.SvgRoot
 import jetbrains.datalore.plot.base.render.svg.LinePath
 import jetbrains.datalore.vis.svg.SvgPathDataBuilder
@@ -39,17 +43,32 @@ class DotplotGeom : GeomBase() {
                 )
                 root.add(path.rootGroup)
             }
+            // TODO: buildHint(p, binWidthPx, geomHelper, ctx)
         }
+    }
 
-        // TODO: Fix hints
-        /*
-        BarTooltipHelper.collectRectangleTargets(
-            emptyList(),
-            aesthetics, pos, coord, ctx,
-            BarGeom.rectangleByDataPoint(ctx, isHintRect = true),
-            { HintColorUtil.fromFill(it) }
+    private fun buildHint(
+        p: DataPointAesthetics,
+        binWidthPx: Double,
+        geomHelper: GeomHelper,
+        ctx: GeomContext
+    ) {
+        val origin = geomHelper.toClient(DoubleVector(
+            p.x()!! - binWidthPx / 2,
+            binWidthPx * (if (p.stacksize()!! > 0) 1.0 else 0.0)
+        ), p)
+        val dimension = DoubleVector(binWidthPx, binWidthPx)
+        val rect = DoubleRectangle(origin, dimension)
+        val colorsByDataPoint = HintColorUtil.fromMappedColors(ctx)
+
+        ctx.targetCollector.addRectangle(
+            p.index(),
+            rect,
+            GeomTargetCollector.TooltipParams.params()
+                .setMainColor(HintColorUtil.fromFill(p))
+                .setColors(colorsByDataPoint(p)),
+            TipLayoutHint.Kind.HORIZONTAL_TOOLTIP
         )
-        */
     }
 
     private class DotHelper constructor(pos: PositionAdjustment, coord: CoordinateSystem, ctx: GeomContext) :
