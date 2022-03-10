e25d0b3049d3bd33fb82af6bb6a0fc80d49a78d5
Valentin Dobidaytis <vdovidaytis@horis.ru>
2022-03-10 16:36:39 +0300

    Fixed Iterable imports

--- python-package/lets_plot/plot/util.py
+++ python-package/lets_plot/plot/util.py
@@ -2,7 +2,7 @@
 # Copyright (c) 2019. JetBrains s.r.o.
 # Use of this source code is governed by the MIT license that can be found in the LICENSE file.
 #
-from collections import Iterable
+from collections.abc import Iterable
 from datetime import datetime
 from typing import Any, Tuple, Sequence
 
