07376810853156f28f1cf129a3e444206c54d53c
Olga Larionova <olarionova@horis.ru>
2022-04-14 14:46:54 +0300

    Legend: use a rectangle with a stroke (instead of a slash-line) to show the stroke color.

--- plot-base-portable/src/commonMain/kotlin/jetbrains/datalore/plot/base/geom/legend/GenericLegendKeyElementFactory.kt
+++ plot-base-portable/src/commonMain/kotlin/jetbrains/datalore/plot/base/geom/legend/GenericLegendKeyElementFactory.kt
@@ -7,26 +7,20 @@ package jetbrains.datalore.plot.base.geom.legend
 
 import jetbrains.datalore.base.geometry.DoubleVector
 import jetbrains.datalore.plot.base.DataPointAesthetics
-import jetbrains.datalore.plot.base.aes.AestheticsUtil
 import jetbrains.datalore.plot.base.geom.util.GeomHelper
 import jetbrains.datalore.plot.base.render.LegendKeyElementFactory
 import jetbrains.datalore.vis.svg.SvgGElement
-import jetbrains.datalore.vis.svg.SvgLineElement
 import jetbrains.datalore.vis.svg.SvgRectElement
 
 class GenericLegendKeyElementFactory : LegendKeyElementFactory {
     override fun createKeyElement(p: DataPointAesthetics, size: DoubleVector): SvgGElement {
-        // background rect (to show fill)
+        // rect with background (to show fill) and stroke (to show color)
         val rect = SvgRectElement(0.0, 0.0, size.x, size.y)
-        AestheticsUtil.updateFill(rect, p)
-
-        // slash-line (to show stroke)
-        val line = SvgLineElement(0.0, size.y, size.x, 0.0)
-        GeomHelper.decorate(line, p)
+        GeomHelper.decorate(rect, p)
+        rect.strokeWidth().set(1.5) // set thickness
 
         val g = SvgGElement()
         g.children().add(rect)
-        g.children().add(line)
         return g
     }
 }
