15d13e287e639ebc219e8a9be7b7c2b62044cd27
ASmirnov-HORIS <asmirnov@horis.ru>
2022-05-12 14:25:15 +0300

    Fix tests for distributions.

--- plot-base-portable/src/commonTest/kotlin/jetbrains/datalore/plot/base/stat/DistributionsTest.kt
+++ plot-base-portable/src/commonTest/kotlin/jetbrains/datalore/plot/base/stat/DistributionsTest.kt
@@ -57,11 +57,11 @@ class DistributionsTest {
     fun normalCDFTest() {
         val dist = NormalDistribution(0.0, 1.0)
         assertEquals(0.0, dist.cumulativeProbability(Double.NEGATIVE_INFINITY))
-        assertEquals(3.1671241833116014e-5, dist.cumulativeProbability(-4.0))
-        assertEquals(0.15865525393145702, dist.cumulativeProbability(-1.0))
-        assertEquals(0.5, dist.cumulativeProbability(0.0))
-        assertEquals(0.841344746068543, dist.cumulativeProbability(1.0))
-        assertEquals(0.9999683287581669, dist.cumulativeProbability(4.0))
+        assertEquals(3.1671241833116014e-5, dist.cumulativeProbability(-4.0), DEFAULT_ACCURACY)
+        assertEquals(0.15865525393145702, dist.cumulativeProbability(-1.0), DEFAULT_ACCURACY)
+        assertEquals(0.5, dist.cumulativeProbability(0.0), DEFAULT_ACCURACY)
+        assertEquals(0.841344746068543, dist.cumulativeProbability(1.0), DEFAULT_ACCURACY)
+        assertEquals(0.9999683287581669, dist.cumulativeProbability(4.0), DEFAULT_ACCURACY)
         assertEquals(1.0, dist.cumulativeProbability(Double.POSITIVE_INFINITY))
     }
 
@@ -69,11 +69,11 @@ class DistributionsTest {
     fun normalQuantileFunctionTest() {
         val dist = NormalDistribution(0.0, 1.0)
         assertEquals(Double.NEGATIVE_INFINITY, dist.inverseCumulativeProbability(0.0))
-        assertEquals(-3.71901648545568, dist.inverseCumulativeProbability(0.0001))
-        assertEquals(-0.5244005127080407, dist.inverseCumulativeProbability(0.3))
-        assertEquals(0.0, dist.inverseCumulativeProbability(0.5))
-        assertEquals(0.5244005127080407, dist.inverseCumulativeProbability(0.7))
-        assertEquals(3.7190164854557084, dist.inverseCumulativeProbability(0.9999))
+        assertEquals(-3.71901648545568, dist.inverseCumulativeProbability(0.0001), DEFAULT_ACCURACY)
+        assertEquals(-0.5244005127080407, dist.inverseCumulativeProbability(0.3), DEFAULT_ACCURACY)
+        assertEquals(0.0, dist.inverseCumulativeProbability(0.5), DEFAULT_ACCURACY)
+        assertEquals(0.5244005127080407, dist.inverseCumulativeProbability(0.7), DEFAULT_ACCURACY)
+        assertEquals(3.7190164854557084, dist.inverseCumulativeProbability(0.9999), DEFAULT_ACCURACY)
         assertEquals(Double.POSITIVE_INFINITY, dist.inverseCumulativeProbability(1.0))
     }
 
@@ -89,13 +89,14 @@ class DistributionsTest {
 
     @Test
     fun tCDFTest() {
+        val accuracy = 1e-15
         val dist = TDistribution(1.0)
         assertEquals(0.0, dist.cumulativeProbability(Double.NEGATIVE_INFINITY))
-        assertEquals(0.077979130377369, dist.cumulativeProbability(-4.0), 1e-15)
-        assertEquals(0.25, dist.cumulativeProbability(-1.0), 1e-15)
-        assertEquals(0.5, dist.cumulativeProbability(0.0))
-        assertEquals(0.75, dist.cumulativeProbability(1.0), 1e-15)
-        assertEquals(0.922020869622631, dist.cumulativeProbability(4.0), 1e-15)
+        assertEquals(0.077979130377369, dist.cumulativeProbability(-4.0), accuracy)
+        assertEquals(0.25, dist.cumulativeProbability(-1.0), accuracy)
+        assertEquals(0.5, dist.cumulativeProbability(0.0), accuracy)
+        assertEquals(0.75, dist.cumulativeProbability(1.0), accuracy)
+        assertEquals(0.922020869622631, dist.cumulativeProbability(4.0), accuracy)
         assertEquals(1.0, dist.cumulativeProbability(Double.POSITIVE_INFINITY))
     }
 
@@ -106,7 +107,7 @@ class DistributionsTest {
         assertEquals(Double.NEGATIVE_INFINITY, dist.inverseCumulativeProbability(0.0))
         assertEquals(-31.820515954, dist.inverseCumulativeProbability(0.01), accuracy)
         assertEquals(-0.726542528, dist.inverseCumulativeProbability(0.3), accuracy)
-        assertEquals(0.0, dist.inverseCumulativeProbability(0.5))
+        assertEquals(0.0, dist.inverseCumulativeProbability(0.5), accuracy)
         assertEquals(0.726542528, dist.inverseCumulativeProbability(0.7), accuracy)
         assertEquals(31.820515954, dist.inverseCumulativeProbability(0.99), accuracy)
         assertEquals(Double.POSITIVE_INFINITY, dist.inverseCumulativeProbability(1.0))
@@ -132,11 +133,11 @@ class DistributionsTest {
     fun gammaCDFTest() {
         val dist = GammaDistribution(1.0, 1.0)
         assertEquals(0.0, dist.cumulativeProbability(Double.NEGATIVE_INFINITY))
-        assertEquals(0.0, dist.cumulativeProbability(-1.0))
-        assertEquals(0.0, dist.cumulativeProbability(0.0))
-        assertEquals(0.632120558828558, dist.cumulativeProbability(1.0))
-        assertEquals(0.9816843611112658, dist.cumulativeProbability(4.0))
-        assertEquals(0.9999546000702375, dist.cumulativeProbability(10.0))
+        assertEquals(0.0, dist.cumulativeProbability(-1.0), DEFAULT_ACCURACY)
+        assertEquals(0.0, dist.cumulativeProbability(0.0), DEFAULT_ACCURACY)
+        assertEquals(0.632120558828558, dist.cumulativeProbability(1.0), DEFAULT_ACCURACY)
+        assertEquals(0.9816843611112658, dist.cumulativeProbability(4.0), DEFAULT_ACCURACY)
+        assertEquals(0.9999546000702375, dist.cumulativeProbability(10.0), DEFAULT_ACCURACY)
         assertEquals(1.0, dist.cumulativeProbability(Double.POSITIVE_INFINITY))
     }
 
@@ -151,5 +152,10 @@ class DistributionsTest {
         assertEquals(1.203972804, dist.inverseCumulativeProbability(0.7), accuracy)
         assertEquals(9.210340372, dist.inverseCumulativeProbability(0.9999), accuracy)
         assertEquals(Double.POSITIVE_INFINITY, dist.inverseCumulativeProbability(1.0))
+    }
+
+
+    companion object {
+        const val DEFAULT_ACCURACY = 1e-16
     }
 }
