98f97b33ccdfad26f53bee264925d80146ef93d8
Olga Larionova <olarionova@horis.ru>
2022-05-13 15:51:42 +0300

    Fix message.

--- plot-base-portable/src/commonMain/kotlin/jetbrains/datalore/plot/base/render/svg/SvgUID.kt
+++ plot-base-portable/src/commonMain/kotlin/jetbrains/datalore/plot/base/render/svg/SvgUID.kt
@@ -22,10 +22,11 @@ object SvgUID {
     }
 
     fun get(prefix: String): String {
+        val letters = ('a'..'z') + ('A'..'Z')
         require(prefix.isNotEmpty()) { "ID prefix should not be empty" }
-        require(prefix.first() in ('a'..'z') + ('A'..'Z')) { "ID should start with a letter" }
-        require(prefix.all { it in ('a'..'z') + ('A'..'Z') + ('0'..'9') || it == '-' || it == '_' }) {
-            "ID prefix can contain only the letter or digits, plus the hyphen and the underscore"
+        require(prefix.first() in letters) { "ID should start with a letter" }
+        require(prefix.all { it in letters + ('0'..'9') || it == '-' || it == '_' }) {
+            "ID prefix can contain only the characters [a-zA-Z0-9], the hyphen (-) and the underscore (_)"
         }
         return "$prefix${suffixGen()}"
     }
