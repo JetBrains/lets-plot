5c6961347dc02d5530915f1d02b7cb62d0d7eca2
Olga Larionova <olarionova@horis.ru>
2022-05-25 19:16:25 +0300

    Fix comment.

--- vis-svg-mapper-jfx/src/jvmMain/kotlin/vis/svgMapper/jfx/attr/SvgShapeMapping.kt
+++ vis-svg-mapper-jfx/src/jvmMain/kotlin/vis/svgMapper/jfx/attr/SvgShapeMapping.kt
@@ -22,7 +22,7 @@ internal abstract class SvgShapeMapping<TargetT : Shape> : SvgAttrMapping<Target
         when (name) {
             SvgShape.FILL.name -> setColor(value, fillGet(target), fillSet(target))
             SvgShape.FILL_OPACITY.name -> {
-                // To fix artifacts on a 100% opaque shape
+                // To fix artifacts on a 100% opaque shape (JFX problem only).
                 // Issue: https://github.com/JetBrains/lets-plot/issues/539
                 // * The problem occurs on rectangles with 100% opacity.
                 // So it was enough to use this fix in `SvgRectAttrMapping`,
