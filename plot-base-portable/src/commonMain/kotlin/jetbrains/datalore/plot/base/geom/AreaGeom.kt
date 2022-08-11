9ff5fb2df688aec7c6cfcdf067ef2d8945fc854c
Artem Smirnov <asmirnov@horis.ru>
2022-08-11 14:36:27 +0300

    Fixes #552

--- plot-base-portable/src/commonMain/kotlin/jetbrains/datalore/plot/base/geom/AreaGeom.kt
+++ plot-base-portable/src/commonMain/kotlin/jetbrains/datalore/plot/base/geom/AreaGeom.kt
@@ -33,13 +33,13 @@ open class AreaGeom : GeomBase() {
         val helper = LinesHelper(pos, coord, ctx)
         val paths = helper.createBands(dataPoints, GeomUtil.TO_LOCATION_X_Y, GeomUtil.TO_LOCATION_X_ZERO)
         paths.reverse()
-        appendNodes(paths, root)
-
-        //if you want to retain the side edges of area: comment out the following codes,
-        // and switch decorate method in LinesHelper.createbands
+        // If you want to retain the side edges of area: comment out the following codes,
+        // and switch decorate method in LinesHelper.createBands
         helper.setAlphaEnabled(false)
         val lines = helper.createLines(dataPoints, GeomUtil.TO_LOCATION_X_Y)
-        appendNodes(lines, root)
+        lines.reverse()
+        for ((path, line) in paths zip lines)
+            appendNodes(listOf(path, line), root)
 
         buildHints(aesthetics, pos, coord, ctx)
     }
