bd92ef53f6d1034274933be11a90b1ab0c4598e6
Olga Larionova <olarionova@horis.ru>
2022-10-26 17:53:19 +0300

    Add new example.

--- plot-demo-common/src/commonMain/kotlin/jetbrains/datalore/plotDemo/model/plotConfig/Pie.kt
+++ plot-demo-common/src/commonMain/kotlin/jetbrains/datalore/plotDemo/model/plotConfig/Pie.kt
@@ -10,15 +10,17 @@ import jetbrains.datalore.plot.parsePlotSpec
 class Pie {
     fun plotSpecList(): List<MutableMap<String, Any>> {
         return listOf(
-             pie(hole = 0.0),
-             pie(hole = 0.2),
+            pie(hole = 0.0),
+            pie(hole = 0.2),
+            pieStatCount()
         )
     }
 
     private val data = mapOf(
-        "name" to ('A'..'H').toList(),
-        "value" to  listOf(160, 90, 34, 44, 21, 86, 15, 100)
+        "name" to ('A'..'H').toList() + 'B',
+        "value" to listOf(160, 90, 34, 44, 21, 86, 15, 100, 20)
     )
+
     private fun pie(hole: Double): MutableMap<String, Any> {
         val spec = "{" +
                 "   'kind': 'plot'," +
@@ -28,12 +30,45 @@ class Pie {
                 "              }," +
                 "   'layers': [" +
                 "               {" +
-                "                 'geom': 'pie', " +
+                "                  'geom': 'pie', " +
+                "                  'stat': 'identity', " +
                 "                  'x':0, 'y':0, " +
                 "                  'hole': $hole," +
                 "                  'size': 40" +
                 "               }" +
                 "             ]" +
+                "}"
+
+        val plotSpec = HashMap(parsePlotSpec(spec))
+        plotSpec["data"] = data
+        return plotSpec
+    }
+
+    private fun pieStatCount(): MutableMap<String, Any> {
+        val spec = "{" +
+                "   'kind': 'plot'," +
+                "   'mapping': {" +
+                "                'fill': 'name'," +
+                "                'weight': 'value'" +
+                "              }," +
+                "   'layers': [" +
+                "               {" +
+                "                  'geom': 'pie', " +
+                "                  'x':0, 'y':0, " +
+                "                  'size': 40" +
+                "               }" +
+                "             ]," +
+                "   'data_meta': {" +
+                "      'mapping_annotations': [" +
+                "          {" +
+                "               'aes': 'fill'," +
+                "               'annotation': 'as_discrete'," +
+                "               'parameters': {" +
+                "                   'label':'name', 'order_by': '..count..', 'order': -1 " +
+                "               }" +
+                "          }" +
+                "       ]" +
+                "   }" +
                 "}"
 
         val plotSpec = HashMap(parsePlotSpec(spec))
