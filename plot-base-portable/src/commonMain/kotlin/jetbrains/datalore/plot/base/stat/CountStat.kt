fe5b436332c0267e84702e0d63b897ac91b46222
Olga Larionova <olarionova@horis.ru>
2022-10-26 18:00:57 +0300

    Add AbstractCountStat.

--- plot-base-portable/src/commonMain/kotlin/jetbrains/datalore/plot/base/stat/CountStat.kt
+++ plot-base-portable/src/commonMain/kotlin/jetbrains/datalore/plot/base/stat/CountStat.kt
@@ -7,42 +7,16 @@ package jetbrains.datalore.plot.base.stat
 
 import jetbrains.datalore.plot.base.Aes
 import jetbrains.datalore.plot.base.DataFrame
-import jetbrains.datalore.plot.base.StatContext
-import jetbrains.datalore.plot.base.data.TransformVar
-import jetbrains.datalore.plot.common.util.MutableDouble
-import jetbrains.datalore.plot.common.data.SeriesUtil
 
 /**
  * Counts the number of cases at each x position.
  * (or if the weight aesthetic is supplied, the sum of the weights)
  */
-internal class CountStat : BaseStat(DEF_MAPPING) {
+
+internal class CountStat : AbstractCountStat(DEF_MAPPING, Aes.X) {
 
     override fun consumes(): List<Aes<*>> {
         return listOf(Aes.X, Aes.WEIGHT)
-    }
-
-    override fun apply(data: DataFrame, statCtx: StatContext, messageConsumer: (s: String) -> Unit): DataFrame {
-        if (!hasRequiredValues(data, Aes.X)) {
-            return withEmptyStatValues()
-        }
-
-        val valuesX = data.getNumeric(TransformVar.X)
-        val weight = BinStatUtil.weightVector(valuesX.size, data)
-
-        val statX = ArrayList<Double>()
-        val statCount = ArrayList<Double>()
-
-        val countByX = countByX(valuesX, weight)
-        for (x in countByX.keys) {
-            statX.add(x)
-            statCount.add(countByX[x]!!.get())
-        }
-
-        return DataFrame.Builder()
-            .putNumeric(Stats.X, statX)
-            .putNumeric(Stats.COUNT, statCount)
-            .build()
     }
 
     companion object {
@@ -50,19 +24,5 @@ internal class CountStat : BaseStat(DEF_MAPPING) {
             Aes.X to Stats.X,
             Aes.Y to Stats.COUNT
         )
-
-        private fun countByX(valuesX: List<Double?>, weight: List<Double?>): Map<Double, MutableDouble> {
-            val result = LinkedHashMap<Double, MutableDouble>()
-            for (i in valuesX.indices) {
-                val x = valuesX[i]
-                if (SeriesUtil.isFinite(x)) {
-                    if (!result.containsKey(x!!)) {
-                        result[x] = MutableDouble(0.0)
-                    }
-                    result[x]!!.getAndAdd(SeriesUtil.asFinite(weight[i], 0.0))
-                }
-            }
-            return result
-        }
     }
 }
