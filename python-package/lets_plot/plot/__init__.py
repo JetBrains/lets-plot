bd079152868665e8241bbe18317b8303691a9f67
Olga Larionova <olarionova@horis.ru>
2022-12-02 18:47:43 +0300

    Add annotations.py.

--- python-package/lets_plot/plot/__init__.py
+++ python-package/lets_plot/plot/__init__.py
@@ -21,6 +21,7 @@ from .scale_identity import *
 from .theme_ import *
 from .theme_set import *
 from .tooltip import *
+from .annotation import *
 from .font_features import *
 
 __all__ = (coord.__all__ +
@@ -41,6 +42,7 @@ __all__ = (coord.__all__ +
            theme_.__all__ +
            theme_set.__all__ +
            tooltip.__all__ +
+           annotation.__all__ +
            marginal_layer.__all__ +
            font_features.__all__
            )
