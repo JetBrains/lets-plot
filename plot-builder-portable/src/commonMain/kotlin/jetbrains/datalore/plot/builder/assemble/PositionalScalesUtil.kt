2d7320523c25638c3d5662c31b39cdd090b5f2f5
Olga Larionova <olarionova@horis.ru>
2022-12-12 21:48:16 +0300

    Code fix: `scale.transform is DiscreteTransform` -> `!scale.isContinuousDomain`

--- plot-builder-portable/src/commonMain/kotlin/jetbrains/datalore/plot/builder/assemble/PositionalScalesUtil.kt
+++ plot-builder-portable/src/commonMain/kotlin/jetbrains/datalore/plot/builder/assemble/PositionalScalesUtil.kt
@@ -280,7 +280,7 @@ internal object PositionalScalesUtil {
             widthAxis to when {
                 geom is WithWidth -> {
                     val resolution = geomCtx.getResolution(widthAxis)
-                    val isDiscrete = layer.scaleMap.get(widthAxis).transform is DiscreteTransform
+                    val isDiscrete = !layer.scaleMap.get(widthAxis).isContinuousDomain
                     computeLayerDryRunRangeAfterSizeExpand(aesthetics) { p ->
                         geom.widthSpan(p, widthAxis, resolution, isDiscrete)
                     }
@@ -298,7 +298,7 @@ internal object PositionalScalesUtil {
             heightAxis to when {
                 geom is WithHeight -> {
                     val resolution = geomCtx.getResolution(heightAxis)
-                    val isDiscrete = layer.scaleMap.get(heightAxis).transform is DiscreteTransform
+                    val isDiscrete = !layer.scaleMap.get(heightAxis).isContinuousDomain
                     computeLayerDryRunRangeAfterSizeExpand(aesthetics) { p ->
                         geom.heightSpan(p, heightAxis, resolution, isDiscrete)
                     }
