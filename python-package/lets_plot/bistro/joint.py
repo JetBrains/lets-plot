f897a8281849648bc7cb5a4947c1e114d9c781c5
Artem Smirnov <asmirnov@horis.ru>
2023-02-16 18:17:42 +0100

    Small updates in the joint_plot().

--- python-package/lets_plot/bistro/joint.py
+++ python-package/lets_plot/bistro/joint.py
@@ -10,10 +10,8 @@ from ._plot2d_common import *
 __all__ = ['joint_plot']
 
 _GEOM_DEF = 'point'
-_REG_LINE_DEF = True
 
 _REG_LINE_METHOD = 'lm'
-_REG_LINE_SE = False
 _REG_LINE_COLOR = "magenta"
 _REG_LINE_LINETYPE = 'dashed'
 
@@ -26,15 +24,26 @@ def _get_marginal_def(geom_kind, color_by=None):
     return "hist:tr"
 
 
+def _is_reg_line_needed(reg_line, geom_kind):
+    if reg_line is not None:
+        return reg_line
+    if geom_kind == 'point':
+        return True
+    else:
+        return False
+
+
 def joint_plot(data, x, y, *,
-               geom=_GEOM_DEF,
+               geom=None,
                bins=None, binwidth=None,
                color=None, size=None, alpha=None,
                color_by=None,
                show_legend=None,
-               reg_line=_REG_LINE_DEF,
+               reg_line=None,
+               se=None,
                marginal=None):
     # prepare parameters
+    geom_kind = geom or _GEOM_DEF
     binwidth2d, bins2d = _get_bin_params_2d(data[x], data[y], binwidth, bins)
     # prepare mapping
     mapping_dict = {'x': x, 'y': y}
@@ -43,20 +52,20 @@ def joint_plot(data, x, y, *,
     # prepare layers
     layers = []
     # main layer
-    main_layer = _get_geom2d_layer(geom, binwidth2d, bins2d, color, color_by, size, alpha, show_legend)
+    main_layer = _get_geom2d_layer(geom_kind, binwidth2d, bins2d, color, color_by, size, alpha, show_legend)
     if main_layer is not None:
         layers.append(main_layer)
     # smooth layer
-    if reg_line:
+    if _is_reg_line_needed(reg_line, geom_kind):
         layers.append(geom_smooth(
             aes(group=color_by),
-            method=_REG_LINE_METHOD, se=_REG_LINE_SE,
+            method=_REG_LINE_METHOD, se=se,
             color=_REG_LINE_COLOR, linetype=_REG_LINE_LINETYPE
         ))
     # marginal layers
     if len(data[x]) == 0:
         marginal = 'none'
-    defined_marginal = marginal or _get_marginal_def(geom, color_by)
+    defined_marginal = marginal or _get_marginal_def(geom_kind, color_by)
     if defined_marginal != 'none':
         layers += _get_marginal_layers(defined_marginal, binwidth2d, bins2d, color, color_by, show_legend)
 
