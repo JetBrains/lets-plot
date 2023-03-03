dedd46173c43eaf3e4bf6219d214d2d847c359d2
Artem Smirnov <asmirnov@horis.ru>
2023-03-03 15:46:31 +0100

    Fix docstrings for gggrid().

--- python-package/lets_plot/plot/gggrid_.py
+++ python-package/lets_plot/plot/gggrid_.py
@@ -22,31 +22,25 @@ def gggrid(plots: list, ncol: int = None, *,
     Parameters
     ----------
     plots : list
-        A list where each element is a plot specificatio, a subplots specification, or `None`.
-        Use value `None` to fill-in empty cells in grid.
+        A list where each element is a plot specification, a subplots specification, or None.
+        Use value None to fill-in empty cells in grid.
     ncol : int
         Number of columns in grid.
         If not specified, shows plots horizontally, in one row.
-    widths : list
-        A numeric list.
+    widths : list of numbers
         Relative width of each column of grid, left to right.
-    heights : list
-        A numeric list.
+    heights : list of numbers
         Relative height of each row of grid, top-down.
-    hspace : float
-        Default: 4px
-        Cell horizontal spacing.
-    vspace : float
-        Default: 4px
-        Cell vertical spacing.
-    fit: bool
-        Default: True
+    hspace : float, default=4.0
+        Cell horizontal spacing in px.
+    vspace : float, default=4.0
+        Cell vertical spacing in px.
+    fit : bool, default=True
         Whether to stretch each plot to match the aspect ratio of its cell (`fit=True`),
         or to preserve the original aspect ratio of plots (`fit=False`).
-    align: bool
-        Default: False
-        If `True`, align inner areas (i.e. "geom" bounds) of plots.
-        Howether, cells containing other (sub)grids are not participating in the plot "inner areas" layouting.
+    align : bool, default=False
+        If True, align inner areas (i.e. "geom" bounds) of plots.
+        However, cells containing other (sub)grids are not participating in the plot "inner areas" layouting.
 
     Returns
     -------
@@ -57,7 +51,7 @@ def gggrid(plots: list, ncol: int = None, *,
     --------
     .. jupyter-execute::
         :linenos:
-        :emphasize-lines: 10, 13
+        :emphasize-lines: 11, 14
 
         import numpy as np
         from lets_plot import *
