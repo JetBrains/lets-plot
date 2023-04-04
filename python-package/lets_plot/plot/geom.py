f6bfa036ea3c9ed67e8e916b7b7630c7263099bd
Artem Smirnov <asmirnov@horis.ru>
2023-04-04 18:49:17 +0200

    Add docstrings for the lollipop chart.

--- python-package/lets_plot/plot/geom.py
+++ python-package/lets_plot/plot/geom.py
@@ -24,7 +24,7 @@ __all__ = ['geom_point', 'geom_path', 'geom_line',
            'geom_density2d', 'geom_density2df', 'geom_jitter',
            'geom_qq', 'geom_qq2', 'geom_qq_line', 'geom_qq2_line',
            'geom_freqpoly', 'geom_step', 'geom_rect', 'geom_segment',
-           'geom_text', 'geom_label', 'geom_pie']
+           'geom_text', 'geom_label', 'geom_pie', 'geom_lollipop']
 
 
 def geom_point(mapping=None, *, data=None, stat=None, position=None, show_legend=None, sampling=None, tooltips=None,
@@ -6205,6 +6205,119 @@ def geom_pie(mapping=None, *, data=None, stat=None, position=None, show_legend=N
                  labels=labels,
                  map=map, map_join=map_join, use_crs=use_crs,
                  hole=hole, fill_by=fill_by, stroke=stroke, stroke_color=stroke_color,
+                 **other_args)
+
+
+def geom_lollipop(mapping=None, *, data=None, stat=None, position=None, show_legend=None, sampling=None, tooltips=None,
+                  slope=None, intercept=None,
+                  color_by=None, fill_by=None,
+                  **other_args):
+    """
+    Draw lollipop chart.
+
+    Parameters
+    ----------
+    mapping : `FeatureSpec`
+        Set of aesthetic mappings created by `aes()` function.
+        Aesthetic mappings describe the way that variables in the data are
+        mapped to plot "aesthetics".
+    data : dict or `DataFrame`
+        The data to be displayed in this layer. If None, the default, the data
+        is inherited from the plot data as specified in the call to ggplot.
+    stat : str, default='identity'
+        The statistical transformation to use on the data for this layer, as a string.
+        Supported transformations: 'identity' (leaves the data unchanged),
+        'count' (counts number of points with same x-axis coordinate),
+        'bin' (counts number of points with x-axis coordinate in the same bin),
+        'smooth' (performs smoothing - linear default),
+        'density' (computes and draws kernel density estimate).
+    position : str or `FeatureSpec`, default='identity'
+        Position adjustment, either as a string ('identity', 'stack', 'dodge', ...),
+        or the result of a call to a position adjustment function.
+    show_legend : bool, default=True
+        False - do not show legend for this layer.
+    sampling : `FeatureSpec`
+        Result of the call to the `sampling_xxx()` function.
+        To prevent any sampling for this layer pass value "none" (string "none").
+    tooltips : `layer_tooltips`
+        Result of the call to the `layer_tooltips()` function.
+        Specify appearance, style and content.
+    slope : float
+        The baseline slope.
+    intercept : float
+        The value of y at the point where the baseline crosses the y axis.
+    color_by : {'fill', 'color', 'paint_a', 'paint_b', 'paint_c'}, default='color'
+        Define the color aesthetic for the geometry.
+    fill_by : {'fill', 'color', 'paint_a', 'paint_b', 'paint_c'}, default='fill'
+        Define the fill aesthetic for the geometry.
+    other_args
+        Other arguments passed on to the layer.
+        These are often aesthetics settings used to set an aesthetic to a fixed value,
+        like color='red', fill='blue', size=3 or shape=21.
+        They may also be parameters to the paired geom/stat.
+
+    Returns
+    -------
+    `LayerSpec`
+        Geom object specification.
+
+    Notes
+    -----
+    `geom_lollipop()` understands the following aesthetics mappings:
+
+    - x : x-axis value.
+    - y : y-axis value.
+    - alpha : transparency level of the point. Accept values between 0 and 1.
+    - color (colour) : color of the geometry. Can be continuous or discrete. For continuous value this will be a color gradient between two colors.
+    - fill : color to paint shape's inner points. Is applied only to the points of shapes having inner points.
+    - shape : shape of the point, an integer from 0 to 25.
+    - size : size of the point.
+    - stroke : width of the line and shape border.
+
+    Examples
+    --------
+    .. jupyter-execute::
+        :linenos:
+        :emphasize-lines: 7
+
+        from lets_plot import *
+        LetsPlot.setup_html()
+        data = {
+            'x': [-3, -2, -1, 0, 1, 2, 3],
+            'y': [2, 3, -2, 3, -1, 0, 4],
+        }
+        ggplot(data, aes('x', 'y')) + geom_lollipop()
+
+    |
+
+    .. jupyter-execute::
+        :linenos:
+        :emphasize-lines: 10-11
+
+        from lets_plot import *
+        LetsPlot.setup_html()
+        data = {
+            'x': [-3, -2, -1, 0, 1, 2, 3],
+            'y': [-1, -3, -2, 3, -1, 2, -1],
+            'g': ['a', 'a', 'b', 'b', 'b', 'a', 'a'],
+        }
+        ggplot(data, aes('x', 'y')) + \\
+            geom_abline(slope=1, intercept=1, size=1.5, color="black") + \\
+            geom_lollipop(aes(fill='g'), slope=1, intercept=1, shape=22, \\
+                          size=5, stroke=2, color="black") + \\
+            coord_fixed()
+
+    """
+    return _geom('lollipop',
+                 mapping=mapping,
+                 data=data,
+                 stat=stat,
+                 position=position,
+                 show_legend=show_legend,
+                 sampling=sampling,
+                 tooltips=tooltips,
+                 slope=slope, intercept=intercept,
+                 color_by=color_by, fill_by=fill_by,
                  **other_args)
 
 
