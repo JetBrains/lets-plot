3af8656880fd75d29f8b43a8ab62d6c256ace9ed
Igor Alshannikov <Igor.Alshannikov@jetbrains.com>
2023-07-27 18:43:40 -0400

    Update "deprecated symbols".

---
+++ deprecated-in-v4/src/commonMain/kotlin/jetbrains/datalore/base/geometry/depracetedSymbols.kt
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2023. JetBrains s.r.o.
+ * Use of this source code is governed by the MIT license that can be found in the LICENSE file.
+ */
+
+package jetbrains.datalore.base.geometry
+
+@Deprecated(
+    "Moved to package \"org.jetbrains.letsPlot.commons.geometry\"",
+    ReplaceWith("org.jetbrains.letsPlot.commons.geometry.DoubleVector"), level = DeprecationLevel.WARNING
+)
+typealias DoubleVector = org.jetbrains.letsPlot.commons.geometry.DoubleVector
