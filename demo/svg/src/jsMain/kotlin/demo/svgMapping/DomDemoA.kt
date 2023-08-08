fd146f00cfb6ee5d956d9fde5c55a5173b0647b8
Valentin Dovidaytis <vdovidaytis@horis.ru>
2023-08-08 15:20:03 +0200

    Add JsExport annotation to demo-svg. Add opt-in setting.

--- demo/svg/src/jsMain/kotlin/demo/svgMapping/DomDemoA.kt
+++ demo/svg/src/jsMain/kotlin/demo/svgMapping/DomDemoA.kt
@@ -9,6 +9,7 @@ import org.jetbrains.letsPlot.datamodel.svg.dom.SvgSvgElement
 import demo.svgMapping.model.DemoModelA
 import org.jetbrains.letsPlot.platf.w3c.mapping.util.SvgToW3c.generateDom
 
+@JsExport
 fun svgElementsDemoA() {
     val svgGroup = DemoModelA.createModel()
     val svgRoot = SvgSvgElement(500.0, 500.0)
