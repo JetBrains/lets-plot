c4a0938291f001fdcaedc3ff07e42d48eee1c207
Ivan Kupriyanov <ikupriyanov@horis.ru>
2023-08-22 12:46:45 +0200

    Fix copyrights

--- commons/src/commonMain/kotlin/org/jetbrains/letsPlot/commons/intern/gcommon/collect/Iterables.kt
+++ commons/src/commonMain/kotlin/org/jetbrains/letsPlot/commons/intern/gcommon/collect/Iterables.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019 JetBrains s.r.o.
+ * Copyright (c) 2023 JetBrains s.r.o.
  * Use of this source code is governed by the MIT license that can be found in the LICENSE file.
  *
  * This file has been modified by JetBrains : Java code has been converted to Kotlin code.
