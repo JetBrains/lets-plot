d03961ca3c2bc77741517f103e0d213df33284ca
Rashid Yangazov <129742127+RYangazov@users.noreply.github.com>
2023-09-27 19:56:14 +0300

    Horizontal orientation by assigning y, xmin, xmax aesthetics of geoms: (#889)

--- plot-base/src/commonMain/kotlin/org/jetbrains/letsPlot/core/plot/base/geom/LineRangeGeom.kt
+++ plot-base/src/commonMain/kotlin/org/jetbrains/letsPlot/core/plot/base/geom/LineRangeGeom.kt
@@ -9,19 +9,33 @@ import org.jetbrains.letsPlot.commons.geometry.DoubleRectangle
 import org.jetbrains.letsPlot.commons.geometry.DoubleVector
 import org.jetbrains.letsPlot.core.plot.base.*
 import org.jetbrains.letsPlot.core.plot.base.aes.AesScaling
-import org.jetbrains.letsPlot.core.plot.base.geom.util.BarTooltipHelper
-import org.jetbrains.letsPlot.core.plot.base.geom.util.GeomHelper
-import org.jetbrains.letsPlot.core.plot.base.geom.util.GeomUtil
-import org.jetbrains.letsPlot.core.plot.base.geom.util.GeomUtil.extendWidth
-import org.jetbrains.letsPlot.core.plot.base.geom.util.HintColorUtil
+import org.jetbrains.letsPlot.core.plot.base.geom.util.*
 import org.jetbrains.letsPlot.core.plot.base.render.LegendKeyElementFactory
 import org.jetbrains.letsPlot.core.plot.base.render.SvgRoot
 import kotlin.math.max
 
-class LineRangeGeom : GeomBase() {
+class LineRangeGeom(private val isVertical: Boolean) : GeomBase() {
+    private val flipHelper = FlippableGeomHelper(isVertical)
+
+    private fun afterRotation(aes: Aes<Double>): Aes<Double> {
+        return flipHelper.getEffectiveAes(aes)
+    }
+
+    private fun afterRotation(rectangle: DoubleRectangle): DoubleRectangle {
+        return flipHelper.flip(rectangle)
+    }
+
+    private fun afterRotation(vector: DoubleVector): DoubleVector {
+        return flipHelper.flip(vector)
+    }
 
     override val legendKeyElementFactory: LegendKeyElementFactory
         get() = VLineGeom.LEGEND_KEY_ELEMENT_FACTORY
+
+    override val wontRender: List<Aes<*>>
+        get() {
+            return listOf(Aes.Y, Aes.XMIN, Aes.XMAX).map(::afterRotation)
+        }
 
     override fun buildIntern(
         root: SvgRoot,
@@ -30,25 +44,27 @@ class LineRangeGeom : GeomBase() {
         coord: CoordinateSystem,
         ctx: GeomContext
     ) {
+        val xAes = afterRotation(Aes.X)
+        val minAes = afterRotation(Aes.YMIN)
+        val maxAes = afterRotation(Aes.YMAX)
+        val dataPoints = GeomUtil.withDefined(aesthetics.dataPoints(), xAes, minAes, maxAes)
+
         val geomHelper = GeomHelper(pos, coord, ctx)
         val helper = geomHelper.createSvgElementHelper()
         helper.setStrokeAlphaEnabled(true)
         val colorsByDataPoint = HintColorUtil.createColorMarkerMapper(GeomKind.LINE_RANGE, ctx)
-        for (p in GeomUtil.withDefined(aesthetics.dataPoints(), Aes.X, Aes.YMIN, Aes.YMAX)) {
-            val x = p.x()!!
-            val ymin = p.ymin()!!
-            val ymax = p.ymax()!!
-
-            val start = DoubleVector(x, ymin)
-            val end = DoubleVector(x, ymax)
-            val line = helper.createLine(start, end, p)
-            if (line != null) {
-                root.add(line)
-            }
+        for (p in dataPoints) {
+            val x = p[xAes]!!
+            val ymin = p[minAes]!!
+            val ymax = p[maxAes]!!
+            // line
+            val start = afterRotation(DoubleVector(x, ymin))
+            val end = afterRotation(DoubleVector(x, ymax))
+            helper.createLine(start, end, p)?.let { root.add(it) }
         }
-
-        BarTooltipHelper.collectRectangleTargets(
-            listOf(Aes.YMAX, Aes.YMIN),
+        // tooltip
+        flipHelper.buildHints(
+            listOf(minAes, maxAes),
             aesthetics, pos, coord, ctx,
             clientRectByDataPoint(ctx, geomHelper),
             { HintColorUtil.colorWithAlpha(it) },
@@ -56,30 +72,41 @@ class LineRangeGeom : GeomBase() {
         )
     }
 
-    companion object {
-        const val HANDLES_GROUPS = false
+    private fun clientRectByDataPoint(
+        ctx: GeomContext,
+        geomHelper: GeomHelper
+    ): (DataPointAesthetics) -> DoubleRectangle? {
+        return { p ->
+            val xAes = afterRotation(Aes.X)
+            val minAes = afterRotation(Aes.YMIN)
+            val maxAes = afterRotation(Aes.YMAX)
+            if (p.defined(xAes) &&
+                p.defined(minAes) &&
+                p.defined(maxAes)
+            ) {
+                val x = p[xAes]!!
+                val ymin = p[minAes]!!
+                val ymax = p[maxAes]!!
+                val height = ymax - ymin
 
-        private fun clientRectByDataPoint(ctx: GeomContext, geomHelper: GeomHelper): (DataPointAesthetics) -> DoubleRectangle? {
-            return { p ->
-                if (p.defined(Aes.X) &&
-                    p.defined(Aes.YMIN) &&
-                    p.defined(Aes.YMAX)
-                ) {
-                    val x = p.x()!!
-                    val ymin = p.ymin()!!
-                    val ymax = p.ymax()!!
-                    val height = ymax - ymin
-
-                    val rect = geomHelper.toClient(
-                        DoubleRectangle(DoubleVector(x, ymax - height / 2), DoubleVector.ZERO),
-                        p
-                    )!!
-                    val width = max(AesScaling.strokeWidth(p), 2.0)
-                    extendWidth(rect, width, ctx.flipped)
-                } else {
-                    null
+                val rect = geomHelper.toClient(
+                    afterRotation(DoubleRectangle(DoubleVector(x, ymax - height / 2.0), DoubleVector.ZERO)),
+                    p
+                )!!
+                val width = max(AesScaling.strokeWidth(p), MIN_TOOLTIP_RECTANGLE_WIDTH)
+                val needToFlip = when {
+                    isVertical -> ctx.flipped
+                    else -> !ctx.flipped
                 }
+                GeomUtil.extendWidth(rect, width, needToFlip)
+            } else {
+                null
             }
         }
+    }
+
+    companion object {
+        const val HANDLES_GROUPS = false
+        const val MIN_TOOLTIP_RECTANGLE_WIDTH = 2.0
     }
 }
