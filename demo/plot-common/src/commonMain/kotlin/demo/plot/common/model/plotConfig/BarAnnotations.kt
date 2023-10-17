34022056a9ff1756584ebe7c2937d84dbe7c977a
Olga Larionova <olarionova@horis.ru>
2023-10-17 18:07:52 +0300

    Add demo batik.

--- demo/plot-common/src/commonMain/kotlin/demo/plot/common/model/plotConfig/BarAnnotations.kt
+++ demo/plot-common/src/commonMain/kotlin/demo/plot/common/model/plotConfig/BarAnnotations.kt
@@ -10,6 +10,7 @@ import demoAndTestShared.parsePlotSpec
 class BarAnnotations {
     fun plotSpecList(): List<MutableMap<String, Any>> {
         return listOf(
+            /*
             barPlot(grouped = false, flip = false),
             barPlot("dodge", flip = false),
             barPlot("stack", flip = false),
@@ -18,7 +19,15 @@ class BarAnnotations {
             barPlot(grouped = false, flip = true),
             barPlot("dodge", flip = true),
             barPlot("stack", flip = true),
-            barPlot("fill", flip = true),
+            barPlot("fill", flip = true),*
+           */
+            withNegativeValues("stack", flip = false),
+            withNegativeValues("fill", flip = false),
+            withNegativeValues("dodge", flip = false),
+
+            withNegativeValues("stack", flip = true),
+            withNegativeValues("fill", flip = true),
+            withNegativeValues("dodge", flip = true),
         )
     }
 
@@ -26,7 +35,7 @@ class BarAnnotations {
     val data = mapOf(
         "x" to List(6) { 'A' } + List(51) { 'B' } + List(4) { 'D' },
         "v" to listOf('a', 'a', 'a', 'b', 'c', 'c') + List(50) { ('a'..'d').random() } + listOf('e') +
-                listOf('a', 'a', 'b', 'c' )
+                listOf('a', 'a', 'b', 'c')
     )
 
     private fun barPlot(
@@ -59,5 +68,50 @@ class BarAnnotations {
         val plotSpec = HashMap(parsePlotSpec(spec))
         plotSpec["data"] = data
         return plotSpec
+    }
+
+    private val data2 = mapOf(
+        "x" to (0..3).map { ('A'..'D').toList() }.flatten(),
+        "group" to (0..3).map { ('A'..'D').map { "group$it" } }.flatten().sortedBy { it },
+        //"value" to List(16) { (-2..6).random() }.map { if (it == 0) 0.5 else it }
+        "value" to listOf(0.5, 5, -1.5, 3, 1, 2, 4, 0.5, -1, 6, 5, -0.2, 5, -2, 5, 4)
+    )
+
+    private fun withNegativeValues(position: String, flip: Boolean): MutableMap<String, Any> {
+        val coordSpec = when {
+            flip -> "'coord': {'name': 'flip', 'flip': true},"
+            else -> ""
+        }
+        val spec = """
+        {
+          'mapping': { 'x': 'x', 'y': 'value', 'fill': 'group' },
+           $coordSpec
+          'kind': 'plot',
+          'layers': [
+            {
+                'geom': 'bar',
+                'stat': 'identity',
+                'position': '$position',
+                'labels': {
+                    'lines': [
+                        '@value'
+                    ],
+                    'annotation_size': 15
+                },
+                'color': 'pen',
+                'size': 0.3
+            },
+            {
+                'geom': 'hline',
+                'tooltips': 'none',
+                'yintercept': 0.0
+            }
+          ]
+        }""".trimIndent()
+
+        val plotSpec = HashMap(parsePlotSpec(spec))
+        plotSpec["data"] = data2
+        return plotSpec
+
     }
 }
