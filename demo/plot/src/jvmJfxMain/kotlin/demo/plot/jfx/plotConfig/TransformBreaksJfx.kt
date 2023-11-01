efa4d74796ebf1780146ea12c38a33e7b3ee92d0
Artem Smirnov <asmirnov@horis.ru>
2023-11-01 12:46:40 +0100

    Prefer breaks which are integer powers of 10 (#919)
    * Demo for break transformations.
    
    * Fix #850.
    
    * Mention #850 in the future_changes.md file.

---
+++ demo/plot/src/jvmJfxMain/kotlin/demo/plot/jfx/plotConfig/TransformBreaksJfx.kt
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2023. JetBrains s.r.o.
+ * Use of this source code is governed by the MIT license that can be found in the LICENSE file.
+ */
+
+package demo.plot.jfx.plotConfig
+
+import demo.common.jfx.demoUtils.PlotSpecsDemoWindowJfx
+import demo.plot.common.model.plotConfig.TransformBreaks
+
+fun main() {
+    with(TransformBreaks()) {
+        PlotSpecsDemoWindowJfx(
+            "Transform breaks",
+            plotSpecList()
+        ).open()
+    }
+}
