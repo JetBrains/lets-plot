ab0e3f9ec2479bed1b32aa10ce92380b7474fb35
Ivan Kupriyanov <ikupriyanov@horis.ru>
2023-11-23 20:10:47 +0100

    Test for LP-942

--- platf-awt/src/jvmTest/kotlin/org/jetbrains/letsPlot/awt/plot/PlotSvgExportTest.kt
+++ platf-awt/src/jvmTest/kotlin/org/jetbrains/letsPlot/awt/plot/PlotSvgExportTest.kt
@@ -88,4 +88,64 @@ internal class PlotSvgExportTest {
             assertTrue(it.contains("style=\"image-rendering: optimizeSpeed; image-rendering: pixelated\""))
         }
     }
+
+
+    @Test
+    @Ignore("Disabled to have it in master and to not break the build.")
+    fun `LP-942 gggrid composite plot is not visible if saved with ggsave`() {
+        val spec = """
+        |{
+        |  "theme": { "name": "classic", "line": "blank", "axis": "blank" },
+        |  "kind": "subplots",
+        |  "layout": { "ncol": 2.0, "nrow": 1.0, "name": "grid" },
+        |  "figures": [
+        |    {
+        |      "kind": "plot",
+        |      "layers": [ { "geom": "point", "x": 0.0, "y": 0.0, "size": 24.0, "color": "red" } ]
+        |    },
+        |    {
+        |      "kind": "subplots",
+        |      "layout": { "ncol": 1.0, "nrow": 1.0, "name": "grid" },
+        |      "figures": [
+        |        {
+        |          "kind": "plot",
+        |          "layers": [ { "geom": "point", "x": 0.0, "y": 0.0, "size": 24.0, "color": "green" } ]
+        |        }
+        |      ]
+        |    }
+        |  ]
+        |}
+        """.trimMargin()
+
+        val svg = PlotSvgExport.buildSvgImageFromRawSpecs(plotSpec = parsePlotSpec(spec))
+
+        fun findAll(str: String, substr: String): List<Int> {
+            val result = mutableListOf<Int>()
+            var cur = 0
+            while (true) {
+                val i = str.indexOf(substr, cur)
+                if (i == -1) break
+                result += i
+                cur = i + 1
+            }
+            return result
+        }
+
+        val svgTags = findAll(svg, "<svg ") + findAll(svg, "</svg>")
+
+        val (lastOpenSvgTag, lastCloseSvgTag) = svgTags
+            .sorted() // sort by position, to properly handle nested tags
+            .drop(1).dropLast(1) // leave only inner svg elements
+            .takeLast(2) // take last svg element
+
+        // Not sure why we have 3 nested svg elements instead of 2. But it was like this before.
+        assertEquals(6, svgTags.size)
+        val lastSvgElement = svg.substring(lastOpenSvgTag, lastCloseSvgTag + "</svg>".length)
+
+        // The last svg element started to contain <rect> element which covers the whole svg area of the composite plot.
+        // Just keep how it was before - without <rect> element.
+        // But it's better to not have that last empty <svg> element at all.
+        assertEquals(0, findAll(lastSvgElement, "<rect").count())
+    }
+
 }
