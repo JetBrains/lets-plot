f18fc50892fbd6f5bfa97ccd8dbc6ff43e1aaa6c
Rashid Yangazov <129742127+RYangazov@users.noreply.github.com>
2024-02-26 14:21:25 +0300

    Add setup_show_ext() function (#1024)

---
+++ python-package/lets_plot/frontend_context/_webbr_html_page_ctx.py
@@ -0,0 +1,27 @@
+#
+# Copyright (c) 2019. JetBrains s.r.o.
+# Use of this source code is governed by the MIT license that can be found in the LICENSE file.
+#
+import io
+import webbrowser
+import tempfile
+from typing import Dict
+
+from ._frontend_ctx import FrontendContext
+from .. import _kbridge as kbr
+
+
+class WebBrHtmlPageContext(FrontendContext):
+
+    def __init__(self, exec: str, new: bool) -> None:
+        super().__init__()
+        self.exec = exec
+        self.new = new
+
+    def show(self, plot_spec: Dict) -> str:
+        html_page = kbr._generate_static_html_page(plot_spec, iframe=False)
+
+        path = tempfile.NamedTemporaryFile(suffix=".html", delete=False).name
+        io.open(path, 'w').write(html_page)
+
+        webbrowser.get(self.exec).open('file://' + path, new=1 if self.new else 2)
