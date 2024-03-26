d73e748c7d26b35857ecac021222669f6321493a
Ivan Kupriyanov <ikupriyanov@horis.ru>
2024-03-26 17:09:49 +0100

    Compact JsonFormatter output

--- commons/src/commonMain/kotlin/org/jetbrains/letsPlot/commons/intern/json/JsonFormatter.kt
+++ commons/src/commonMain/kotlin/org/jetbrains/letsPlot/commons/intern/json/JsonFormatter.kt
@@ -14,31 +14,33 @@ internal class JsonFormatter(pretty: Boolean = false) {
     }
 
     private fun handleList(list: List<*>) {
-        output.startList()
-        list.headTail({
-            output.firstItem()
-            handleValue(it)
-        }) { tail ->
+        val ctx = output.newCollectionContext(list)
+
+        output.startList(ctx)
+        list.headTail()?.let { (head, tail) ->
+            output.firstItem(ctx)
+            handleValue(head)
             tail.forEach {
-                output.nextItem()
+                output.nextItem(ctx)
                 handleValue(it)
             }
         }
-        output.endList()
+        output.endList(ctx)
     }
 
     private fun handleMap(map: Map<*, *>) {
-        output.startMap()
-        map.entries.headTail({
-            output.firstItem()
-            handlePair(it)
-        }) { tail ->
+        val ctx = output.newCollectionContext(map.values)
+
+        output.startMap(ctx)
+        map.entries.headTail()?.let {(head, tail) ->
+            output.firstItem(ctx)
+            handlePair(head)
             tail.forEach {
-                output.nextItem()
+                output.nextItem(ctx)
                 handlePair(it)
             }
         }
-        output.endMap()
+        output.endMap(ctx)
     }
 
     private fun handleValue(v: Any?) {
@@ -68,24 +70,29 @@ internal class JsonFormatter(pretty: Boolean = false) {
 
     private fun append(s: String) = output.append(s)
 
-    private fun <E> Collection<E>.headTail(head: (E) -> Unit, tail: (Sequence<E>) -> Unit) {
-        if (!isEmpty()) {
-            head(first())
-            tail(asSequence().drop(1))
+    private fun <E> Collection<E>.headTail(): Pair<E, Sequence<E>>? {
+        if (isEmpty()) {
+            return null
         }
+
+        return Pair(first(), asSequence().drop(1))
     }
 
     interface Output {
+        interface CollectionContext
+
+        fun newCollectionContext(c: Collection<*>): CollectionContext
+
         fun append(s: String)
 
-        fun startList()
-        fun endList()
+        fun startList(ctx: CollectionContext)
+        fun endList(ctx: CollectionContext)
 
-        fun startMap()
-        fun endMap()
+        fun startMap(ctx: CollectionContext)
+        fun endMap(ctx: CollectionContext)
 
-        fun firstItem()
-        fun nextItem()
+        fun firstItem(ctx: CollectionContext)
+        fun nextItem(ctx: CollectionContext)
 
         fun asString(): String
     }
@@ -93,14 +100,16 @@ internal class JsonFormatter(pretty: Boolean = false) {
     class Simple : Output {
         private var buffer: StringBuilder = StringBuilder()
 
-        override fun startList() = append("[")
-        override fun endList() = append("]")
-        override fun startMap() = append("{")
-        override fun endMap() = append("}")
-        override fun asString(): String = buffer.toString()
-        override fun firstItem() = Unit
+        override fun newCollectionContext(c: Collection<*>) = object : Output.CollectionContext {}
 
-        override fun nextItem() {
+        override fun startList(ctx: Output.CollectionContext) = append("[")
+        override fun endList(ctx: Output.CollectionContext) = append("]")
+        override fun startMap(ctx: Output.CollectionContext) = append("{")
+        override fun endMap(ctx: Output.CollectionContext) = append("}")
+        override fun asString(): String = buffer.toString()
+        override fun firstItem(ctx: Output.CollectionContext) = Unit
+
+        override fun nextItem(ctx: Output.CollectionContext) {
             buffer.append(",")
         }
 
@@ -111,30 +120,50 @@ internal class JsonFormatter(pretty: Boolean = false) {
 
 
     class Pretty : Output {
+        class PrettyContext(
+            collection: Collection<*>,
+        ) : Output.CollectionContext {
+            val splitLines = collection.size > 10 || collection.any { it is Collection<*> || it is Map<*, *> }
+        }
+
+        override fun newCollectionContext(c: Collection<*>) = PrettyContext(c)
+
         private var indent = 0
         private var buffer: StringBuilder = StringBuilder()
 
-        override fun startList() {
+        override fun startList(ctx: Output.CollectionContext) {
             buffer.append("[")
             indent++
         }
 
-        override fun endList() {
+        override fun endList(ctx: Output.CollectionContext) {
             indent--
-            buffer.append("\n")
-            buffer.append(indent())
+
+            if ((ctx as PrettyContext).splitLines) {
+                buffer.append("\n")
+                buffer.append(indent())
+            } else {
+                buffer.append(" ")
+            }
+
             buffer.append("]")
         }
 
-        override fun startMap() {
+        override fun startMap(ctx: Output.CollectionContext) {
             buffer.append("{")
             indent++
         }
 
-        override fun endMap() {
+        override fun endMap(ctx: Output.CollectionContext) {
             indent--
-            buffer.append("\n")
-            buffer.append(indent())
+
+            if ((ctx as PrettyContext).splitLines) {
+                buffer.append("\n")
+                buffer.append(indent())
+            } else {
+                buffer.append(" ")
+            }
+
             buffer.append("}")
         }
 
@@ -150,15 +179,21 @@ internal class JsonFormatter(pretty: Boolean = false) {
             return "  ".repeat(indent * 2)
         }
 
-        override fun firstItem() {
-            buffer.append("\n")
-            buffer.append(indent())
+        override fun firstItem(ctx: Output.CollectionContext) {
+            if ((ctx as PrettyContext).splitLines) {
+                buffer.append("\n")
+                buffer.append(indent())
+            } else {
+                buffer.append(" ")
+            }
         }
 
-        override fun nextItem() {
+        override fun nextItem(ctx: Output.CollectionContext) {
             buffer.append(", ")
-            buffer.append("\n")
-            buffer.append(indent())
+            if ((ctx as PrettyContext).splitLines) {
+                buffer.append("\n")
+                buffer.append(indent())
+            }
         }
     }
 }
