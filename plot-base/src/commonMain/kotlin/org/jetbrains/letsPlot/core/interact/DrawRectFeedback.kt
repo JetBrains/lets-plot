6f5a26132faf5b34b325bc4db964c57b3f50a722
Ivan Kupriyanov <ikupriyanov@horis.ru>
2024-06-19 22:58:55 +0200

    Interactivity: centered box with fixed aspect ratio on vertical selection

--- plot-base/src/commonMain/kotlin/org/jetbrains/letsPlot/core/interact/DrawRectFeedback.kt
+++ plot-base/src/commonMain/kotlin/org/jetbrains/letsPlot/core/interact/DrawRectFeedback.kt
@@ -13,10 +13,13 @@ import org.jetbrains.letsPlot.datamodel.svg.dom.SvgGraphicsElement
 import org.jetbrains.letsPlot.datamodel.svg.dom.SvgPathDataBuilder
 import org.jetbrains.letsPlot.datamodel.svg.dom.SvgPathElement
 import org.jetbrains.letsPlot.datamodel.svg.dom.SvgRectElement
+import kotlin.math.abs
 
 class DrawRectFeedback(
     private val onCompleted: ((DoubleRectangle) -> Unit)
 ) : DragFeedback {
+    private var fixAspectRatio: Boolean? = null
+
     private val dragRectSvg = SvgRectElement().apply {
         strokeColor().set(Color.BLACK)
         fillColor().set(Color.TRANSPARENT)
@@ -101,6 +104,7 @@ class DrawRectFeedback(
                     onCompleted(dataBounds)
                 }
 
+                fixAspectRatio = null
                 decorationsLayer.children().remove(dragRectSvg)
                 decorationsLayer.children().remove(selectionSvg)
             },
@@ -122,9 +126,57 @@ class DrawRectFeedback(
         dragFrom: DoubleVector,
         dragTo: DoubleVector,
         target: InteractionTarget
-    ) = DoubleRectangle.span(dragFrom, dragTo)
-        .intersect(target.geomBounds)
-        ?: DoubleRectangle(dragFrom, DoubleVector.ZERO) // at least move the selection rect to the drag start point
+    ): DoubleRectangle {
+        @Suppress("NAME_SHADOWING")
+        val dragTo = DoubleVector(
+            x = dragTo.x.coerceIn(target.geomBounds.left, target.geomBounds.right),
+            y = dragTo.y.coerceIn(target.geomBounds.top, target.geomBounds.bottom)
+        )
+        val drag = dragTo.subtract(dragFrom)
+
+        if (fixAspectRatio == null && drag.length() > 20) {
+            fixAspectRatio = abs(drag.x) < 7
+        }
+
+        return if (fixAspectRatio == true) {
+            val ratio = target.geomBounds.width / target.geomBounds.height
+            val size = if (ratio > 1) {
+                val dragHeight = abs(drag.y)
+
+                val height = when {
+                    dragFrom.y + dragHeight > target.geomBounds.bottom -> (target.geomBounds.bottom - dragFrom.y)
+                    dragFrom.y - dragHeight < target.geomBounds.top -> (dragFrom.y - target.geomBounds.top)
+                    else -> dragHeight
+                }
+
+                val width = (height * ratio)
+
+                val finalWidth = when {
+                    dragFrom.x + width > target.geomBounds.right -> target.geomBounds.right - dragFrom.x
+                    dragFrom.x - width < target.geomBounds.left -> dragFrom.x - target.geomBounds.left
+                    else -> width
+                }
+
+                val finalHeight = finalWidth / ratio
+
+                DoubleVector(finalWidth, finalHeight)
+            } else {
+                // Simple implementation for now. Try to flip and use the code above.
+                val width = abs(drag.x)
+                val height = width / ratio
+                DoubleVector(width, height)
+            }
+
+            DoubleRectangle(
+                origin = dragFrom.subtract(size),
+                dimension = size.mul(2.0)
+            )
+        } else {
+            DoubleRectangle.span(dragFrom, dragTo)
+                .intersect(target.geomBounds)
+                ?: DoubleRectangle(dragFrom, DoubleVector.ZERO) // at least move the selection rect to the drag start point
+        }
+    }
 
     private fun isSelectionAcceptable(selection: DoubleRectangle): Boolean {
         return selection.width > 25 || selection.height > 25
