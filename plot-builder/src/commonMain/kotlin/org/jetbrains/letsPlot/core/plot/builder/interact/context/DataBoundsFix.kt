4b8ad2a2c74826720cf357f6e3e9a6f3cd711599
Igor Alshannikov <Igor.Alshannikov@jetbrains.com>
2024-10-08 18:54:29 -0400

    Interact: improve solution preventing data bound implosion.

--- plot-builder/src/commonMain/kotlin/org/jetbrains/letsPlot/core/plot/builder/interact/context/DataBoundsFix.kt
+++ plot-builder/src/commonMain/kotlin/org/jetbrains/letsPlot/core/plot/builder/interact/context/DataBoundsFix.kt
@@ -10,31 +10,20 @@ import org.jetbrains.letsPlot.commons.geometry.DoubleVector
 import kotlin.math.nextUp
 
 internal object DataBoundsFix {
-    private const val MIN_SIZE = 1e-13  // actually, signif. digits can go down to e-16
 
     /**
      * Prevent a rect from being too small and collapsing
      * due to limited precision of the Double.
      */
     fun unImplode(rect: DoubleRectangle): DoubleRectangle {
-        val right = if (rect.width < MIN_SIZE) {
-            shiftedUp(rect.left)
-        } else {
-            rect.right
-        }
-
-        val bottom = if (rect.height < MIN_SIZE) {
-            shiftedUp(rect.top)
-        } else {
-            rect.bottom
-        }
-
+        val right = shiftedUp(rect.left)
+        val bottom = shiftedUp(rect.top)
         return rect.union(DoubleRectangle.span(rect.origin, DoubleVector(right, bottom)))
     }
 
     private fun shiftedUp(value: Double): Double {
         var result = value
-        repeat(100) {
+        repeat(1000) {
             result = result.nextUp()
         }
         return result
