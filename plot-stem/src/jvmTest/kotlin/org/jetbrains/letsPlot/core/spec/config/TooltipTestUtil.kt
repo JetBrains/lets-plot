1561a8f14654a770a66d34a80cb6d7ec56f43e32
Ivan Kupriyanov <ikupriyanov@horis.ru>
2024-11-29 13:34:27 +0100

    Minor refactoring

--- plot-stem/src/jvmTest/kotlin/org/jetbrains/letsPlot/core/spec/config/TooltipTestUtil.kt
+++ plot-stem/src/jvmTest/kotlin/org/jetbrains/letsPlot/core/spec/config/TooltipTestUtil.kt
@@ -5,23 +5,12 @@
 
 package org.jetbrains.letsPlot.core.spec.config
 
-import org.jetbrains.letsPlot.commons.geometry.DoubleVector
-import org.jetbrains.letsPlot.commons.values.Color
-import org.jetbrains.letsPlot.commons.values.FontFace
-import org.jetbrains.letsPlot.commons.values.FontFamily
-import org.jetbrains.letsPlot.core.plot.base.Aes
-import org.jetbrains.letsPlot.core.plot.base.PlotContext
-import org.jetbrains.letsPlot.core.plot.base.layout.TextJustification
-import org.jetbrains.letsPlot.core.plot.base.layout.Thickness
-import org.jetbrains.letsPlot.core.plot.base.render.linetype.NamedLineType
-import org.jetbrains.letsPlot.core.plot.base.theme.AxisTheme
-import org.jetbrains.letsPlot.core.plot.base.theme.ThemeTextStyle
-import org.jetbrains.letsPlot.core.plot.base.tooltip.ContextualMapping
+import org.jetbrains.letsPlot.commons.geometry.DoubleVector.Companion.ZERO
 import org.jetbrains.letsPlot.core.plot.base.tooltip.GeomTarget
 import org.jetbrains.letsPlot.core.plot.base.tooltip.TipLayoutHint
 import org.jetbrains.letsPlot.core.plot.builder.GeomLayer
 import org.jetbrains.letsPlot.core.plot.builder.assemble.TestingPlotContext
-import org.jetbrains.letsPlot.core.plot.builder.presentation.Defaults
+import org.jetbrains.letsPlot.core.plot.builder.defaultTheme.DefaultTheme
 import org.jetbrains.letsPlot.core.plot.builder.tooltip.spec.TooltipSpec
 import org.jetbrains.letsPlot.core.plot.builder.tooltip.spec.TooltipSpec.Line
 import org.jetbrains.letsPlot.core.plot.builder.tooltip.spec.TooltipSpecFactory
@@ -29,82 +18,28 @@ import kotlin.test.assertEquals
 
 object TooltipTestUtil {
 
-    private val axisTheme = object : AxisTheme {
-        override val axis: String
-            get() = TODO("Not yet implemented")
+    private val axisTheme = DefaultTheme.minimal2().horizontalAxis(false)
 
-        override fun showLine() = TODO("Not yet implemented")
-        override fun showTickMarks() = TODO("Not yet implemented")
-        override fun showLabels() = TODO("Not yet implemented")
-        override fun showTitle() = TODO("Not yet implemented")
-        override fun showTooltip() = TODO("Not yet implemented")
-        override fun titleStyle(): ThemeTextStyle = TODO("Not yet implemented")
-        override fun titleJustification() = TextJustification(0.5, 1.0)
-        override fun titleMargins() = Thickness()
-        override fun lineWidth() = TODO("Not yet implemented")
-        override fun lineColor() = TODO("Not yet implemented")
-        override fun tickMarkColor() = TODO("Not yet implemented")
-        override fun lineType() = NamedLineType.SOLID
-        override fun tickMarkLineType() = NamedLineType.SOLID
-        override fun labelStyle(): ThemeTextStyle = TODO("Not yet implemented")
-        override fun rotateLabels() = TODO("Not yet implemented")
-        override fun labelAngle() = TODO("Not yet implemented")
-        override fun tickMarkWidth() = TODO("Not yet implemented")
-        override fun tickMarkLength() = TODO("Not yet implemented")
-        override fun tickLabelMargins() = Thickness()
-        override fun tooltipFill() = Color.WHITE
-        override fun tooltipColor() = Color.BLACK
-        override fun tooltipStrokeWidth() = 1.0
-        override fun tooltipLineType() = NamedLineType.SOLID
-        override fun tooltipTextStyle(): ThemeTextStyle =
-            ThemeTextStyle(
-//                    Defaults.FONT_FAMILY_NORMAL,
-                FontFamily.SERIF,
-                FontFace.NORMAL,
-                Defaults.Common.Tooltip.AXIS_TOOLTIP_FONT_SIZE,
-                Color.GRAY
-            )
-
-    }
-
-    private fun createGeneralTooltipSpecs(
-        contextualMapping: ContextualMapping,
-        ctx: PlotContext,
-        hitIndex: Int = 0
-    ): List<TooltipSpec> {
-        return createTooltipSpecs(hitIndex, TipLayoutHint.cursorTooltip(coord = DoubleVector.ZERO), ctx, contextualMapping)
-    }
-
-    private fun createXAxisTooltipSpecs(
-        contextualMapping: ContextualMapping,
-        ctx: PlotContext,
-        hitIndex: Int
-    ): List<TooltipSpec> {
-        return createTooltipSpecs(hitIndex, TipLayoutHint.xAxisTooltip(coord = DoubleVector.ZERO), ctx, contextualMapping)
-    }
-
-    private fun createTooltipSpecs(
-        hitIndex: Int,
-        tipLayoutHint: TipLayoutHint,
-        ctx: PlotContext,
-        contextualMapping: ContextualMapping,
-        aesTipLayoutHints: Map<Aes<*>, TipLayoutHint> = emptyMap()
-    ): List<TooltipSpec> {
-        val factory =
-            TooltipSpecFactory(contextualMapping, DoubleVector.ZERO, flippedAxis = false, axisTheme, axisTheme)
+    private fun createTooltipSpecs(layer: GeomLayer, hitIndex: Int, tipLayoutHint: TipLayoutHint): List<TooltipSpec> {
+        val factory = TooltipSpecFactory(
+            contextualMapping = layer.createContextualMapping(),
+            axisOrigin = ZERO,
+            flippedAxis = false,
+            xAxisTheme = axisTheme,
+            yAxisTheme = axisTheme
+        )
         return factory.create(
-            GeomTarget(
+            geomTarget = GeomTarget(
                 hitIndex = hitIndex,
                 tipLayoutHint = tipLayoutHint,
-                aesTipLayoutHints = aesTipLayoutHints
+                aesTipLayoutHints = emptyMap()
             ),
-            ctx
+            ctx = TestingPlotContext.create(layer)
         )
     }
 
     internal fun assertXAxisTooltip(layer: GeomLayer, expectedLines: List<String>, hitIndex: Int = 0) {
-        val ctx = TestingPlotContext.create(layer)
-        val tooltipSpecs = createXAxisTooltipSpecs(layer.createContextualMapping(), ctx, hitIndex)
+        val tooltipSpecs = createTooltipSpecs(layer, hitIndex, TipLayoutHint.xAxisTooltip(coord = ZERO))
         val actualXAxisTooltip = tooltipSpecs.filter { it.layoutHint.kind == TipLayoutHint.Kind.X_AXIS_TOOLTIP }
 
         assertEquals(expectedLines.isEmpty(), actualXAxisTooltip.isEmpty())
@@ -116,9 +51,8 @@ object TooltipTestUtil {
         }
     }
 
-    internal fun assertGeneralTooltip(layer: GeomLayer, expectedLines: List<String>) {
-        val ctx = TestingPlotContext.create(layer)
-        val tooltipSpecs = createGeneralTooltipSpecs(layer.createContextualMapping(), ctx)
+    internal fun assertGeneralTooltip(layer: GeomLayer, expectedLines: List<String>, hitIndex: Int = 0) {
+        val tooltipSpecs = createTooltipSpecs(layer, hitIndex, TipLayoutHint.cursorTooltip(coord = ZERO))
         val actualGeneralTooltips = tooltipSpecs.filterNot(TooltipSpec::isSide)
 
         assertEquals(expectedLines.isEmpty(), actualGeneralTooltips.isEmpty())
