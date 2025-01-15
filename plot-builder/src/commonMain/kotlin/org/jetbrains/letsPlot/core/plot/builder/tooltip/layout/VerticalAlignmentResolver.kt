2c1004a4b288cc245ded42c2a7f5e03aaa22d15c
Ivan Kupriyanov <ikupriyanov@horis.ru>
2025-01-15 12:14:04 +0100

    Refactor VerticalAlignmentResolver to simplify rule matching.

--- plot-builder/src/commonMain/kotlin/org/jetbrains/letsPlot/core/plot/builder/tooltip/layout/VerticalAlignmentResolver.kt
+++ plot-builder/src/commonMain/kotlin/org/jetbrains/letsPlot/core/plot/builder/tooltip/layout/VerticalAlignmentResolver.kt
@@ -17,138 +17,55 @@ internal class VerticalAlignmentResolver(private val myVerticalSpace: DoubleSpan
         preferredPlacement: VerticalAlignment,
         cursorRange: DoubleSpan
     ): VerticalAlignment {
-        val currentState = Matcher()
-            .topCursorOk(!topPlacementRange.connected(cursorRange))
-            .topSpaceOk(topPlacementRange in myVerticalSpace)
-            .bottomCursorOk(!bottomPlacementRange.connected(cursorRange))
-            .bottomSpaceOk(bottomPlacementRange in myVerticalSpace)
-            .preferredAlignment(preferredPlacement)
+        val topCursorOk = !topPlacementRange.connected(cursorRange)
+        val topSpaceOk = topPlacementRange in myVerticalSpace
+        val bottomCursorOk = !bottomPlacementRange.connected(cursorRange)
+        val bottomSpaceOk = bottomPlacementRange in myVerticalSpace
+        val preferredAlignment = preferredPlacement
 
-        for (matcher in PLACEMENT_MATCHERS) {
-            if (matcher.first.match(currentState)) {
-                return matcher.second
-            }
+        val matchedRule = RULES.first {
+            it.matches(
+                topSpaceOk = topSpaceOk,
+                topCursorOk = topCursorOk,
+                bottomSpaceOk = bottomSpaceOk,
+                bottomCursorOk = bottomCursorOk,
+                preferredAlignment = preferredAlignment
+            )
         }
 
-        throw IllegalStateException("Some matcher should match")
+        return matchedRule.resultAlignment
     }
 
-    internal class Matcher {
-
-        private var myTopSpaceOk: Boolean? = null
-        private var myTopCursorOk: Boolean? = null
-        private var myBottomSpaceOk: Boolean? = null
-        private var myBottomCursorOk: Boolean? = null
-        private var myPreferredAlignment: VerticalAlignment? = null
-
-        fun match(other: Matcher): Boolean {
-            return (match({ getBottomCursorOk(it) }, other) &&
-                    match({ getBottomSpaceOk(it) }, other) &&
-                    match({ getTopCursorOk(it) }, other) &&
-                    match({ getTopSpaceOk(it) }, other) &&
-                    match({ getPreferredAlignment(it) }, other))
-
-        }
-
-        fun topSpaceOk(topSpaceOk: Boolean?): Matcher {
-            myTopSpaceOk = topSpaceOk
-            return this
-        }
-
-        fun topCursorOk(topCursorOk: Boolean?): Matcher {
-            myTopCursorOk = topCursorOk
-            return this
-        }
-
-        fun bottomSpaceOk(bottomSpaceOk: Boolean?): Matcher {
-            myBottomSpaceOk = bottomSpaceOk
-            return this
-        }
-
-        fun bottomCursorOk(bottomCursorOk: Boolean?): Matcher {
-            myBottomCursorOk = bottomCursorOk
-            return this
-        }
-
-        fun preferredAlignment(preferredAlignment: VerticalAlignment): Matcher {
-            myPreferredAlignment = preferredAlignment
-            return this
-        }
-
-        private fun <T> match(propertyGetter: (Matcher) -> T, actual: Matcher): Boolean {
-            val expected = propertyGetter(this) ?: return true
-
-            return expected == propertyGetter(actual)
-        }
-
-        companion object {
-            fun getTopSpaceOk(matcher: Matcher): Boolean? {
-                return matcher.myTopSpaceOk
-            }
-
-            fun getTopCursorOk(matcher: Matcher): Boolean? {
-                return matcher.myTopCursorOk
-            }
-
-            fun getBottomSpaceOk(matcher: Matcher): Boolean? {
-                return matcher.myBottomSpaceOk
-            }
-
-            fun getBottomCursorOk(matcher: Matcher): Boolean? {
-                return matcher.myBottomCursorOk
-            }
-
-            fun getPreferredAlignment(matcher: Matcher): VerticalAlignment? {
-                return matcher.myPreferredAlignment
-            }
-        }
+    internal class Rule(
+        val resultAlignment: VerticalAlignment,
+        private val topSpaceOk: Boolean? = null,
+        private val topCursorOk: Boolean? = null,
+        private val bottomSpaceOk: Boolean? = null,
+        private val bottomCursorOk: Boolean? = null,
+        private val preferredAlignment: VerticalAlignment? = null
+    ) {
+        fun matches(
+            topSpaceOk: Boolean,
+            topCursorOk: Boolean,
+            bottomSpaceOk: Boolean,
+            bottomCursorOk: Boolean,
+            preferredAlignment: VerticalAlignment
+        ) =
+            (this.bottomCursorOk?.equals(bottomCursorOk) != false) &&
+                    (this.bottomSpaceOk?.equals(bottomSpaceOk) != false) &&
+                    (this.topCursorOk?.equals(topCursorOk) != false) &&
+                    (this.topSpaceOk?.equals(topSpaceOk) != false) &&
+                    (this.preferredAlignment?.equals(preferredAlignment) != false)
     }
 
     companion object {
-        private val PLACEMENT_MATCHERS = listOf(
-            rule(
-                Matcher()
-                    .preferredAlignment(TOP)
-                    .topSpaceOk(true)
-                    .topCursorOk(true),
-                TOP
-            ),
-
-            rule(
-                Matcher()
-                    .preferredAlignment(BOTTOM)
-                    .bottomSpaceOk(true)
-                    .bottomCursorOk(true),
-                BOTTOM
-            ),
-
-            rule(
-                Matcher()
-                    .bottomSpaceOk(true),
-                BOTTOM
-            ),
-
-            rule(
-                Matcher()
-                    .topSpaceOk(true),
-                TOP
-            ),
-
-            rule(
-                Matcher()
-                    .topSpaceOk(false)
-                    .bottomSpaceOk(false),
-                FIT
-            ),
-
-            rule(
-                Matcher(),
-                FIT
-            )
+        private val RULES = listOf(
+            Rule(resultAlignment = TOP, preferredAlignment = TOP, topSpaceOk = true, topCursorOk = true),
+            Rule(resultAlignment = BOTTOM, preferredAlignment = BOTTOM, bottomSpaceOk = true, bottomCursorOk = true),
+            Rule(resultAlignment = BOTTOM, bottomSpaceOk = true),
+            Rule(resultAlignment = TOP, topSpaceOk = true),
+            Rule(resultAlignment = FIT, topSpaceOk = false, bottomSpaceOk = false),
+            Rule(resultAlignment = FIT)
         )
-
-        private fun rule(condition: Matcher, result: VerticalAlignment): Pair<Matcher, VerticalAlignment> {
-            return Pair(condition, result)
-        }
     }
 }
