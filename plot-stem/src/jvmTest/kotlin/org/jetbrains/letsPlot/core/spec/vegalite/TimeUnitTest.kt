bbfbf91032f3ab717bbbe1e568c6b248b82385ea
Ivan Kupriyanov <ikupriyanov@horis.ru>
2025-02-04 21:44:15 +0100

    Vega-Lite: support timeUnit parameter

---
+++ plot-stem/src/jvmTest/kotlin/org/jetbrains/letsPlot/core/spec/vegalite/TimeUnitTest.kt
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2025. JetBrains s.r.o.
+ * Use of this source code is governed by the MIT license that can be found in the LICENSE file.
+ */
+
+package org.jetbrains.letsPlot.core.spec.vegalite
+
+import org.assertj.core.api.Assertions.assertThat
+import org.jetbrains.letsPlot.commons.intern.datetime.Date
+import org.jetbrains.letsPlot.commons.intern.datetime.DateTime
+import org.jetbrains.letsPlot.commons.intern.datetime.Month
+import org.jetbrains.letsPlot.commons.intern.datetime.Time
+import kotlin.test.Test
+
+class TimeUnitTest {
+    @Test
+    fun simple() {
+        val dt = DateTime(
+            Date(day = 8, month = Month.MARCH, year = 2010),
+            Time(hours = 12, minutes = 34, seconds = 56, milliseconds = 789)
+        )
+
+        assertThat(Util.applyTimeUnit(dt, "year"))
+            .isEqualTo(
+                DateTime(
+                    Date(day = 1, month = Month.JANUARY, year = 2010),
+                    Time(hours = 0, minutes = 0, seconds = 0 )
+                )
+            )
+    }
+
+    @Test
+    fun `year and month`() {
+        val dt = DateTime(
+            Date(day = 8, month = Month.MARCH, year = 2010),
+            Time(hours = 12, minutes = 34, seconds = 56, milliseconds = 789)
+        )
+
+        assertThat(Util.applyTimeUnit(dt, "yearmonth"))
+            .isEqualTo(
+                DateTime(
+                    Date(day = 1, month = Month.MARCH, year = 2010),
+                    Time(hours = 0, minutes = 0, seconds = 0 )
+                )
+            )
+    }
+
+}
