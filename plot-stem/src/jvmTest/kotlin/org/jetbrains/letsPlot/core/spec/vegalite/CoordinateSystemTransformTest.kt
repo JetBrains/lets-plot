3995853aa415226dc9e8eddad8bad88749a168b0
Ivan Kupriyanov <ikupriyanov@horis.ru>
2025-02-10 18:53:35 +0100

    Vega-Lite: fix test

--- plot-stem/src/jvmTest/kotlin/org/jetbrains/letsPlot/core/spec/vegalite/CoordinateSystemTransformTest.kt
+++ plot-stem/src/jvmTest/kotlin/org/jetbrains/letsPlot/core/spec/vegalite/CoordinateSystemTransformTest.kt
@@ -20,7 +20,6 @@ import org.jetbrains.letsPlot.core.spec.Option.PlotBase
 import org.jetbrains.letsPlot.core.spec.asMutable
 import org.jetbrains.letsPlot.core.spec.back.SpecTransformBackendUtil
 import org.jetbrains.letsPlot.core.spec.getMap
-import org.junit.Ignore
 import org.junit.Test
 import java.util.Map.entry
 
@@ -64,7 +63,7 @@ class CoordinateSystemTransformTest {
         val plotSpec = SpecTransformBackendUtil.processTransform(vegaSpec)
 
         assertThat(plotSpec.getMap(Option.Plot.COORD)).containsOnly(
-            entry(Option.Meta.NAME, Option.CoordName.CARTESIAN),
+            entry(Meta.NAME, Option.CoordName.CARTESIAN),
             entry(Option.Coord.X_LIM, listOf(300.0, 450.0)),
             entry(Option.Coord.Y_LIM, listOf(1.5, 3.0))
         )
@@ -108,13 +107,12 @@ class CoordinateSystemTransformTest {
         val plotSpec = SpecTransformBackendUtil.processTransform(vegaSpec)
 
         assertThat(plotSpec.getMap(Option.Plot.COORD)).containsOnly(
-            entry(Option.Meta.NAME, Option.CoordName.CARTESIAN),
+            entry(Meta.NAME, Option.CoordName.CARTESIAN),
             entry(Option.Coord.X_LIM, listOf(300.0, null)),
             entry(Option.Coord.Y_LIM, listOf(null, 3.0))
         )
     }
 
-    @Ignore
     @Test
     fun `lonlat channel should produce xy mapping with coord_map`() {
         val vegaSpec = parseJson(
@@ -140,9 +138,11 @@ class CoordinateSystemTransformTest {
 
         assertThat(plotSpec.getMap(PlotBase.DATA)).isNull()
         assertThat(plotSpec.getMap(PlotBase.MAPPING)).isNull()
-        assertThat(plotSpec.getMap(Plot.COORD)).containsOnly(entry(Option.Meta.NAME, CoordName.MAP))
+        assertThat(plotSpec.getMap(Plot.COORD)).containsOnly(entry(Meta.NAME, CoordName.MAP))
         assertThat(plotSpec.getMap(Plot.LAYERS, 0)).containsOnly(
             entry(Layer.GEOM, fromGeomKind(GeomKind.LIVE_MAP)),
+            entry(Option.Geom.LiveMap.CONST_SIZE_ZOOMIN, 0),
+            entry(Option.Geom.LiveMap.DATA_SIZE_ZOOMIN, 0),
             entry(PlotBase.DATA, empty()),
             entry(Option.Geom.LiveMap.TILES, mapOf(
                 Option.Geom.LiveMap.Tile.KIND to Option.Geom.LiveMap.Tile.KIND_VECTOR_LETS_PLOT,
