31ca250bef3acb357981e88e5d4d1fe1a0081eff
Igor Alshannikov <Igor.Alshannikov@jetbrains.com>
2025-03-27 16:07:25 -0400

    fat jar script proj.: add (c)

--- jvm-package/jvm-publish-idea-lets-plot-plugin/build.gradle.kts
+++ jvm-package/jvm-publish-idea-lets-plot-plugin/build.gradle.kts
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2025. JetBrains s.r.o.
+ * Use of this source code is governed by the MIT license that can be found in the LICENSE file.
+ */
+
 plugins {
     kotlin("jvm")
     `maven-publish`
