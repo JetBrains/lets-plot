813bd50fe09b5144583fadb9d41c2db0e5784d2b
Ivan Kupriyanov <ikupriyanov@horis.ru>
2025-05-27 21:43:07 +0200

    Refactor MagickCanvas: implement snapshot support, extend Context2d functionality with drawImage method, get rid of ScaledCanvas and ScaledContext2d in MagickCanvas

--- platf-imagick/src/nativeMain/kotlin/org/jetbrains/letsPlot/imagick/canvas/MagickCanvas.kt
+++ platf-imagick/src/nativeMain/kotlin/org/jetbrains/letsPlot/imagick/canvas/MagickCanvas.kt
@@ -9,19 +9,21 @@ import kotlinx.cinterop.CPointer
 import kotlinx.cinterop.toKString
 import org.jetbrains.letsPlot.commons.geometry.Vector
 import org.jetbrains.letsPlot.commons.intern.async.Async
+import org.jetbrains.letsPlot.commons.intern.async.Asyncs
+import org.jetbrains.letsPlot.commons.registration.Disposable
 import org.jetbrains.letsPlot.core.canvas.Canvas
+import org.jetbrains.letsPlot.core.canvas.Context2d
 import org.jetbrains.letsPlot.core.canvas.ScaledCanvas
 
 class MagickCanvas(
-    private val _img: CPointer<ImageMagick.MagickWand>?,
-    size: Vector,
+    private val _img: CPointer<ImageMagick.MagickWand>,
+    override val size: Vector,
     pixelDensity: Double,
-) : ScaledCanvas(MagickContext2d(_img), size, pixelDensity) {
+) : Canvas {
 
     // TODO: replace usage in tests with Snapshot
     val img: CPointer<ImageMagick.MagickWand>? get ()  {
         val wand = (context2d as MagickContext2d).wand
-
         if (false) {
             val v = ImageMagick.DrawGetVectorGraphics(wand)
             println(v!!.toKString())
@@ -31,12 +33,15 @@ class MagickCanvas(
         return _img
     }
 
+    override val context2d: Context2d = MagickContext2d(_img, pixelDensity)
+
+
     override fun takeSnapshot(): Async<Canvas.Snapshot> {
-        TODO("Not yet implemented")
+        return Asyncs.constant(immidiateSnapshot())
     }
 
     override fun immidiateSnapshot(): Canvas.Snapshot {
-        TODO("Not yet implemented")
+        return MagickSnapshot(_img)
     }
 
     fun saveBmp(filename: String) {
@@ -51,11 +56,32 @@ class MagickCanvas(
         }
 
         fun create(size: Vector, pixelDensity: Number): MagickCanvas {
-            val wand = ImageMagick.NewMagickWand()
+            val wand = ImageMagick.NewMagickWand() ?: error("MagickCanvas: Failed to create new MagickWand")
             val background = ImageMagick.NewPixelWand()
             ImageMagick.PixelSetColor(background, "white")
             ImageMagick.MagickNewImage(wand, size.x.toULong(), size.y.toULong(), background)
             return MagickCanvas(wand, size, pixelDensity = pixelDensity.toDouble())
         }
+    }
+
+    class MagickSnapshot(
+        img: CPointer<ImageMagick.MagickWand>
+    ) : Disposable, Canvas.Snapshot {
+        val img: CPointer<ImageMagick.MagickWand>
+
+        init {
+            //ImageMagick.DrawImage(img)
+            this.img = ImageMagick.CloneMagickWand(img) ?: error("MagickSnapshot: Failed to clone image wand")
+        }
+
+        override fun dispose() {
+            ImageMagick.DestroyMagickWand(img)
+        }
+
+        override fun copy(): Canvas.Snapshot {
+            val copiedImg = ImageMagick.CloneMagickWand(img) ?: error("MagickSnapshot: Failed to clone image wand")
+            return MagickSnapshot(copiedImg)
+        }
+
     }
 }
