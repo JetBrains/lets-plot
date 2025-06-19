3ca5d9f35cdc8c9ee645af5759acf7fb6cefa399
Ivan Seleznev <iseleznev@horis.ru>
2025-06-19 13:19:30 +0200

    [ref] geom_bar() annotation

--- plot-base/src/commonMain/kotlin/org/jetbrains/letsPlot/core/plot/base/geom/BarGeom.kt
+++ plot-base/src/commonMain/kotlin/org/jetbrains/letsPlot/core/plot/base/geom/BarGeom.kt
@@ -9,10 +9,8 @@ import org.jetbrains.letsPlot.commons.geometry.DoubleRectangle
 import org.jetbrains.letsPlot.commons.geometry.DoubleVector
 import org.jetbrains.letsPlot.core.plot.base.*
 import org.jetbrains.letsPlot.core.plot.base.geom.annotation.BarAnnotation
-import org.jetbrains.letsPlot.core.plot.base.geom.util.RectangleTooltipHelper
-import org.jetbrains.letsPlot.core.plot.base.geom.util.RectanglesHelper
+import org.jetbrains.letsPlot.core.plot.base.geom.util.*
 import org.jetbrains.letsPlot.core.plot.base.render.SvgRoot
-import org.jetbrains.letsPlot.datamodel.svg.dom.SvgNode
 
 open class BarGeom : GeomBase() {
 
@@ -25,62 +23,50 @@ open class BarGeom : GeomBase() {
         coord: CoordinateSystem,
         ctx: GeomContext
     ) {
-        val helper = RectanglesHelper(aesthetics, pos, coord, ctx, visualRectByDataPoint(ctx))
-        val tooltipHelper = RectangleTooltipHelper(pos, coord, ctx)
-        val rectangles = mutableListOf<SvgNode>()
-        if (coord.isLinear) {
-            helper.createRectangles { _, svgNode, _ -> rectangles.add(svgNode) }
+        val dataPoints = aesthetics.dataPoints()
+        val linesHelper = LinesHelper(pos, coord, ctx)
+        linesHelper.setResamplingEnabled(!coord.isLinear)
 
-            // Snap tooltips to the proper side (e.g. bottom for negative values, right for coord_flip)
-            val hintHelper = RectanglesHelper(aesthetics, pos, coord, ctx, hintRectByDataPoint(ctx))
-            hintHelper.createRectangles { aes, _, rect -> tooltipHelper.addTarget(aes, rect) }
-        } else {
-            helper.createNonLinearRectangles { aes, svgNode, polygon ->
-                rectangles.add(svgNode)
-                tooltipHelper.addTarget(aes, polygon)
-            }
+        val targetCollectorHelper = TargetCollectorHelper(GeomKind.BAR, ctx)
+        val polygons = linesHelper.createRectPolygon(dataPoints, polygonByDataPoint(ctx))
+
+        polygons.forEach { (svg, polygonData) ->
+            targetCollectorHelper.addPolygons(polygonData)
+            root.add(svg)
         }
-        rectangles.reverse() // TODO: why reverse?
-        rectangles.forEach(root::add)
 
-        ctx.annotation?.let { BarAnnotation.build(root, helper, coord, ctx) }
+        ctx.annotation?.let {
+            BarAnnotation.build(
+                root,
+                polygons.map { (_, polygonData) -> polygonData },
+                ::rectByDataPoint,
+                linesHelper,
+                coord,
+                ctx
+            )
+        }
     }
 
     companion object {
         const val HANDLES_GROUPS = false
-        private fun hintRectByDataPoint(ctx: GeomContext): (DataPointAesthetics) -> DoubleRectangle? {
-            fun factory(p: DataPointAesthetics): DoubleRectangle? {
-                val (x, y, width ) = p.finiteOrNull(Aes.X, Aes.Y, Aes.WIDTH) ?: return null
 
-                val w = width * ctx.getResolution(Aes.X)
-                val origin = DoubleVector(x - w / 2, y)
-                val dimension = DoubleVector(w, 0.0)
-                return DoubleRectangle(origin, dimension)
+        private fun polygonByDataPoint(ctx: GeomContext): (DataPointAesthetics) -> List<DoubleVector>? {
+            fun factory(p: DataPointAesthetics): List<DoubleVector>? {
+                return rectByDataPoint(p, ctx)?.points
             }
 
             return ::factory
         }
 
-        private fun visualRectByDataPoint(ctx: GeomContext): (DataPointAesthetics) -> DoubleRectangle? {
-            fun factory(p: DataPointAesthetics): DoubleRectangle? {
-                val (x, y, width ) = p.finiteOrNull(Aes.X, Aes.Y, Aes.WIDTH) ?: return null
+        private fun rectByDataPoint(p: DataPointAesthetics, ctx: GeomContext): DoubleRectangle? {
+            val (x, y, width) = p.finiteOrNull(Aes.X, Aes.Y, Aes.WIDTH) ?: return null
 
-                val w = width * ctx.getResolution(Aes.X)
-                val origin: DoubleVector
-                val dimension: DoubleVector
+            val w = width * ctx.getResolution(Aes.X)
 
-                if (y >= 0) {
-                    origin = DoubleVector(x - w / 2, 0.0)
-                    dimension = DoubleVector(w, y)
-                } else {
-                    origin = DoubleVector(x - w / 2, y)
-                    dimension = DoubleVector(w, -y)
-                }
+            val origin = DoubleVector(x - w / 2, 0.0)
+            val dimension = DoubleVector(w, y)
 
-                return DoubleRectangle(origin, dimension)
-            }
-
-            return ::factory
+            return DoubleRectangle(origin, dimension)
         }
     }
 }
