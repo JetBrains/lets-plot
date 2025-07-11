1dee6e23b4ca8de4e7aef703268d45d32c6b3302
Ivan Kupriyanov <ikupriyanov@horis.ru>
2025-07-11 13:23:40 +0200

    waterfall_plot: fix label='blank'

--- plot-stem/src/jvmTest/kotlin/org/jetbrains/letsPlot/core/spec/back/transform/bistro/waterfall/WaterfallPlotSpecChangeTest.kt
+++ plot-stem/src/jvmTest/kotlin/org/jetbrains/letsPlot/core/spec/back/transform/bistro/waterfall/WaterfallPlotSpecChangeTest.kt
@@ -9,6 +9,7 @@ import demoAndTestShared.parsePlotSpec
 import org.assertj.core.api.Assertions.assertThat
 import org.jetbrains.letsPlot.core.spec.back.transform.PlotConfigBackendTransforms
 import org.jetbrains.letsPlot.core.spec.getBool
+import org.jetbrains.letsPlot.core.spec.getString
 import kotlin.test.Test
 
 class WaterfallPlotSpecChangeTest {
@@ -100,6 +101,17 @@ class WaterfallPlotSpecChangeTest {
 
     @Test
     fun `waterfall_plot(color='flow_type', label=element_text(color='red'), relative_labels=layer_labels()-inherit_color())`() {
+        /*
+        waterfall_plot({'cat': ['A', 'B'], 'val': [2, -1]}, 'cat', 'val',
+               size=.75,
+               fill='gray90',
+               color='flow_type',
+               label=element_text(color='white'),
+               relative_labels=layer_labels()
+                   .line('@..dy..')
+                   .inherit_color()
+              )
+         */
         val spec = """
             |{
             |  "data": {
@@ -139,5 +151,38 @@ class WaterfallPlotSpecChangeTest {
 
         assertThat(resSpec.getBool("layers", 1, "labels", "use_layer_color")).isTrue()
         assertThat(resSpec.getBool("layers", 2, "labels", "use_layer_color")).isFalse()
+    }
+
+    @Test
+    fun `waterfall_plot(label='blank')`() {
+        // waterfall_plot({'cat': ['A', 'B'], 'val': [2, -1]}, 'cat', 'val', label='blank')
+        val spec = """
+            |{
+            |  "data": {
+            |    "cat": [ "A", "B" ],
+            |    "val": [ 2.0, -1.0 ]
+            |  },
+            |  "bistro": {
+            |    "name": "waterfall",
+            |    "x": "cat",
+            |    "y": "val",
+            |    "label": "blank",
+            |    "background_layers": []
+            |  },
+            |  "data_meta": {
+            |    "series_annotations": [
+            |      { "type": "str", "column": "cat" },
+            |      { "type": "int", "column": "val" }
+            |    ]
+            |  },
+            |  "kind": "plot"
+            |}
+        """.trimMargin()
+
+        val plotSpec = parsePlotSpec(spec)
+        val resSpec = PlotConfigBackendTransforms.bistroTransform().apply(plotSpec)
+
+        assertThat(resSpec.getString("layers", 1, "labels")).isEqualTo("none")
+        assertThat(resSpec.getString("layers", 2, "labels")).isEqualTo("none")
     }
 }
