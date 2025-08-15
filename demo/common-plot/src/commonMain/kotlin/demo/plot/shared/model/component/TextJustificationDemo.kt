bdbb28c0690be73f4bf808af4a6d44e5056672dc
Artem Smirnov <asmirnov@horis.ru>
2025-08-15 13:19:19 +0200

    Tiny refactor.

--- demo/common-plot/src/commonMain/kotlin/demo/plot/shared/model/component/TextJustificationDemo.kt
+++ demo/common-plot/src/commonMain/kotlin/demo/plot/shared/model/component/TextJustificationDemo.kt
@@ -115,7 +115,7 @@ class TextJustificationDemo : SimpleDemoBase(DEMO_BOX_SIZE) {
                 justification,
                 rotation
             )
-            label.setLineHeights((0 until label.linesCount()).map { lineHeight }) // TODO
+            label.setLineHeights(List(label.linesCount()) { lineHeight })
             label.setHorizontalAnchor(hAnchor)
             rotation?.angle?.let(label::rotate)
             label.moveTo(position)
