f63c555fbe8fb56afc74451d63d38ffcce052638
Ivan Kupriyanov <ikupriyanov@horis.ru>
2025-08-26 14:28:04 +0200

    platf-awt: replace int-based Graphics2d.fillRect(...) method with Graphics2d.fill(Rectangle2d.Double(...)) to fix gaps on geom_bin2d()

--- platf-awt/src/main/kotlin/org/jetbrains/letsPlot/awt/canvas/AwtContext2d.kt
+++ platf-awt/src/main/kotlin/org/jetbrains/letsPlot/awt/canvas/AwtContext2d.kt
@@ -168,7 +168,8 @@ internal class AwtContext2d(private val graphics: Graphics2D) : Context2d {
 
     override fun fillRect(x: Double, y: Double, w: Double, h: Double) {
         graphics.color = state.fillColor
-        graphics.fillRect(x.toInt(), y.toInt(), w.toInt(), h.toInt())
+        val rect = Rectangle2D.Double(x, y, w, h)
+        graphics.fill(rect)
     }
 
     override fun moveTo(x: Double, y: Double) {
