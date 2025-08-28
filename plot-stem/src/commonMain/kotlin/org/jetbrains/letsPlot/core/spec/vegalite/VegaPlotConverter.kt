49b60f0260541d040e00fb4a948d84d8b390e8b9
Ivan Kupriyanov <ikupriyanov@horis.ru>
2025-08-28 13:28:20 +0200

    Make VegaPlotConverter public

--- plot-stem/src/commonMain/kotlin/org/jetbrains/letsPlot/core/spec/vegalite/VegaPlotConverter.kt
+++ plot-stem/src/commonMain/kotlin/org/jetbrains/letsPlot/core/spec/vegalite/VegaPlotConverter.kt
@@ -29,7 +29,7 @@ import org.jetbrains.letsPlot.core.spec.vegalite.VegaOption.LetsPlotExt
 import org.jetbrains.letsPlot.core.spec.vegalite.VegaOption.Mark
 import kotlin.math.sqrt
 
-internal class VegaPlotConverter private constructor(
+class VegaPlotConverter private constructor(
     private val vegaPlotSpecMap: MutableMap<String, Any?>
 ) {
     private val accessLogger = TraceableMapWrapper.AccessLogger()
