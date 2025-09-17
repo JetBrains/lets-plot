d0c952a0d6c2fc023a2f0721c28293d29e79a7fe
Ivan Seleznev <iseleznev@horis.ru>
2025-09-17 10:39:02 +0200

    Prevent zoom out

--- plot-base/src/commonMain/kotlin/org/jetbrains/letsPlot/core/plot/base/geom/PointGeom.kt
+++ plot-base/src/commonMain/kotlin/org/jetbrains/letsPlot/core/plot/base/geom/PointGeom.kt
@@ -49,7 +49,8 @@ open class PointGeom : GeomBase() {
             // but Shape.size() can't be used because it takes DataPointAesthetics as param
 
             val scaleFactor = if (sizeUnit.isNullOrBlank()) {
-                ctx.getScaleFactor()
+                val factor = ctx.getScaleFactor()
+                if (factor >= 1) factor else 1.0 // do not enlarge points if scaleFactor < 1
             } else {
                 AesScaling.sizeUnitRatio(point, coord, sizeUnit, AesScaling.POINT_UNIT_SIZE)
             }
