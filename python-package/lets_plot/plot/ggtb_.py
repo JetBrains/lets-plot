bf1b68751c71e6468ed183f4b703b40adc83b44e
Ivan Seleznev <iseleznev@horis.ru>
2025-09-18 14:29:59 +0200

    Fix ggtb() docs

--- python-package/lets_plot/plot/ggtb_.py
+++ python-package/lets_plot/plot/ggtb_.py
@@ -33,21 +33,21 @@ def ggtb(size_basis=None, size_zoomin=None) -> FeatureSpec:
 
     Parameters
     ----------
+    size_zoomin : int, default=0
+        Control how zooming in affects the size of geometry objects on the plot. Currently, works only with
+        the geom_point layer and layers based on it (geom_jitter, geom_sina, etc.).
+        0 - size never increases;
+        -1 - size will be increasing without limits;
+        n - the number of times the size of objects will increase (relative to the initial state of the plot).
+        Farther zooming will no longer affect the size.
     size_basis: String, default="max" {'x', 'y', 'min', 'max'}
-        Control how zooming of the map widget affects the scale of geometry objects on plot.
+        Defines the axis along which the scaling factor for geometry objects will be calculated.
         'x' - size changes only when zooming in/out along x-axis;
         'y' - size changes only when zooming in/out along y-axis;
         'min' - size changes when zooming in/out along any axis, but the change is determined by the axis
                with the minimum zoom factor;
         'max' - size changes when zooming in/out along any axis, but the change is determined by the axis
                with the maximum zoom factor.
-    size_zoomin : int, default=0
-        Control how zooming-in of the map widget increases size of geometry objects on plot
-        when the size is set by means of mapping between the data and the ``size`` aesthetic.
-        0 - size never increases;
-        -1 - size will be increasing without limits;
-        n - the number of times the size of objects will increase (relative to the initial state of the plot).
-        Farther zooming will no longer affect the size.
 
     Returns
     -------
