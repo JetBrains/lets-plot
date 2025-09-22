1064a243bbe52d8957a50ec8163a33133f2bcf1b
Mikhail Koroteev <horis.mkoroteev@gmail.com>
2025-09-22 15:26:54 +0300

    Add more string literals for the legend_justification parameter, issue #1028

--- plot-base/src/commonMain/kotlin/org/jetbrains/letsPlot/core/plot/base/guide/LegendJustification.kt
+++ plot-base/src/commonMain/kotlin/org/jetbrains/letsPlot/core/plot/base/guide/LegendJustification.kt
@@ -6,10 +6,14 @@
 package org.jetbrains.letsPlot.core.plot.base.guide
 
 /**
- * anchor point for positioning legend inside plot ("center" or two-element numeric vector)
+ * anchor point for positioning legend inside the area defined by legend_position ('center', 'left', 'right', 'top', 'bottom' or a two-element numeric vector)
  */
 class LegendJustification(val x: Double, val y: Double) {
     companion object {
         val CENTER = LegendJustification(0.5, 0.5)
+        val LEFT = LegendJustification(0.0, 0.5)
+        val RIGHT = LegendJustification(1.0, 0.5)
+        val TOP = LegendJustification(0.5, 1.0)
+        val BOTTOM = LegendJustification(0.5, 0.0)
     }
 }
