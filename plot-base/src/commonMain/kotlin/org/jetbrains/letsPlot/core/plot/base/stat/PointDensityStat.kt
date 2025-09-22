84e5b19d215753ee5576d0ecac786ad9980ed144
Artem Smirnov <asmirnov@horis.ru>
2025-09-22 17:37:19 +0200

    Refactor PointDensityStat to extend AbstractDensity2dStat and implement KDE2D method for density calculations.

--- plot-base/src/commonMain/kotlin/org/jetbrains/letsPlot/core/plot/base/stat/PointDensityStat.kt
+++ plot-base/src/commonMain/kotlin/org/jetbrains/letsPlot/core/plot/base/stat/PointDensityStat.kt
@@ -14,9 +14,21 @@ import org.jetbrains.letsPlot.core.plot.base.StatContext
 import org.jetbrains.letsPlot.core.plot.base.data.TransformVar
 
 class PointDensityStat(
-    private val adjust: Double,
+    adjust: Double,
     private val method: Method
-) : BaseStat(DEF_MAPPING) {
+) : AbstractDensity2dStat(
+    defaultMappings = DEF_MAPPING,
+    bandWidthX = null, // TODO
+    bandWidthY = null, // TODO
+    bandWidthMethod = DensityStat.BandWidthMethod.NRD, // TODO
+    adjust = adjust,
+    kernel = DensityStat.Kernel.GAUSSIAN, // TODO
+    nX = DEF_N, // TODO
+    nY = DEF_N, // TODO
+    isContour = false,
+    binCount = DEF_BIN_COUNT,
+    binWidth = DEF_BIN_WIDTH
+) {
 
     override fun consumes(): List<Aes<*>> {
         return listOf(Aes.X, Aes.Y)
@@ -54,16 +66,7 @@ class PointDensityStat(
     ): Map<DataFrame.Variable, List<Double>> {
         return when (method) {
             Method.NEIGHBOURS -> buildNeighboursStat(xValues, yValues, xRange, yRange)
-            Method.KDE2D -> {
-                // TODO
-                mapOf(
-                    Stats.X to emptyList(),
-                    Stats.Y to emptyList(),
-                    Stats.COUNT to emptyList(),
-                    Stats.DENSITY to emptyList(),
-                    Stats.SCALED to emptyList()
-                )
-            }
+            Method.KDE2D -> buildKde2dStat(xValues, yValues, xRange, yRange)
         }
     }
 
@@ -81,6 +84,43 @@ class PointDensityStat(
         val statDensity = statCount.map { it / statCount.size } // Never divide by zero - no mapping if no points
         val maxCount = statCount.maxOrNull() ?: 0.0 // null only if there are no points (each point counts itself)
         val statScaled = statCount.map { it / maxCount } // Never divide by zero - no mapping if no points
+        return mapOf(
+            Stats.X to xValues,
+            Stats.Y to yValues,
+            Stats.COUNT to statCount,
+            Stats.DENSITY to statDensity,
+            Stats.SCALED to statScaled
+        )
+    }
+
+    private fun buildKde2dStat(
+        xValues: List<Double>,
+        yValues: List<Double>,
+        xRange: DoubleSpan,
+        yRange: DoubleSpan
+    ): Map<DataFrame.Variable, List<Double>> {
+        val weights = List(xValues.size) { 1.0 } // TODO
+
+        val statCount = ArrayList<Double>()
+        val statDensity = ArrayList<Double>()
+        val statScaled = ArrayList<Double>()
+
+        val (stepsX, stepsY, densityMatrix) = density2dGrid(xValues, yValues, weights, xRange, yRange)
+
+        val indices = findInterval(yValues, stepsY) zip findInterval(xValues, stepsX)
+        val weightsSum = SeriesUtil.sum(weights)
+        for ((row, col) in indices) {
+            val count = densityMatrix.getEntry(row, col)
+            statCount.add(count)
+            statDensity.add(count / weightsSum)
+        }
+
+        statCount.maxOrNull()?.let { maxCount ->
+            for (d in statCount) {
+                statScaled.add(d / maxCount)
+            }
+        }
+
         return mapOf(
             Stats.X to xValues,
             Stats.Y to yValues,
@@ -111,6 +151,25 @@ class PointDensityStat(
         const val DEF_ADJUST = 1.0
         val DEF_METHOD = Method.NEIGHBOURS
 
+        // It's assumed that breaks are sorted and contain at least one value
+        internal fun findInterval(value: Double, breaks: List<Double>): Int {
+            if (value < breaks.first()) return 0
+            if (value >= breaks.last()) return breaks.size - 1
+
+            var lower = 0
+            var upper = breaks.size - 1
+            while (lower < upper) {
+                val mid = (lower + upper) / 2
+                if (value >= breaks[mid]) {
+                    lower = mid + 1
+                } else {
+                    upper = mid
+                }
+            }
+
+            return lower
+        }
+
         private val DEF_MAPPING: Map<Aes<*>, DataFrame.Variable> = mapOf(
             Aes.X to Stats.X,
             Aes.Y to Stats.Y,
@@ -139,6 +198,10 @@ class PointDensityStat(
 
         private fun scaledDistanceSquared(x1: Double, y1: Double, x2: Double, y2: Double, xy: Double): Double {
             return (x1 - x2) * (x1 - x2) / xy + (y1 - y2) * (y1 - y2) * xy
+        }
+
+        private fun findInterval(values: List<Double>, breaks: List<Double>): List<Int> {
+            return values.map { findInterval(it, breaks) }
         }
     }
 }
