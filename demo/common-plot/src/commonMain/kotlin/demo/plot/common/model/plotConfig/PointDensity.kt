cecc4de7a29af83c1161b17ff61e0b683590bb55
Artem Smirnov <asmirnov@horis.ru>
2025-10-06 17:00:02 +0200

    Add Batik demo for point density plot.

---
+++ demo/common-plot/src/commonMain/kotlin/demo/plot/common/model/plotConfig/PointDensity.kt
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2025. JetBrains s.r.o.
+ * Use of this source code is governed by the MIT license that can be found in the LICENSE file.
+ */
+
+package demo.plot.common.model.plotConfig
+
+import demo.plot.common.data.Iris
+import demoAndTestShared.parsePlotSpec
+import kotlin.collections.set
+
+class PointDensity {
+    fun plotSpecList(): List<MutableMap<String, Any>> {
+        return listOf(
+            basic(),
+        )
+    }
+
+    private fun basic(): MutableMap<String, Any> {
+        val spec = """
+            {
+              'kind': 'plot',
+              'mapping': {
+                'x': 'sepal width (cm)',
+                'y': 'sepal length (cm)'
+              },
+              'ggtitle': {
+                'text': 'Default pointdensity'
+              },
+              'layers': [
+                {
+                  'geom': 'pointdensity'
+                }
+              ]
+            }
+        """.trimIndent()
+
+        val plotSpec = HashMap(parsePlotSpec(spec))
+        plotSpec["data"] = Iris.df
+        return plotSpec
+
+    }
+}
