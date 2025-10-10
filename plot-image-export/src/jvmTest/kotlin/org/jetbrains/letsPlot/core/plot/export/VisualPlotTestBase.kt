f67d84734931dbfbccb9b939140ce1937b7a7b03
Ivan Seleznev <iseleznev@horis.ru>
2025-10-10 10:39:54 +0200

    Add VisualPlotTestBase

---
+++ plot-image-export/src/jvmTest/kotlin/org/jetbrains/letsPlot/core/plot/export/VisualPlotTestBase.kt
@@ -0,0 +1,109 @@
+/*
+ * Copyright (c) 2025. JetBrains s.r.o.
+ * Use of this source code is governed by the MIT license that can be found in the LICENSE file.
+ */
+
+package org.jetbrains.letsPlot.core.plot.export
+
+import demoAndTestShared.AwtBitmapIO
+import demoAndTestShared.AwtTestCanvasProvider
+import demoAndTestShared.ImageComparer
+import org.jetbrains.letsPlot.commons.geometry.DoubleVector
+import org.jetbrains.letsPlot.commons.values.awt.BitmapUtil
+import org.jetbrains.letsPlot.core.spec.Option
+import org.jetbrains.letsPlot.core.spec.getMap
+import org.jetbrains.letsPlot.core.util.PlotExportCommon.SizeUnit
+import org.junit.BeforeClass
+import java.awt.Font
+import java.awt.FontFormatException
+import java.awt.GraphicsEnvironment
+import java.io.IOException
+import java.io.InputStream
+import javax.imageio.ImageIO
+
+open class VisualPlotTestBase {
+
+    protected fun MutableMap<String, Any>.themeTextNotoSans(): MutableMap<String, Any> {
+        val theme = getMap("theme") ?: emptyMap()
+        this[Option.Plot.THEME] =  theme + mapOf(
+            "text" to mapOf(
+                "blank" to false,
+                "family" to "Noto Sans"
+            ),
+            "axis_title_y" to mapOf(
+                "blank" to true // hide rotated text - antialiasing may cause image differences
+            )
+        )
+        return this
+    }
+
+    private fun createImageComparer(): ImageComparer {
+        return ImageComparer(
+            canvasProvider = AwtTestCanvasProvider(),
+            bitmapIO = AwtBitmapIO,
+            expectedDir = System.getProperty("user.dir") + "/src/jvmTest/resources/expected/",
+            outDir = System.getProperty("user.dir") + "/build/reports/"
+        )
+    }
+
+    private val imageComparer by lazy { createImageComparer() }
+
+    fun assertPlot(
+        expectedFileName: String,
+        plotSpec: MutableMap<String, Any>,
+        width: Number? = null,
+        height: Number? = null,
+        unit: SizeUnit? = null,
+        dpi: Number? = null,
+        scale: Number? = null
+    ) {
+        val plotSize = if (width != null && height != null) DoubleVector(width, height) else null
+
+        val imageData = PlotImageExport.buildImageFromRawSpecs(
+            plotSpec = plotSpec,
+            format = PlotImageExport.Format.PNG,
+            scalingFactor = scale ?: 1.0,
+            targetDPI = dpi,
+            plotSize = plotSize,
+            unit = unit
+        )
+        val image = ImageIO.read(imageData.bytes.inputStream())
+        val bitmap = BitmapUtil.fromBufferedImage(image)
+
+
+        imageComparer.assertBitmapEquals(expectedFileName, bitmap)
+    }
+
+    companion object {
+        @JvmStatic
+        @BeforeClass
+        fun setUp() {
+            registerFont("NotoSans-Regular.ttf")
+            registerFont("NotoSans-Bold.ttf")
+            registerFont("NotoSans-Italic.ttf")
+            registerFont("NotoSans-BoldItalic.ttf")
+            registerFont("NotoSerif-Regular.ttf")
+        }
+
+        private fun registerFont(resourceName: String) {
+            val fontStream: InputStream? = PlotImageExportVisualTest::class.java.getClassLoader().getResourceAsStream(resourceName)
+            try {
+                val customFont = Font.createFont(Font.TRUETYPE_FONT, fontStream)
+                val ge = GraphicsEnvironment.getLocalGraphicsEnvironment()
+                ge.registerFont(customFont)
+            } catch (e: FontFormatException) {
+                e.printStackTrace()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            } finally {
+                if (fontStream != null) {
+                    try {
+                        fontStream.close()
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    }
+                }
+            }
+        }
+    }
+}
