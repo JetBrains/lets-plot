11e39134be36423fdbcb6f8244b8f1201098cd6f
Ivan Kupriyanov <ikupriyanov@horis.ru>
2025-11-17 17:06:15 +0100

    Revert "MagickContext2d: wrap clip-path definition in its own PushDrawingWand/PopDrawingWand to fix performance degradation"
    This reverts commit 0611b392df87b4468db93eb0f0c171db88977c57.

--- platf-imagick/src/nativeMain/kotlin/org/jetbrains/letsPlot/imagick/canvas/MagickContext2d.kt
+++ platf-imagick/src/nativeMain/kotlin/org/jetbrains/letsPlot/imagick/canvas/MagickContext2d.kt
@@ -317,11 +317,6 @@ class MagickContext2d(
         val inverseCTMTransform = stateDelegate.getCTM().inverse() ?: return
         val clipId = clipPath.hashCode().toUInt().toString(16)
 
-        // Always wrap clipPath definition to avoid error that slows down drawing and produces no warnings.
-        // Only the way to get error message is to use magick CLI with generated MVG content.
-        // magick -size 1200x800 mvg:./void.mvg void.png
-        // > magick: non-conforming drawing primitive definition `circle’ @ error/draw.c/RenderMVGContent/4548.
-        ImageMagick.PushDrawingWand(wand)
         ImageMagick.DrawPushDefs(wand)
         ImageMagick.DrawPushClipPath(wand, clipId)
 
@@ -333,7 +328,6 @@ class MagickContext2d(
         ImageMagick.DrawPopDefs(wand)
 
         ImageMagick.DrawSetClipPath(wand, clipId)
-        ImageMagick.PopDrawingWand(wand)
     }
 
     override fun transform(sx: Double, ry: Double, rx: Double, sy: Double, tx: Double, ty: Double) {
