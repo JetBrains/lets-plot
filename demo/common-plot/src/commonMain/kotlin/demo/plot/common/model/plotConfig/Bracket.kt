59508a0087b546a83a3e00485055c60982fc2d1e
Artem Smirnov <horis.asmirnov@gmail.com>
2026-01-28 14:18:39 +0100

    Add the new demo.

--- demo/common-plot/src/commonMain/kotlin/demo/plot/common/model/plotConfig/Bracket.kt
+++ demo/common-plot/src/commonMain/kotlin/demo/plot/common/model/plotConfig/Bracket.kt
@@ -12,6 +12,7 @@ class Bracket {
     fun plotSpecList(): List<MutableMap<String, Any>> {
         return listOf(
             basic(),
+            grouped(),
         )
     }
 
@@ -43,6 +44,58 @@ class Bracket {
                     'max': ['Asia', 'Europe'],
                     'y': [48, 51],
                     'p': [0.01, 0.02]
+                  }
+                }
+              ]
+            }
+        """.trimIndent()
+
+        val plotSpec = HashMap(parsePlotSpec(spec))
+        plotSpec["data"] = AutoMpg.df
+        return plotSpec
+
+    }
+
+    private fun grouped(): MutableMap<String, Any> {
+        val spec = """
+            {
+              'kind': 'plot',
+              'mapping': {
+                'x': 'number of cylinders',
+                'y': 'miles per gallon',
+                'fill': 'origin of car'
+              },
+              'data_meta': {
+                'mapping_annotations': [
+                  {
+                    'aes': 'x',
+                    'annotation': 'as_discrete'
+                  }
+                ]
+              },
+              'ggtitle': {
+                'text': 'Grouping demo'
+              },
+              'layers': [
+                {
+                  'geom': 'boxplot'
+                },
+                {
+                  'geom': 'bracket',
+                  'mapping': {
+                    'xmin': 'min',
+                    'xmax': 'max',
+                    'y': 'y',
+                    'label': 'p',
+                    'color': 'g'
+                  },
+                  'position': {'name': 'dodgev'},
+                  'data': {
+                    'min': [6, 6, 6, 6, 6, 6],
+                    'max': [4, 3, 4, 3, 4, 3],
+                    'y': [53, 61, 53, 61, 53, 61],
+                    'p': [0.01, 0.04, 0.02, 0.05, 0.03, 0.06],
+                    'g': ['US', 'US', 'Asia', 'Asia', 'Europe', 'Europe']
                   }
                 }
               ]
