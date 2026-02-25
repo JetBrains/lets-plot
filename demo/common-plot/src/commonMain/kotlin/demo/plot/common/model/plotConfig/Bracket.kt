f54e4bb428bc3ef10802dc557a9552fe585b234d
Artem Smirnov <horis.asmirnov@gmail.com>
2026-02-25 16:58:41 +0100

    Demo for the 'bracket_dodge' geometry.

--- demo/common-plot/src/commonMain/kotlin/demo/plot/common/model/plotConfig/Bracket.kt
+++ demo/common-plot/src/commonMain/kotlin/demo/plot/common/model/plotConfig/Bracket.kt
@@ -12,8 +12,8 @@ class Bracket {
     fun plotSpecList(): List<MutableMap<String, Any>> {
         return listOf(
             basic(),
-            betweenGroups(),
             negativeTips(),
+            bracketDodge(),
         )
     }
 
@@ -26,7 +26,7 @@ class Bracket {
                 'y': 'miles per gallon'
               },
               'ggtitle': {
-                'text': 'Basic demo'
+                'text': 'Bracket basic'
               },
               'layers': [
                 {
@@ -46,58 +46,6 @@ class Bracket {
                     'max': ['Asia', 'Europe'],
                     'y': [48, 51],
                     'p': [0.01, 0.02]
-                  }
-                }
-              ]
-            }
-        """.trimIndent()
-
-        val plotSpec = HashMap(parsePlotSpec(spec))
-        plotSpec["data"] = AutoMpg.df
-        return plotSpec
-
-    }
-
-    private fun betweenGroups(): MutableMap<String, Any> {
-        val spec = """
-            {
-              'kind': 'plot',
-              'mapping': {
-                'x': 'number of cylinders',
-                'y': 'miles per gallon',
-                'fill': 'origin of car'
-              },
-              'data_meta': {
-                'mapping_annotations': [
-                  {
-                    'aes': 'x',
-                    'annotation': 'as_discrete'
-                  }
-                ]
-              },
-              'ggtitle': {
-                'text': 'Brackets between groups'
-              },
-              'layers': [
-                {
-                  'geom': 'boxplot'
-                },
-                {
-                  'geom': 'bracket',
-                  'mapping': {
-                    'x': 'number of cylinders',
-                    'gstart': 'gstart',
-                    'gend': 'gend',
-                    'y': 'y',
-                    'label': 'p'
-                  },
-                  'inherit_aes': false,
-                  'data': {
-                    'number of cylinders': [6, 6, 6, 4, 4, 4],
-                    'gstart': [0, 0, 1, 0, 0, 1],
-                    'gend': [1, 2, 2, 1, 2, 2],
-                    'y': [53, 61, 69, 53, 61, 69],
-                    'p': [0.01, 0.04, 0.02, 0.05, 0.03, 0.06]
                   }
                 }
               ]
@@ -143,6 +91,58 @@ class Bracket {
                     'max': ['Asia', 'Europe'],
                     'y': [5, 2],
                     'p': [0.01, 0.02]
+                  }
+                }
+              ]
+            }
+        """.trimIndent()
+
+        val plotSpec = HashMap(parsePlotSpec(spec))
+        plotSpec["data"] = AutoMpg.df
+        return plotSpec
+
+    }
+
+    private fun bracketDodge(): MutableMap<String, Any> {
+        val spec = """
+            {
+              'kind': 'plot',
+              'mapping': {
+                'x': 'number of cylinders',
+                'y': 'miles per gallon',
+                'fill': 'origin of car'
+              },
+              'data_meta': {
+                'mapping_annotations': [
+                  {
+                    'aes': 'x',
+                    'annotation': 'as_discrete'
+                  }
+                ]
+              },
+              'ggtitle': {
+                'text': 'Bracket dodge basic'
+              },
+              'layers': [
+                {
+                  'geom': 'boxplot'
+                },
+                {
+                  'geom': 'bracket_dodge',
+                  'mapping': {
+                    'x': 'number of cylinders',
+                    'gstart': 'gstart',
+                    'gend': 'gend',
+                    'y': 'y',
+                    'label': 'p'
+                  },
+                  'inherit_aes': false,
+                  'data': {
+                    'number of cylinders': [6, 6, 6, 4, 4, 4],
+                    'gstart': [0, 0, 1, 0, 0, 1],
+                    'gend': [1, 2, 2, 1, 2, 2],
+                    'y': [48, 52, 56, 48, 52, 56],
+                    'p': [0.01, 0.02, 0.03, 0.04, 0.05, 0.06]
                   }
                 }
               ]
