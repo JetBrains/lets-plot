99d91957f9483ac8ed3362bc92776037c59d4775
Ivan Kupriyanov <horis.ikupriyanov@gmail.com>
2026-03-27 20:26:32 +0100

    Add suppression for OPT_IN_USAGE

--- js-package/src/jsMain/kotlin/FigureModelJs.kt
+++ js-package/src/jsMain/kotlin/FigureModelJs.kt
@@ -1,7 +1,9 @@
 /*
- * Copyright (c) 2024. JetBrains s.r.o.
+ * Copyright (c) 2026. JetBrains s.r.o.
  * Use of this source code is governed by the MIT license that can be found in the LICENSE file.
  */
+
+@file:Suppress("OPT_IN_USAGE")
 
 import org.jetbrains.letsPlot.commons.geometry.DoubleVector
 import org.jetbrains.letsPlot.commons.logging.PortableLogging
--- js-package/src/jsMain/kotlin/tools/FigureToolsControllerJs.kt
+++ js-package/src/jsMain/kotlin/tools/FigureToolsControllerJs.kt
@@ -1,7 +1,9 @@
 /*
- * Copyright (c) 2025. JetBrains s.r.o.
+ * Copyright (c) 2026. JetBrains s.r.o.
  * Use of this source code is governed by the MIT license that can be found in the LICENSE file.
  */
+
+@file:Suppress("OPT_IN_USAGE")
 
 package tools
 
