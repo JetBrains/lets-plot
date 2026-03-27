f0a0226bb262ac21f3402231e05147a29925ca26
Ivan Kupriyanov <horis.ikupriyanov@gmail.com>
2026-03-27 20:15:58 +0100

    Enable experimental Wasm DSL in build configuration

--- plot-base/build.gradle.kts
+++ plot-base/build.gradle.kts
@@ -1,3 +1,7 @@
+@file:OptIn(ExperimentalWasmDsl::class)
+
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+
 /*
  * Copyright (c) 2019. JetBrains s.r.o.
  * Use of this source code is governed by the MIT license that can be found in the LICENSE file.
