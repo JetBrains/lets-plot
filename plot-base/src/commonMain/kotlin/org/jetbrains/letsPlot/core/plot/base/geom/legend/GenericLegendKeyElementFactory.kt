916c7780f2a4c82148d1e13523432244c95701a9
Mikhail Koroteev <horis.mkoroteev@gmail.com>
2026-04-07 01:29:21 +0300

    Experiments with size in legend

--- plot-base/src/commonMain/kotlin/org/jetbrains/letsPlot/core/plot/base/geom/legend/GenericLegendKeyElementFactory.kt
+++ plot-base/src/commonMain/kotlin/org/jetbrains/letsPlot/core/plot/base/geom/legend/GenericLegendKeyElementFactory.kt
@@ -7,20 +7,40 @@ package org.jetbrains.letsPlot.core.plot.base.geom.legend
 
 import org.jetbrains.letsPlot.commons.geometry.DoubleVector
 import org.jetbrains.letsPlot.core.plot.base.DataPointAesthetics
+import org.jetbrains.letsPlot.core.plot.base.aes.AesScaling
 import org.jetbrains.letsPlot.core.plot.base.geom.util.GeomHelper
 import org.jetbrains.letsPlot.core.plot.base.render.LegendKeyElementFactory
 import org.jetbrains.letsPlot.datamodel.svg.dom.SvgGElement
 import org.jetbrains.letsPlot.datamodel.svg.dom.SvgRectElement
 
 class GenericLegendKeyElementFactory : LegendKeyElementFactory {
-    override fun createKeyElement(p: DataPointAesthetics, size: DoubleVector): SvgGElement {
-        // rect with background (to show fill) and stroke (to show color)
-        val rect = SvgRectElement(0.0, 0.0, size.x, size.y)
-        GeomHelper.decorate(rect, p)
-        rect.strokeWidth().set(1.5) // set thickness
+    companion object {
+        private const val MIN_INTERIOR_SIZE = 6.0
+    }
 
-        val g = SvgGElement()
-        g.children().add(rect)
-        return g
+    override fun createKeyElement(p: DataPointAesthetics, size: DoubleVector): SvgGElement {
+        val sw = AesScaling.strokeWidth(p)
+
+        val rect = SvgRectElement(
+            sw / 2,
+            sw / 2,
+            maxOf(0.0, size.x - sw),
+            maxOf(0.0, size.y - sw)
+        )
+        GeomHelper.decorate(rect, p)
+
+        return SvgGElement().apply {
+            children().add(rect)
+        }
+    }
+
+    override fun minimumKeySize(p: DataPointAesthetics): DoubleVector {
+        val sw = AesScaling.strokeWidth(p)
+        val cellSize = MIN_INTERIOR_SIZE + 2 * sw
+
+        // If LegendComponent subtracts 2 before passing size into createKeyElement(),
+        // compensate here:
+        return DoubleVector(cellSize + 2, cellSize + 2)
+//        return DoubleVector(cellSize, cellSize)
     }
 }
