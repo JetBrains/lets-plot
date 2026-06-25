18bbf2592ca40cb15fdaa51727a87073a7acd0ed
Mikhail Koroteev <horis.mkoroteev@gmail.com>
2026-06-25 21:25:37 +0300

    Fix vjust/hjust for vertical axis labels
    * Fix label justification for vertical axes
    * Fix handling of axis labels with angles like 270
    * Add more examples in scale_label_justification.ipynb
    * Fix axis label bounds used to detect overlapping
    * Fix code review findings
    * Update future_changes.md

--- plot-builder/src/commonMain/kotlin/org/jetbrains/letsPlot/core/plot/builder/layout/axis/label/VerticalRotatedLabelsLayout.kt
+++ plot-builder/src/commonMain/kotlin/org/jetbrains/letsPlot/core/plot/builder/layout/axis/label/VerticalRotatedLabelsLayout.kt
@@ -51,64 +51,115 @@ internal class VerticalRotatedLabelsLayout(
 
         val maxLabelWidth = labelBoundsList.maxOfOrNull { it.width } ?: 0.0
 
-        val radAngle = toRadians(myRotationAngle)
-        val sinA = sin(radAngle)
-        val cosA = cos(radAngle)
-        val isVertical = abs(cosA) < 1e-6
-        val isUpsideDown = cosA < 0
-        val isHorizontal = abs(sinA) < 1e-6 && !isUpsideDown
-
         val orientationSign = when (orientation) {
             Orientation.LEFT -> -1.0
             Orientation.RIGHT -> 1.0
             else -> throw IllegalStateException("Unsupported orientation $orientation")
         }
 
+        val radAngle = toRadians(myRotationAngle)
+        val sinA = sin(radAngle)
+        val cosA = cos(radAngle)
+        val isVertical = abs(cosA) < 1e-6
+        val isUpsideDown = cosA < 0
+        val isHorizontal = abs(sinA) < 1e-6 && !isUpsideDown
+        val isLabelDirectedFromTick = when (orientation) {
+            Orientation.LEFT -> sinA > 0
+            Orientation.RIGHT -> sinA < 0
+        }
+
+        val flushHjust = if (orientation == Orientation.LEFT) 1.0 else 0.0
+        val hJust = if (theme.labelHJust().isNaN()) flushHjust else theme.labelHJust()
+
         val vJust = if (theme.labelVJust().isNaN()) {
             when {
-                isVertical || isHorizontal -> 0.5
-                orientation == Orientation.LEFT && sinA > 0 -> 0.0
-                orientation == Orientation.LEFT && sinA < 0 -> 1.0
-                orientation == Orientation.RIGHT && sinA > 0 -> 1.0
-                orientation == Orientation.RIGHT && sinA < 0 -> 0.0
-                else -> 0.0
+                isHorizontal || isVertical -> 0.5
+                isLabelDirectedFromTick -> 0.0
+                else -> 1.0
             }
         } else {
             theme.labelVJust()
         }
 
-        val hJust = if (theme.labelHJust().isNaN()) {
-            if (orientation == Orientation.RIGHT) 1.0 else 0.0
-        } else {
-            theme.labelHJust()
+        val verticalAnchor = when {
+            isVertical || isUpsideDown -> Text.VerticalAnchor.CENTER
+            isHorizontal && vJust == 0.0 -> Text.VerticalAnchor.TOP
+            isHorizontal && vJust == 1.0 -> Text.VerticalAnchor.BOTTOM
+            vJust == 0.0 && isLabelDirectedFromTick -> Text.VerticalAnchor.BOTTOM
+            vJust == 1.0 && !isLabelDirectedFromTick -> Text.VerticalAnchor.TOP
+            else -> Text.VerticalAnchor.CENTER
         }
 
+        val isCornerCase = !isHorizontal && verticalAnchor != Text.VerticalAnchor.CENTER
+
+        val horizontalAnchor = when {
+            isVertical -> hAnchorForVerticalLabels(vJust, sinA)
+            isHorizontal && hJust == 0.0 -> Text.HorizontalAnchor.LEFT
+            isHorizontal && hJust == 1.0 -> Text.HorizontalAnchor.RIGHT
+            isCornerCase && orientation == Orientation.LEFT -> Text.HorizontalAnchor.RIGHT
+            isCornerCase && orientation == Orientation.RIGHT -> Text.HorizontalAnchor.LEFT
+            else -> Text.HorizontalAnchor.MIDDLE
+        }
+
+        val slotCenterX = orientationSign * maxLabelWidth / 2
         val xBBoxOffset: (DoubleRectangle) -> Double = { rect: DoubleRectangle ->
-            (maxLabelWidth - rect.width) * ((orientationSign + 1.0) / 2 - hJust)
+            slotCenterX + (maxLabelWidth - rect.width) * (hJust - 0.5)
         }
 
         val yBBoxOffset: (DoubleRectangle) -> Double = { rect: DoubleRectangle ->
-            rect.height * (-vJust)
+            rect.height * (0.5 - vJust)
         }
 
+        val leftAnchorX = slotCenterX - maxLabelWidth / 2
+        val rightAnchorX = slotCenterX + maxLabelWidth / 2
+
         val xOffset: (DoubleRectangle) -> Double = { rect: DoubleRectangle ->
-            xBBoxOffset(rect).let { it + orientationSign * rect.width / 2 }
+            when {
+                isCornerCase -> (maxLabelWidth - rect.width) * (hJust - flushHjust)
+                isHorizontal && horizontalAnchor == Text.HorizontalAnchor.LEFT -> leftAnchorX
+                isHorizontal && horizontalAnchor == Text.HorizontalAnchor.RIGHT -> rightAnchorX
+                else -> xBBoxOffset(rect)
+            }
         }
 
         val yOffset: (DoubleRectangle) -> Double = { rect: DoubleRectangle ->
-            rect.height * (0.5 - vJust)
+            when {
+                isVertical -> 0.0
+                verticalAnchor != Text.VerticalAnchor.CENTER -> 0.0
+                else -> yBBoxOffset(rect)
+            }
         }
 
         val labelAdditionalOffsets = labelBoundsList.map {
             DoubleVector(xOffset(it), yOffset(it))
         }
 
-        val horizontalAnchor = Text.HorizontalAnchor.MIDDLE
-        val verticalAnchor = Text.VerticalAnchor.CENTER
+        val renderedLabelBounds: (DoubleRectangle, DoubleVector) -> DoubleRectangle = { rect, offset ->
+            val w = rect.width
+            val h = rect.height
+            val centerShiftY = when {
+                isVertical -> when (horizontalAnchor) {
+                    Text.HorizontalAnchor.LEFT -> if (sinA > 0) -h / 2 else h / 2
+                    Text.HorizontalAnchor.RIGHT -> if (sinA > 0) h / 2 else -h / 2
+                    else -> 0.0
+                }
+                verticalAnchor == Text.VerticalAnchor.TOP -> h / 2
+                verticalAnchor == Text.VerticalAnchor.BOTTOM -> -h / 2
+                else -> 0.0
+            }
+            val centerShiftX = when {
+                isVertical -> 0.0
+                horizontalAnchor == Text.HorizontalAnchor.LEFT -> w / 2
+                horizontalAnchor == Text.HorizontalAnchor.RIGHT -> -w / 2
+                else -> 0.0
+            }
+            val centerX = rect.origin.x + offset.x + centerShiftX
+            val centerY = rect.origin.y + offset.y + centerShiftY
+            DoubleRectangle(centerX - w / 2, centerY - h / 2, w, h)
+        }
 
-        val adjustedLabelBoundsList = labelBoundsList.map {
-            val origin = DoubleVector(it.origin.x + xBBoxOffset(it), yBBoxOffset(it) + it.origin.y)
-            DoubleRectangle(origin, it.dimension)
+        val adjustedLabelBoundsList = labelBoundsList.mapIndexed { i, rect ->
+            renderedLabelBounds(rect, labelAdditionalOffsets[i])
         }
 
         return createAxisLabelsLayoutInfoBuilder(bounds, overlap)
@@ -116,13 +167,24 @@ internal class VerticalRotatedLabelsLayout(
             .labelVerticalAnchor(verticalAnchor)
             .labelRotationAngle(-myRotationAngle)
             .labelAdditionalOffsets(labelAdditionalOffsets)
-            .labelBoundsList(adjustedLabelBoundsList.map(::alignToLabelMargin)) // for debug drawing
+            .labelBoundsList(adjustedLabelBoundsList.map(::alignToLabelMargin))
             .build()
     }
 
     override fun labelBounds(labelNormalSize: DoubleVector): DoubleRectangle {
         return BreakLabelsLayoutUtil.rotatedLabelBounds(labelNormalSize, myRotationAngle).let {
             DoubleRectangle(0.0, 0.0, it.width, it.height)
+        }
+    }
+
+    private fun hAnchorForVerticalLabels(vjust: Double, sinA: Double): Text.HorizontalAnchor {
+        if (vjust != 0.0 && vjust != 1.0) {
+            return Text.HorizontalAnchor.MIDDLE
+        }
+        return when {
+            sinA > 0.0 -> if (vjust == 1.0) Text.HorizontalAnchor.LEFT else Text.HorizontalAnchor.RIGHT
+            sinA < 0.0 -> if (vjust == 1.0) Text.HorizontalAnchor.RIGHT else Text.HorizontalAnchor.LEFT
+            else -> Text.HorizontalAnchor.MIDDLE
         }
     }
 }
